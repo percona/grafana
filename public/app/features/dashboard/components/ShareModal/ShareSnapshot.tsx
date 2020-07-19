54f8bbe0a5bb3db17c39c8e74193903568428c25
Alexander Tymchuk <alexander.tymchuk@percona.com>
2020-07-20 00:41:55 +0300

    SAAS-178 apply pmm patches (#2)
    * SAAS-178 apply fav icon patch
    
    * SAAS-178 apply rm update tab patch
    
    * SAAS-178 apply share panel patch
    
    * SAAS-178 review follow up

--- public/app/features/dashboard/components/ShareModal/ShareSnapshot.tsx
+++ public/app/features/dashboard/components/ShareModal/ShareSnapshot.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Button, ClipboardButton, Icon, LegacyForms, LinkButton } from '@grafana/ui';
+import { Button, ClipboardButton, Icon, LegacyForms } from '@grafana/ui';
 import { AppEvents, SelectableValue } from '@grafana/data';
 import { getBackendSrv } from '@grafana/runtime';
 import { DashboardModel, PanelModel } from 'app/features/dashboard/state';
@@ -16,6 +16,7 @@ const expireOptions: Array<SelectableValue<number>> = [
   { label: '1 Hour', value: 60 * 60 },
   { label: '1 Day', value: 60 * 60 * 24 },
   { label: '7 Days', value: 60 * 60 * 24 * 7 },
+  { label: '90 Days', value: 60 * 60 * 24 * 90 },
 ];
 
 interface Props {
@@ -46,10 +47,10 @@ export class ShareSnapshot extends PureComponent<Props, State> {
     this.state = {
       isLoading: false,
       step: 1,
-      selectedExpireOption: expireOptions[0],
-      snapshotExpires: expireOptions[0].value,
+      selectedExpireOption: expireOptions[4],
+      snapshotExpires: expireOptions[4].value,
       snapshotName: props.dashboard.title,
-      timeoutSeconds: 4,
+      timeoutSeconds: 30,
       snapshotUrl: '',
       deleteUrl: '',
       externalEnabled: false,
@@ -212,13 +213,33 @@ export class ShareSnapshot extends PureComponent<Props, State> {
       <>
         <div>
           <p className="share-modal-info-text">
-            A snapshot is an instant way to share an interactive dashboard publicly. When created, we{' '}
-            <strong>strip sensitive data</strong> like queries (metric, template and annotation) and panel links,
-            leaving only the visible metric data and series names embedded into your dashboard.
+            A snapshot is a way to securely share your dashboard with Percona. When created, we{' '}
+            <strong>strip sensitive data </strong>
+            like queries (metrics, template variables, and annotations) along with panel links. The shared dashboard
+            will only be available for viewing by Percona Engineers, and the content on the dashboard will assist
+            Percona Engineers in troubleshooting your case.
           </p>
           <p className="share-modal-info-text">
-            Keep in mind, your <strong>snapshot can be viewed by anyone</strong> that has the link and can reach the
-            URL. Share wisely.
+            You can safely leave the defaults set as they are, but for further information:
+          </p>
+          <p className="share-modal-info-text">
+            <ul>
+              <li>
+                <strong>Snapshot Name:</strong> Give the snapshot a name so that Percona can distinguish your dashboard.
+              </li>
+              <li>
+                <strong>Expire:</strong> The time before snapshot expires. Configure lower if required. Percona
+                automatically purge shared dashboards after 90 days.
+              </li>
+              <li>
+                <strong>Timeout (seconds):</strong> Time the dashboard takes to load before the snapshot is generated.
+              </li>
+            </ul>
+          </p>
+          <p className="share-modal-info-text">
+            <strong>What to do next: </strong> Once you click <i>Share with Percona</i>, wait for the dashboard to be
+            generated, and you will be provided with a unique URL that then needs to be communicated to Percona via your
+            ticket.
           </p>
         </div>
         <div className="gf-form-group share-modal-options">
@@ -275,13 +296,6 @@ export class ShareSnapshot extends PureComponent<Props, State> {
               Copy Link
             </ClipboardButton>
           </div>
-        </div>
-
-        <div className="pull-right" ng-if="step === 2" style={{ padding: '5px' }}>
-          Did you make a mistake?{' '}
-          <LinkButton variant="link" target="_blank" onClick={this.deleteSnapshot}>
-            delete snapshot.
-          </LinkButton>
         </div>
       </>
     );
