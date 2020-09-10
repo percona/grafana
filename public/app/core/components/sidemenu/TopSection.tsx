8bd805692fee4caf4369e236c0bd8f19104ba9e5
Tiago Mota Santos <tiago.mota@percona.com>
2020-09-10 14:09:53 +0100

    PMM-6573 Show DBaaS menu based on settings request

--- public/app/core/components/sidemenu/TopSection.tsx
+++ public/app/core/components/sidemenu/TopSection.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import _ from 'lodash';
 import TopSectionItem from './TopSectionItem';
 import config from '../../config';
-import { getLocationSrv } from '@grafana/runtime';
+import { getLocationSrv, getBackendSrv } from '@grafana/runtime';
 
 const TopSection: FC<any> = () => {
   const navTree = _.cloneDeep(config.bootData.navTree);
@@ -11,17 +11,25 @@ const TopSection: FC<any> = () => {
     text: 'Search',
     icon: 'search',
   };
-
-  mainLinks.push({
+  const dbaasLink = {
     id: 'dbaas',
     icon: 'database',
     text: 'DBaaS',
     url: '/graph/d/pmm-dbaas/pmm-dbaas',
-  });
-
+  };
   const onOpenSearch = () => {
     getLocationSrv().update({ query: { search: 'open' }, partial: true });
   };
+  const [showDBaaS, setShowDBaaS] = useState(false);
+
+  // TODO: once DBaaS is enabled by default move it to config file (api/index.go)
+  useEffect(() => {
+    if (config.bootData.user.isSignedIn) {
+      getBackendSrv()
+        .post('http://localhost/v1/Settings/Get')
+        .then(({ settings }) => setShowDBaaS(settings.dbaas_enabled));
+    }
+  }, []);
 
   return (
     <div className="sidemenu__top">
@@ -29,6 +37,7 @@ const TopSection: FC<any> = () => {
       {mainLinks.map((link, index) => {
         return <TopSectionItem link={link} key={`${link.id}-${index}`} />;
       })}
+      {showDBaaS && <TopSectionItem link={dbaasLink} />}
     </div>
   );
 };
