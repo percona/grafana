031a7e37a97a4325d6f5dd011dc6d90268882bad
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-11-25 22:20:05 +0100

    PMM-6939 merge PMM-6958

--- public/app/features/integrated-alerting/components/AlertRuleTemplate/AlertRuleTemplate.service.ts
+++ public/app/features/integrated-alerting/components/AlertRuleTemplate/AlertRuleTemplate.service.ts
@@ -4,7 +4,7 @@ import { TemplatesList, UploadAlertRuleTemplatePayload } from './AlertRuleTempla
 const BASE_URL = `${window.location.origin}/v1/management/ia/Templates`;
 
 export const AlertRuleTemplateService = {
-  async upload(payload: UploadAlertRuleTemplatePayload) {
+  async upload(payload: UploadAlertRuleTemplatePayload): Promise<void> {
     return getBackendSrv().post(`${BASE_URL}/Create`, payload);
   },
   async list(): Promise<TemplatesList> {
