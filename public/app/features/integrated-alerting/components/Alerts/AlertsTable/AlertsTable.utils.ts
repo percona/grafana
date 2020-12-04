3fa774da0651ea07b42c0d0efb5bf1d8f61122c9
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-04 19:13:29 +0100

    PMM-6994 update table utils

--- public/app/features/integrated-alerting/components/Alerts/AlertsTable/AlertsTable.utils.ts
+++ public/app/features/integrated-alerting/components/Alerts/AlertsTable/AlertsTable.utils.ts
@@ -1,53 +1,28 @@
 import moment from 'moment/moment';
-import {
-  AlertRule,
-  AlertRuleFilterType,
-  AlertRuleSeverity,
-  AlertRulesListResponseFilter,
-  AlertRulesListResponseRule,
-  AlertRulesListResponseTemplate,
-} from '../AlertRules.types';
+import { Alert, AlertsListResponseLabel, AlertsListResponseAlert, AlertStatus } from '../Alerts.types';
+import { AlertRuleSeverity } from '../../AlertRules/AlertRules.types';
 
-export const formatFilter = (filter: AlertRulesListResponseFilter): string => {
-  const { key, type, value } = filter;
+export const formatLabel = (label: [string, string]): string => {
+  const [key, value] = label;
 
-  return `${key}${AlertRuleFilterType[type]}${value}`;
+  return `${key}=${value}`;
 };
 
-export const formatThreshold = (template: AlertRulesListResponseTemplate): string => {
-  const thresholdParam = template.params.find(param => param.name === 'threshold');
-
-  const { value, unit } = thresholdParam;
-
-  return `${value}${unit ? ` ${unit}` : ''}`;
+export const formatLabels = (labels: AlertsListResponseLabel): string[] => {
+  return Object.entries(labels).map(formatLabel);
 };
 
-export const formatDuration = (duration: string): string => {
-  const seconds = parseInt(duration, 10);
-
-  // NOTE: this is needed because the `humanize` function by moment.js returns 'a few seconds' for < ~50 secs
-  if (seconds < 60) {
-    return `${seconds} seconds`;
-  }
-  // TODO: when switching to moment.js v2.25, add thresholds to the `humanize` function,
-  //       making it more precise. Right now it's just approximating:
-  //       3000 (seconds, which is 50 minutes) will result in 'a minute';
-  return moment.duration(seconds, 'seconds').humanize();
-};
-
-export const formatRule = (rule: AlertRulesListResponseRule): AlertRule => {
-  const { created_at, disabled, filters, for: duration, last_notified, template, severity, summary } = rule;
+export const formatAlert = (rule: AlertsListResponseAlert): Alert => {
+  const { active_since, labels, last_notified, severity, status, summary } = rule;
 
   return {
-    createdAt: moment(created_at).format('YYYY-MM-DD HH:mm:ss.SSS'),
-    disabled,
-    duration: formatDuration(duration),
-    filters: filters.map(formatFilter),
+    activeSince: active_since ? moment(active_since).format('YYYY-MM-DD HH:mm:ss.SSS') : '',
+    labels: formatLabels(labels),
     severity: AlertRuleSeverity[severity],
+    status: AlertStatus[status],
     summary,
-    threshold: formatThreshold(template),
     lastNotified: last_notified ? moment(last_notified).format('YYYY-MM-DD HH:mm:ss.SSS') : '',
   };
 };
 
-export const formatRules = (rules: AlertRulesListResponseRule[]): AlertRule[] => rules.map(formatRule);
+export const formatAlerts = (rules: AlertsListResponseAlert[]): Alert[] => rules.map(formatAlert);
