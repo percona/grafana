91e20fb7ee0d95adb6aa8abbd15d6d37de4743c7
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-04 19:12:52 +0100

    PMM-6994 update styles

--- public/app/features/integrated-alerting/components/Alerts/AlertsTable/AlertsTable.styles.ts
+++ public/app/features/integrated-alerting/components/Alerts/AlertsTable/AlertsTable.styles.ts
@@ -3,7 +3,7 @@ import { GrafanaTheme } from '@grafana/data';
 import { stylesFactory } from '@grafana/ui';
 
 export const getStyles = stylesFactory((theme: GrafanaTheme) => {
-  const { colors, palette } = theme;
+  const { colors } = theme;
 
   const borderColor = colors.border2;
   const backgroundColorBody = colors.bg1;
@@ -80,34 +80,19 @@ export const getStyles = stylesFactory((theme: GrafanaTheme) => {
       align-items: center;
       border: 1px solid ${backgroundColorBody};
     `,
-    filtersWrapper: css`
+    labelsWrapper: css`
       padding: 5px;
       display: flex;
       flex-wrap: wrap;
       align-items: center;
       margin: 0 -${cellPadding}px;
     `,
-    filter: css`
+    label: css`
       background-color: ${colors.bg3};
       border-radius: 8px;
       padding: 6px;
       line-height: 1;
       margin: 5px;
-    `,
-    lastNotifiedWrapper: css`
-      display: flex;
-      flex-wrap: nowrap;
-      align-items: center;
-    `,
-    lastNotifiedDate: css`
-      flex: 1;
-    `,
-    lastNotifiedCircle: css`
-      border-radius: 50%;
-      background-color: ${palette.red};
-      margin-left: 10px;
-      height: 16px;
-      width: 16px;
     `,
     disabledRow: css`
       & td {
