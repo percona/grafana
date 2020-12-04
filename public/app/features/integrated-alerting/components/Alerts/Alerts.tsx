e27226dd472f052dbd90832616e9422a8feaa569
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-04 19:16:22 +0100

    PMM-6994 use AlertTable in Alerts

--- public/app/features/integrated-alerting/components/Alerts/Alerts.tsx
+++ public/app/features/integrated-alerting/components/Alerts/Alerts.tsx
@@ -1,4 +1,4 @@
 import React, { FC } from 'react';
 import { AlertsTable } from './AlertsTable';
 
-export const AlertsTable: FC = () => <AlertsTable />;
+export const Alerts: FC = () => <AlertsTable />;
