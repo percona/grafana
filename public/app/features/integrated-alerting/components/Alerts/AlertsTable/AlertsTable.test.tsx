1a2a6a15bbbe720d38e5696f49f5d652c2a56937
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-04 19:44:56 +0100

    PMM-6994 add tests

---
+++ public/app/features/integrated-alerting/components/Alerts/AlertsTable/AlertsTable.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+import { dataQa } from '@percona/platform-core';
+import { AlertsTable } from './AlertsTable';
+import { act } from 'react-dom/test-utils';
+
+describe('AddAlertRuleTemplatesTable', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render the table correctly', async () => {
+    let wrapper: ReactWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+
+    await act(async () => {
+      wrapper = mount(<AlertsTable />);
+    });
+
+    wrapper.update();
+
+    expect(wrapper.find(dataQa('alerts-table-thead')).find('tr')).toHaveLength(1);
+    expect(wrapper.find(dataQa('alerts-table-tbody')).find('tr')).toHaveLength(6);
+    expect(wrapper.find(dataQa('alerts-table-no-data'))).toHaveLength(0);
+  });
+});
