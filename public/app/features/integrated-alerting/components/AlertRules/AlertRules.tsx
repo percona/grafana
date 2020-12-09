e90e089e637059260b8035849ef25d54a6ec8d36
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-10 00:00:57 +0100

    PMM-7008 add create rule modal to AlertRules.tsx

--- public/app/features/integrated-alerting/components/AlertRules/AlertRules.tsx
+++ public/app/features/integrated-alerting/components/AlertRules/AlertRules.tsx
@@ -1,4 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { Button, useStyles } from '@grafana/ui';
 import { AlertRulesTable } from './AlertRulesTable';
+import { AddAlertRuleModal } from './AddAlertRuleModal';
+import { getStyles } from './AlertRules.styles';
+import { Messages } from '../../IntegratedAlerting.messages';
 
-export const AlertRules: FC = () => <AlertRulesTable />;
+export const AlertRules: FC = () => {
+  const styles = useStyles(getStyles);
+  const [addModalVisible, setAddModalVisible] = useState(false);
+
+  return (
+    <>
+      <div className={styles.actionsWrapper}>
+        <Button
+          size="md"
+          icon="plus-square"
+          variant="link"
+          onClick={() => setAddModalVisible(!addModalVisible)}
+          data-qa="alert-rule-template-add-modal-button"
+        >
+          {Messages.alertRuleTemplate.addAction}
+        </Button>
+      </div>
+      <AddAlertRuleModal isVisible={addModalVisible} setVisible={setAddModalVisible} />
+      <AlertRulesTable />
+    </>
+  );
+};
