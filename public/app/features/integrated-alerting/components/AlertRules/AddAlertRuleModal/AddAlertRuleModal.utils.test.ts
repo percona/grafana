02ae41b45f42f9e9199ccf83189d5b18afe50d7c
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-11 22:31:13 +0100

    PMM-7008 add tests for formatFilters

--- public/app/features/integrated-alerting/components/AlertRules/AddAlertRuleModal/AddAlertRuleModal.utils.test.ts
+++ public/app/features/integrated-alerting/components/AlertRules/AddAlertRuleModal/AddAlertRuleModal.utils.test.ts
@@ -1,4 +1,4 @@
-import { formatFilter } from './AddAlertRuleModal.utils';
+import { formatFilter, formatFilters } from './AddAlertRuleModal.utils';
 
 describe('AddAlertRuleModal utils', () => {
   test('formatFilter', () => {
@@ -31,5 +31,50 @@ describe('AddAlertRuleModal utils', () => {
       value: '',
       type: 'EQUAL',
     });
+  });
+
+  test('formatFilters', () => {
+    expect(formatFilters('')).toEqual([]);
+    expect(formatFilters('=')).toEqual([
+      {
+        key: '',
+        value: '',
+        type: 'EQUAL',
+      },
+    ]);
+    expect(formatFilters('test=xyz')).toEqual([
+      {
+        key: 'test',
+        value: 'xyz',
+        type: 'EQUAL',
+      },
+    ]);
+    expect(formatFilters('  test=xyz, ijk=,   foo =bar,\nzyx=abc, \naaa=   zzz ')).toEqual([
+      {
+        key: 'test',
+        value: 'xyz',
+        type: 'EQUAL',
+      },
+      {
+        key: 'ijk',
+        value: '',
+        type: 'EQUAL',
+      },
+      {
+        key: 'foo ',
+        value: 'bar',
+        type: 'EQUAL',
+      },
+      {
+        key: 'zyx',
+        value: 'abc',
+        type: 'EQUAL',
+      },
+      {
+        key: 'aaa',
+        value: '   zzz',
+        type: 'EQUAL',
+      },
+    ]);
   });
 });
