e16ed86ad573e801d237660845e3eca925a94922
Tiago Mota Santos <tiago.mota@percona.com>
2020-12-16 10:44:17 +0000

    PMM-6962 Remove console log

--- public/app/features/integrated-alerting/components/NotificationChannel/NotificationChannelActions/NotificationChannelActions.tsx
+++ public/app/features/integrated-alerting/components/NotificationChannel/NotificationChannelActions/NotificationChannelActions.tsx
@@ -7,7 +7,7 @@ import { NotificationChannelProvider } from '../NotificationChannel.provider';
 export const NotificationChannelActions: FC<NotificationChannelActionsProps> = ({ notificationChannel }) => {
   const styles = useStyles(getStyles);
   const { setSelectedNotificationChannel, setAddModalVisible } = useContext(NotificationChannelProvider);
-  console.log('HEY', setSelectedNotificationChannel);
+
   return (
     <div className={styles.actionsWrapper}>
       <IconButton
