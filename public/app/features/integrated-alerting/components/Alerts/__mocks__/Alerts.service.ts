fcc03bd8cc8bb25d0e200ed284136fbe8f29ed85
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-22 01:23:52 +0100

    PMM-6995 update mocks

--- public/app/features/integrated-alerting/components/Alerts/__mocks__/Alerts.service.ts
+++ public/app/features/integrated-alerting/components/Alerts/__mocks__/Alerts.service.ts
@@ -5,3 +5,4 @@ import * as alertsService from '../Alerts.service';
 export const AlertsService = jest.genMockFromModule<typeof alertsService>('../AlertRules.service').AlertsService;
 
 AlertsService.list = () => Promise.resolve({ alerts: alertsStubs });
+AlertsService.toggle = () => Promise.resolve();
