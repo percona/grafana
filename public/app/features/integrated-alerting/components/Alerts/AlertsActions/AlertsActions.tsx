b2841beeb48e8595978d3da6fb8f1b4273877f46
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-12-22 17:19:55 +0100

    PMM-6995 use local icons

--- public/app/features/integrated-alerting/components/Alerts/AlertsActions/AlertsActions.tsx
+++ public/app/features/integrated-alerting/components/Alerts/AlertsActions/AlertsActions.tsx
@@ -8,14 +8,15 @@ import { AlertsActionsProps } from './AlertsActions.types';
 import { AlertStatus } from '../Alerts.types';
 import { AlertsService } from '../Alerts.service';
 import { Messages } from './AlertsActions.messages';
+import { Bell, BellBarred } from './icons';
 
 export const AlertsActions: FC<AlertsActionsProps> = ({ alert, getAlerts }) => {
   const styles = useStyles(getStyles);
   const [pendingRequest, setPendingRequest] = useState(false);
 
   const isSilenced = alert.status === AlertStatus.SILENCED;
-  // const silenceIcon = isSilenced ? 'bell-alt' : 'bell-barred';
-  // const title = isSilenced ? Messages.activateTitle : Messages.silenceTitle;
+  const ToggleIcon = isSilenced ? Bell : BellBarred;
+  const title = isSilenced ? Messages.activateTitle : Messages.silenceTitle;
 
   const toggleAlert = async () => {
     setPendingRequest(true);
@@ -38,30 +39,9 @@ export const AlertsActions: FC<AlertsActionsProps> = ({ alert, getAlerts }) => {
       {pendingRequest ? (
         <Spinner />
       ) : (
-        <svg
-          onClick={toggleAlert}
-          width="20"
-          height="17"
-          viewBox="0 0 20 17"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M12.7 5.5V2.35C12.7 1.6048 12.0952 1 11.35 1L8.65 1C7.9048 1 7.3 1.6048 7.3 2.35V5.5L0.999999 11.8L19 11.8L12.7 5.5Z"
-            stroke="black"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M13.1499 14.5C12.4137 15.5125 11.3481 16.3 9.9999 16.3C8.6517 16.3 7.5861 15.5134 6.8499 14.5"
-            stroke="black"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        // <IconButton data-qa="silence-alert-button" name={silenceIcon} title={title} onClick={toggleAlert} />
+        <button onClick={toggleAlert} className={styles.button} title={title}>
+          <ToggleIcon />
+        </button>
       )}
     </div>
   );
