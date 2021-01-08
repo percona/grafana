83284f03c3cb8863f8739b8aa0efc751b1bf6fc6
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-01-08 13:29:12 +0100

    Update AlertRulesActions.messages.ts
    Co-authored-by: Tiago Mota Santos <tiago.a.mota@hotmail.com>

--- public/app/features/integrated-alerting/components/AlertRules/AlertRulesActions/AlertRulesActions.messages.ts
+++ public/app/features/integrated-alerting/components/AlertRules/AlertRulesActions/AlertRulesActions.messages.ts
@@ -1,7 +1,7 @@
 export const Messages = {
   getEnabledMessage: (summary: string) => `Alert rule "${summary}" successfully enabled`,
   getDisabledMessage: (summary: string) => `Alert rule "${summary}" successfully disabled`,
-  createSuccess: 'Alert rule created',
+  createSuccess: 'Alert rule successfully created',
   deleteSuccess: 'Alert rule deleted',
   copyOf: 'Copy of',
 };
