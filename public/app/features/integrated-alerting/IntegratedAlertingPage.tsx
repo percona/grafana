263c4cb3409c7da5b0935382ebf576e88ad0d01f
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-01-20 19:38:25 +0100

    PMM-6924 update IntegratedAlertingPage

--- public/app/features/integrated-alerting/IntegratedAlertingPage.tsx
+++ public/app/features/integrated-alerting/IntegratedAlertingPage.tsx
@@ -1,54 +1,57 @@
 import React, { FC, useEffect, useState } from 'react';
-import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux';
-import { hot } from 'react-hot-loader';
+import { useSelector } from 'react-redux';
 import { TabsBar, TabContent, Tab, useStyles } from '@grafana/ui';
-import { UrlQueryValue, NavModel, NavModelItem } from '@grafana/data';
-// import { getNavModel } from 'app/core/selectors/navModel';
+import { UrlQueryValue } from '@grafana/data';
+import { getLocationSrv } from '@grafana/runtime';
+import { Breadcrumb, PageModel } from 'app/core/components/Breadcrumb';
 import { StoreState } from 'app/types';
-import { updateLocation } from 'app/core/actions';
 import { Messages } from './IntegratedAlerting.messages';
 import { getStyles } from './IntegratedAlerting.styles';
 import { TabKeys } from './IntegratedAlerting.types';
 import { Alerts, AlertRuleTemplate, AlertRules, NotificationChannel } from './components';
 import { DEFAULT_TAB } from './integratedAlerting.constants';
 
-interface ConnectedProps {
-  tabKey?: UrlQueryValue;
-  navModel: NavModel;
-}
-
-interface DispatchProps {
-  updateLocation: typeof updateLocation;
-}
-
 const tabs = [
   {
-    label: Messages.tabs.alerts,
-    key: TabKeys.alerts,
+    title: Messages.tabs.alerts,
+    id: TabKeys.alerts,
+    path: `integrated-alerting/${TabKeys.alerts}`,
     component: <Alerts key={TabKeys.alerts} />,
   },
   {
-    label: Messages.tabs.alertRules,
-    key: TabKeys.alertRules,
+    title: Messages.tabs.alertRules,
+    id: TabKeys.alertRules,
+    path: `integrated-alerting/${TabKeys.alertRules}`,
     component: <AlertRules key={TabKeys.alertRules} />,
   },
   {
-    label: Messages.tabs.alertRuleTemplates,
-    key: TabKeys.alertRuleTemplates,
+    title: Messages.tabs.alertRuleTemplates,
+    id: TabKeys.alertRuleTemplates,
+    path: `integrated-alerting/${TabKeys.alertRuleTemplates}`,
     component: <AlertRuleTemplate key={TabKeys.alertRuleTemplates} />,
   },
   {
-    label: Messages.tabs.notificationChannels,
-    key: TabKeys.notificationChannels,
+    title: Messages.tabs.notificationChannels,
+    id: TabKeys.notificationChannels,
+    path: `integrated-alerting/${TabKeys.notificationChannels}`,
     component: <NotificationChannel key={TabKeys.notificationChannels} />,
   },
 ];
 
+const pageModel: PageModel = {
+  title: 'Integrated Alerting',
+  path: 'integrated-alerting',
+  id: 'integrated-alerting',
+  children: tabs.map(({ title, id, path }) => ({ title, id, path })),
+};
+
 const isValidTab = (tab: UrlQueryValue) => Object.values(TabKeys).includes(tab as TabKeys);
 
-const IntegratedAlertingPage: FC<DispatchProps & ConnectedProps> = ({ tabKey, navModel, updateLocation }) => {
+const IntegratedAlertingPage: FC = () => {
   const styles = useStyles(getStyles);
   const [activeTab, setActiveTab] = useState<TabKeys>(DEFAULT_TAB);
+  const tabKey = useSelector((state: StoreState) => state.location.routeParams.tab);
+  const { path: basePath } = pageModel;
 
   useEffect(() => {
     isValidTab(tabKey) || selectTab(DEFAULT_TAB);
@@ -58,61 +61,23 @@ const IntegratedAlertingPage: FC<DispatchProps & ConnectedProps> = ({ tabKey, na
     setActiveTab(isValidTab(tabKey) ? (tabKey as TabKeys) : DEFAULT_TAB);
   }, [tabKey]);
 
-  const selectTab = (tabKey: TabKeys) => {
-    updateLocation({ query: { tab: tabKey }, partial: true });
+  const selectTab = (tabKey: string) => {
+    getLocationSrv().update({
+      path: `${basePath}/${tabKey}`,
+    });
   };
 
   return (
     <div className={styles.integratedAlertingWrapper}>
+      <Breadcrumb pageModel={pageModel} />
       <TabsBar>
         {tabs.map(tab => (
-          <Tab key={tab.key} label={tab.label} active={tab.key === activeTab} onChangeTab={() => selectTab(tab.key)} />
+          <Tab key={tab.id} label={tab.title} active={tab.id === activeTab} onChangeTab={() => selectTab(tab.id)} />
         ))}
       </TabsBar>
-      <TabContent>{tabs.find(tab => tab.key === activeTab).component}</TabContent>
+      <TabContent>{tabs.find(tab => tab.id === activeTab).component}</TabContent>
     </div>
   );
 };
 
-const getChild = (tabKey: TabKeys, activeTab: TabKeys): NavModelItem => ({
-  active: activeTab === tabKey,
-  icon: '',
-  id: tabKey,
-  text: Messages.tabs[tabKey],
-  url: `integrated-alerting?tab=${tabKey}`,
-});
-
-export const getNavModel = (activeTab: TabKeys): NavModel => {
-  const main = {
-    icon: 'list-ul',
-    id: 'integrated-alerting',
-    text: '',
-    url: '',
-    subTitle: '',
-    breadcrumbs: [{ title: 'Integrated Alerting', url: 'integrated-alerting' }],
-    children: tabs.map(tab => getChild(tab.key, activeTab)),
-  };
-
-  return {
-    main,
-    node: main,
-  };
-};
-
-const mapStateToProps: MapStateToProps<ConnectedProps, {}, StoreState> = (state: StoreState) => {
-  const tabKey = state.location.query.tab;
-
-  console.log(state.navIndex);
-
-  return {
-    // navModel: getNavModel(state.navIndex, `integrated-alerting`),
-    navModel: getNavModel(tabKey as TabKeys),
-    tabKey,
-  };
-};
-
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = {
-  updateLocation,
-};
-
-export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(IntegratedAlertingPage));
+export default IntegratedAlertingPage;
