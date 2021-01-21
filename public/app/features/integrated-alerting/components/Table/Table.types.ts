58a68c355a9aa9296bd6432e64bb258fcb0af98a
Fabio Silva <ffjs1993@gmail.com>
2021-01-21 21:15:49 +0000

    PMM-7294 Extend TableInstance type

--- public/app/features/integrated-alerting/components/Table/Table.types.ts
+++ public/app/features/integrated-alerting/components/Table/Table.types.ts
@@ -1,8 +1,25 @@
-import { Column } from 'react-table';
+import { Column, TableInstance, TableState, Row } from 'react-table';
 
 export interface TableProps {
   data: object[];
   columns: Column[];
   pendingRequest?: boolean;
   emptyMessage?: string;
+}
+
+interface ExtendedTableState extends TableState {
+  pageIndex: number;
+  pageSize: number;
+}
+
+export interface ExtendedTableInstance extends TableInstance {
+  page: Row[];
+  canPreviousPage: boolean;
+  canNextPage: boolean;
+  gotoPage: (page: number) => void;
+  previousPage: () => void;
+  nextPage: () => void;
+  pageCount: number;
+  setPageSize: (size: number) => void;
+  state: ExtendedTableState;
 }
