a5f81cfd95e1efd7e8e37c7782ff04df1c2a8e4b
Fabio Silva <ffjs1993@gmail.com>
2021-01-25 23:07:04 +0000

    PMM-7324 Fix type on PagerDutyFields

--- public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/PagerDutyFields/PagerDutyFields.tsx
+++ public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/PagerDutyFields/PagerDutyFields.tsx
@@ -14,7 +14,7 @@ export const PagerDutyFields: FC<PagerDutyFieldsProps> = ({ values, mutators }) 
       <RadioButtonGroupField
         inputProps={{
           /**
-           * TODO We are relying on "onInput" being called after the form's state values are update.
+           * TODO We are relying on "onInput" being called after the form's state values are updated.
            * It works this way, but we should change platform-core to allow to pass values to the options.
            * That way, we can take the value from the event itself here and call the mutator.
            * Also, it then becomes easier to test this interaction.
