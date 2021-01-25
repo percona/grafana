d52a6685e00e072a0034a42f31e427998d757c33
Fabio Silva <ffjs1993@gmail.com>
2021-01-25 23:04:42 +0000

    PMM-7324 AddNotificationChannelModal tests

--- public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/AddNotificationChannelModal.test.tsx
+++ public/app/features/integrated-alerting/components/NotificationChannel/AddNotificationChannelModal/AddNotificationChannelModal.test.tsx
@@ -4,7 +4,6 @@ import { act } from 'react-dom/test-utils';
 import { dataQa } from '@percona/platform-core';
 import { AddNotificationChannelModal } from './AddNotificationChannelModal';
 import { TYPE_OPTIONS } from './AddNotificationChannel.constants';
-import { NotificationChannelType } from '../NotificationChannel.types';
 import { notificationChannelStubs } from '../__mocks__/notificationChannelStubs';
 
 jest.mock('../NotificationChannel.service');
@@ -16,41 +15,7 @@ jest.mock('app/core/app_events');
 const findFormButton = (wrapper: ReactWrapper) =>
   wrapper.find(dataQa('notification-channel-add-button')).find('button');
 
-/**
- * Given a type, return the matching element.
- * The element returned is the one that's actually clickable and that triggers the change
- */
-const findTypeOption = (wrapper: ReactWrapper, type: NotificationChannelType): ReactWrapper => {
-  const menuOptions = wrapper.find({ 'aria-label': 'Select option' });
-  const matchingOption = TYPE_OPTIONS.find(option => option.value === type);
-
-  return menuOptions.filterWhere(node => node.text() === matchingOption.label);
-};
-
-/**
- * Fill the form with a name and a notification channel type
- */
-const fillNameAndType = (wrapper: ReactWrapper, type: NotificationChannelType, name = 'John Doe') => {
-  const inputs = wrapper.find('input');
-  const nameInput = inputs.at(0);
-  const typeInput = inputs.at(1);
-
-  nameInput.simulate('change', {
-    target: {
-      value: name,
-    },
-  });
-
-  typeInput.simulate('keydown', {
-    key: 'ArrowDown',
-  });
-
-  const option = findTypeOption(wrapper, type);
-
-  option.simulate('click');
-};
-
-fdescribe('AddNotificationChannelModal', () => {
+describe('AddNotificationChannelModal', () => {
   it('should render modal with correct fields', () => {
     const wrapper = mount(<AddNotificationChannelModal setVisible={jest.fn()} isVisible />);
 
@@ -108,38 +73,5 @@ fdescribe('AddNotificationChannelModal', () => {
     const button = findFormButton(wrapper);
 
     expect(button.props().disabled).toBeTruthy();
-  });
-
-  describe('Pager Duty option', () => {
-    it('should have the submit button disabled if both routing and service keys are empty', () => {
-      const wrapper = mount(<AddNotificationChannelModal setVisible={jest.fn()} isVisible />);
-
-      fillNameAndType(wrapper, NotificationChannelType.pagerDuty);
-
-      const button = findFormButton(wrapper);
-      expect(button.props().disabled).toBeTruthy();
-    });
-
-    it('should enable submit button if only routing key is provided', () => {
-      const wrapper = mount(<AddNotificationChannelModal setVisible={jest.fn()} isVisible />);
-      fillNameAndType(wrapper, NotificationChannelType.pagerDuty);
-
-      const routingKeyInput = wrapper.find(dataQa('routing-text-input')).at(0);
-      routingKeyInput.simulate('change', { target: { value: 'Sample routing key' } });
-
-      const button = findFormButton(wrapper);
-      expect(button.props().disabled).toBeFalsy();
-    });
-
-    it('should enable submit button if only service key is provided', () => {
-      const wrapper = mount(<AddNotificationChannelModal setVisible={jest.fn()} isVisible />);
-      fillNameAndType(wrapper, NotificationChannelType.pagerDuty);
-
-      const serviceKeyInput = wrapper.find(dataQa('service-text-input')).at(0);
-      serviceKeyInput.simulate('change', { target: { value: 'Sample service key' } });
-
-      const button = findFormButton(wrapper);
-      expect(button.props().disabled).toBeFalsy();
-    });
   });
 });
