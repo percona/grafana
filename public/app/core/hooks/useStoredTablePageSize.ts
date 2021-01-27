b6b6800ca1516bd9a8240b76ef83c8092c47f1f3
Fabio Silva <ffjs1993@gmail.com>
2021-01-27 15:53:55 +0000

    PMM-7294 useStoredTablePageSize hook

---
+++ public/app/core/hooks/useStoredTablePageSize.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { logger } from '@percona/platform-core';
+
+export const useStoredTablePageSize = (tableHash: string) => {
+  let pageSize = 1;
+  const fullHash = `${tableHash}-table-page-size`;
+
+  try {
+    const storedValue = +localStorage.getItem(fullHash);
+
+    if (storedValue && !isNaN(storedValue)) {
+      pageSize = storedValue;
+    }
+  } catch (e) {
+    logger.error(e);
+  }
+  const [value, setValue] = useState(pageSize);
+
+  useEffect(() => {
+    localStorage.setItem(fullHash, `${value}`);
+  }, [value]);
+
+  return [value, setValue] as const;
+};
