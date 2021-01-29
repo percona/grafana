f04cadf4656bf2c517d11b3ad3c826e3bd14eff3
Fabio Silva <ffjs1993@gmail.com>
2021-01-29 16:42:49 +0000

    PMM-7294 Add more tests to Pagination

--- public/app/features/integrated-alerting/components/Table/Pagination/Pagination.test.tsx
+++ public/app/features/integrated-alerting/components/Table/Pagination/Pagination.test.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import { dataQa } from '@percona/platform-core';
 import { Pagination } from './Pagination';
+import { Messages } from './Pagination.messages';
 
 const simulateNextClick = (wrapper: ShallowWrapper, numberOfClicks: number) => {
   for (let i = 0; i < numberOfClicks; i++) {
     wrapper.find(dataQa('next-page-button')).simulate('click');
+  }
+};
+
+const simulatePreviousClick = (wrapper: ShallowWrapper, numberOfClicks: number) => {
+  for (let i = 0; i < numberOfClicks; i++) {
+    wrapper.find(dataQa('previous-page-button')).simulate('click');
   }
 };
 
@@ -38,8 +45,7 @@ describe('Pagination', () => {
     const wrapper = shallow(
       <Pagination totalItems={30} pageCount={3} pageSizeOptions={[]} pageSize={10} nrRowsOnCurrentPage={10} />
     );
-    const nextPageButton = wrapper.find(dataQa('next-page-button'));
-    nextPageButton.simulate('click');
+    simulateNextClick(wrapper, 1);
     expect(wrapper.find(dataQa('previous-page-button')).props().disabled).toBeFalsy();
     expect(wrapper.find(dataQa('first-page-button')).props().disabled).toBeFalsy();
     expect(wrapper.find(dataQa('next-page-button')).props().disabled).toBeFalsy();
@@ -169,7 +175,7 @@ describe('Pagination', () => {
     );
     // There's 5 pages and 3 pages/view, meaning two clicks will bring the last page button into the view
     // After that, any click should move the active page button towards the end, instead of keeping in the center
-    // That means that with 4 clicks, we should have page 5 selected in the right
+    // That means that with 4 clicks, we should have page 5 selected on the right
     simulateNextClick(wrapper, 4);
     expect(
       wrapper
@@ -183,5 +189,109 @@ describe('Pagination', () => {
         .at(2)
         .prop('variant')
     ).toBe('primary');
+  });
+
+  it('should correctly show the items interval being shown', () => {
+    const wrapper = shallow(
+      <Pagination
+        pagesPerView={3}
+        totalItems={15}
+        pageCount={5}
+        pageSizeOptions={[]}
+        pageSize={3}
+        nrRowsOnCurrentPage={3}
+      />
+    );
+    expect(wrapper.find(dataQa('pagination-items-inverval')).text()).toBe(Messages.ITEMS_INTERVAL(1, 3, 15));
+    simulateNextClick(wrapper, 2);
+    expect(wrapper.find(dataQa('pagination-items-inverval')).text()).toBe(Messages.ITEMS_INTERVAL(7, 9, 15));
+  });
+
+  it('should trigger a page change', () => {
+    const cb = jest.fn();
+    const wrapper = shallow(
+      <Pagination
+        pagesPerView={3}
+        totalItems={15}
+        pageCount={5}
+        pageSizeOptions={[]}
+        pageSize={3}
+        nrRowsOnCurrentPage={3}
+        onPageChange={cb}
+      />
+    );
+    simulateNextClick(wrapper, 1);
+    expect(cb).toBeCalledWith(1);
+  });
+
+  it('should not trigger a page change on first page and previous is clicked', () => {
+    const cb = jest.fn();
+    const wrapper = shallow(
+      <Pagination
+        pagesPerView={3}
+        totalItems={15}
+        pageCount={5}
+        pageSizeOptions={[]}
+        pageSize={3}
+        nrRowsOnCurrentPage={3}
+        onPageChange={cb}
+      />
+    );
+    simulatePreviousClick(wrapper, 1);
+    expect(cb).not.toHaveBeenCalled();
+  });
+
+  it('should not trigger a page change if on last page and next is clicked', () => {
+    const cb = jest.fn();
+    const wrapper = shallow(
+      <Pagination
+        pagesPerView={3}
+        totalItems={15}
+        pageCount={5}
+        pageSizeOptions={[]}
+        pageSize={3}
+        nrRowsOnCurrentPage={3}
+        onPageChange={cb}
+      />
+    );
+    simulateNextClick(wrapper, 5);
+    expect(cb).toHaveBeenCalledTimes(4);
+  });
+
+  it('should jump to last page', () => {
+    const wrapper = shallow(
+      <Pagination
+        pagesPerView={3}
+        totalItems={15}
+        pageCount={5}
+        pageSizeOptions={[]}
+        pageSize={3}
+        nrRowsOnCurrentPage={3}
+      />
+    );
+    wrapper.find(dataQa('last-page-button')).simulate('click');
+
+    const activePageButton = wrapper.find(dataQa('page-button')).last();
+    expect(activePageButton.prop('variant')).toBe('primary');
+    expect(activePageButton.text()).toBe('5');
+  });
+
+  it('should jump to first page', () => {
+    const wrapper = shallow(
+      <Pagination
+        pagesPerView={3}
+        totalItems={15}
+        pageCount={5}
+        pageSizeOptions={[]}
+        pageSize={3}
+        nrRowsOnCurrentPage={3}
+      />
+    );
+    simulateNextClick(wrapper, 5);
+    wrapper.find(dataQa('first-page-button')).simulate('click');
+
+    const activePageButton = wrapper.find(dataQa('page-button')).first();
+    expect(activePageButton.prop('variant')).toBe('primary');
+    expect(activePageButton.text()).toBe('1');
   });
 });
