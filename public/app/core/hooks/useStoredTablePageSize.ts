11b02266a05f03a403afca880a3b3bc92ed171dd
Fabio Silva <ffjs1993@gmail.com>
2021-02-03 09:50:04 +0000

    PMM-7294 move getProperPageSize to hook

--- public/app/core/hooks/useStoredTablePageSize.ts
+++ public/app/core/hooks/useStoredTablePageSize.ts
@@ -1,5 +1,24 @@
 import { useEffect, useState } from 'react';
 import { logger } from '@percona/platform-core';
+import { SelectableValue } from '@grafana/data';
+
+export const PAGE_SIZES: Array<SelectableValue<number>> = [
+  {
+    label: '25',
+    value: 25,
+  },
+  {
+    label: '50',
+    value: 50,
+  },
+  {
+    label: '100',
+    value: 100,
+  },
+];
+
+const getProperPageSize = (pageSize: number) =>
+  PAGE_SIZES.find(p => p.value === pageSize) ? pageSize : PAGE_SIZES[0].value;
 
 export const useStoredTablePageSize = (tableHash: string) => {
   let pageSize = 1;
@@ -14,7 +33,7 @@ export const useStoredTablePageSize = (tableHash: string) => {
   } catch (e) {
     logger.error(e);
   }
-  const [value, setValue] = useState(pageSize);
+  const [value, setValue] = useState(getProperPageSize(pageSize));
 
   useEffect(() => {
     if (tableHash) {
