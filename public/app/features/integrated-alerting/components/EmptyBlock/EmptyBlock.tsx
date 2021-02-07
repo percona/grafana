8d73a64fc323c25152a4566a3a281b9749b0c090
Fabio Silva <ffjs1993@gmail.com>
2021-02-07 19:14:48 +0000

    PMM-7259 Create EmptyBlockFields

--- public/app/features/integrated-alerting/components/EmptyBlock/EmptyBlock.tsx
+++ public/app/features/integrated-alerting/components/EmptyBlock/EmptyBlock.tsx
@@ -2,8 +2,9 @@ import { useStyles } from '@grafana/ui';
 import React from 'react';
 import { FC } from 'react';
 import { getStyles } from './EmptyBlock.styles';
+import { EmptyBlockFields } from './EmptyBlock.types';
 
-export const EmptyBlock: FC<{ dataQa?: string }> = ({ children, dataQa }) => {
+export const EmptyBlock: FC<EmptyBlockFields> = ({ children, dataQa }) => {
   const style = useStyles(getStyles);
 
   return (
---
+++ public/app/features/integrated-alerting/components/EmptyBlock/EmptyBlock.types.ts
@@ -0,0 +1,3 @@
+export interface EmptyBlockFields {
+  dataQa?: string;
+}
