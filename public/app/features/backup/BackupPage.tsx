2e6a6883f210f7d33f659658dde1640b4ff6a7ea
Fabio Silva <ffjs1993@gmail.com>
2021-02-10 15:29:49 +0000

    PMM-7449 add backup tabs

--- public/app/features/backup/BackupPage.tsx
+++ public/app/features/backup/BackupPage.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
-import { FC } from 'react';
+import { Tab, TabContent, TabsBar, useStyles } from '@grafana/ui';
+import React, { FC, useState } from 'react';
+import { Messages } from './Backup.messages';
+import { TabKeys } from './Backup.types';
+import { getStyles } from './Backup.styles';
 
-const BackupPage: FC = () => <span>HEY</span>;
+const BackupPage: FC = () => {
+  const [activeTab, setActiveTab] = useState(TabKeys.locations);
+  const tabs = [
+    {
+      label: Messages.tabs.locations,
+      key: TabKeys.locations,
+      component: <span>HEY</span>,
+    },
+  ];
+  const styles = useStyles(getStyles);
+
+  return (
+    <div className={styles.backupWrapper}>
+      <TabsBar>
+        {tabs.map(tab => (
+          <Tab
+            key={tab.key}
+            label={tab.label}
+            active={tab.key === activeTab}
+            onChangeTab={() => setActiveTab(tab.key)}
+          />
+        ))}
+      </TabsBar>
+      <TabContent>{tabs.find(tab => tab.key === activeTab).component}</TabContent>
+    </div>
+  );
+};
 
 export default BackupPage;
