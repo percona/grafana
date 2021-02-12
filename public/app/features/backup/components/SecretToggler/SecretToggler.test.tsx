733f9ead49eb0b2bc1122337e25e1a7737c3f3c9
Fabio Silva <ffjs1993@gmail.com>
2021-02-12 22:15:58 +0000

    PMM-7449 add tests to SecretToggler

---
+++ public/app/features/backup/components/SecretToggler/SecretToggler.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { SecretToggler } from './SecretToggler';
+import { Icon } from '@grafana/ui';
+
+describe('SecretToggler', () => {
+  it('should render hidden characters by default', () => {
+    const wrapper = shallow(<SecretToggler secret="secret" />);
+
+    expect(wrapper.find('input').prop('type')).toBe('password');
+  });
+
+  it('should show the unlock icon when not showing text', () => {
+    const wrapper = shallow(<SecretToggler secret="secret" />);
+
+    expect(wrapper.find(Icon).prop('name')).toBe('unlock');
+  });
+
+  it('should reveal the secret when the lock is clicked', () => {
+    const wrapper = shallow(<SecretToggler secret="secret" />);
+
+    wrapper.find(Icon).simulate('click');
+    expect(wrapper.find(Icon).prop('name')).toBe('lock');
+    expect(wrapper.find('input').prop('type')).toBe('text');
+  });
+
+  it('should have the input always disabled', () => {
+    const wrapper = shallow(<SecretToggler secret="secret" />);
+
+    expect(wrapper.find('input').prop('disabled')).toBeTruthy();
+  });
+});
