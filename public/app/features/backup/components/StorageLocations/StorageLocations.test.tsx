64ed95bda1afa9cb31b23eccf169cf1fd0818cbb
Fabio Silva <ffjs1993@gmail.com>
2021-02-16 21:08:29 +0000

    PMM-7450 StorageLocations: test modal opening

--- public/app/features/backup/components/StorageLocations/StorageLocations.test.tsx
+++ public/app/features/backup/components/StorageLocations/StorageLocations.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
+import { mount, ReactWrapper, shallow } from 'enzyme';
+import { dataQa } from '@percona/platform-core';
 import { act } from 'react-dom/test-utils';
 import { Table } from 'app/features/integrated-alerting/components/Table/Table';
 import { StorageLocations } from './StorageLocations';
+import { AddStorageLocationModal } from './AddStorageLocationModal';
 
 const stubResponseArray = ['this is stubbed'];
 jest.mock('./StorageLocations.service');
@@ -19,5 +21,13 @@ describe('StorageLocations', () => {
     wrapper.update();
 
     expect(wrapper.find(Table).prop('data')).toEqual(stubResponseArray);
+  });
+
+  it('should open the modal by clicking the "Add" button', () => {
+    const wrapper = shallow(<StorageLocations />);
+
+    expect(wrapper.find(AddStorageLocationModal).prop('isVisible')).toBeFalsy();
+    wrapper.find(dataQa('storage-location-add-modal-button')).simulate('click');
+    expect(wrapper.find(AddStorageLocationModal).prop('isVisible')).toBeTruthy();
   });
 });
