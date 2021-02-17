7ca48bb007fe20ec5d28c5477ff3ff3314356716
Tiago Mota Santos <tiago.mota@percona.com>
2021-02-17 17:51:29 +0000

    PMM-7419 DB Cluster logs

---
+++ public/app/percona/dbaas/components/DBCluster/DBClusterLogsModal/DBClusterLogsModal.types.ts
@@ -0,0 +1,11 @@
+import { DBCluster, DBClusterPodLogs } from '../DBCluster.types';
+
+export interface DBClusterLogsModalProps {
+  dbCluster: DBCluster;
+  isVisible: boolean;
+  setVisible: (value: boolean) => void;
+}
+
+export interface DBClusterLogsMap {
+  [key: string]: DBClusterPodLogs;
+}
