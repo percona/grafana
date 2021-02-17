1ea3e9a4fb22e254130d4c8be3fedf7b7d3f9e25
Fabio Silva <ffjs1993@gmail.com>
2021-02-17 10:18:11 +0000

    PMM-7450 add tests to AddStorageLocation.utils

---
+++ public/app/features/backup/components/StorageLocations/AddStorageLocationModal/AddStorageLocation.utils.test.ts
@@ -0,0 +1,136 @@
+import { LocationType, S3Location, StorageLocation } from '../StorageLocations.types';
+import { toFormStorageLocation, toStorageLocation } from './AddStorageLocation.utils';
+import { AddStorageLocationFormProps, FormStorageType } from './AddStorageLocationModal.types';
+
+describe('AddStorageLocationUtils', () => {
+  describe('toFormStorageLocation', () => {
+    it('should return an empty S3 object by default', () => {
+      expect(toFormStorageLocation(null)).toEqual({
+        name: '',
+        description: '',
+        type: FormStorageType.S3,
+        endpoint: '',
+        client: '',
+        server: '',
+        accessKey: '',
+        secretKey: '',
+      });
+    });
+
+    it('should convert an S3Location', () => {
+      const location: S3Location = {
+        name: 'S3',
+        description: 'S3 location',
+        path: 's3://foo',
+        type: LocationType.s3,
+        accessKey: 'accessKey',
+        secretKey: 'secretKey',
+      };
+
+      expect(toFormStorageLocation(location)).toEqual({
+        name: location.name,
+        description: location.description,
+        type: FormStorageType.S3,
+        endpoint: location.path,
+        accessKey: location.accessKey,
+        secretKey: location.secretKey,
+      });
+    });
+
+    it('should convert a local client location', () => {
+      const location: StorageLocation = {
+        name: 'Local Client',
+        description: 'Client location',
+        path: '/foo/bar',
+        type: LocationType.localClient,
+      };
+
+      expect(toFormStorageLocation(location)).toEqual({
+        name: location.name,
+        description: location.description,
+        type: FormStorageType.CLIENT,
+        client: location.path,
+      });
+    });
+
+    it('should convert a local server location', () => {
+      const location: StorageLocation = {
+        name: 'Local Server',
+        description: 'Server location',
+        path: '/foo/bar',
+        type: LocationType.localServer,
+      };
+
+      expect(toFormStorageLocation(location)).toEqual({
+        name: location.name,
+        description: location.description,
+        type: FormStorageType.SERVER,
+        server: location.path,
+      });
+    });
+  });
+
+  describe('toStorageLocation', () => {
+    it('should convert an S3 location', () => {
+      const location: AddStorageLocationFormProps = {
+        name: 'S3 Location',
+        description: 'location',
+        type: FormStorageType.S3,
+        endpoint: 's3://foo',
+        client: '',
+        server: '',
+        accessKey: 'accessKey',
+        secretKey: 'secretKey',
+      };
+
+      expect(toStorageLocation(location)).toEqual({
+        name: location.name,
+        description: location.description,
+        type: LocationType.s3,
+        path: location.endpoint,
+        accessKey: location.accessKey,
+        secretKey: location.secretKey,
+      });
+    });
+
+    it('should convert a local client location', () => {
+      const location: AddStorageLocationFormProps = {
+        name: 'Client Location',
+        description: 'client',
+        type: FormStorageType.CLIENT,
+        endpoint: '',
+        client: '/foo/bar',
+        server: '',
+        accessKey: '',
+        secretKey: '',
+      };
+
+      expect(toStorageLocation(location)).toEqual({
+        name: location.name,
+        description: location.description,
+        type: LocationType.localClient,
+        path: location.client,
+      });
+    });
+
+    it('should convert a local server location', () => {
+      const location: AddStorageLocationFormProps = {
+        name: 'Server Location',
+        description: 'server',
+        type: FormStorageType.SERVER,
+        endpoint: '',
+        client: '',
+        server: '/foo/bar',
+        accessKey: '',
+        secretKey: '',
+      };
+
+      expect(toStorageLocation(location)).toEqual({
+        name: location.name,
+        description: location.description,
+        type: LocationType.localServer,
+        path: location.server,
+      });
+    });
+  });
+});
