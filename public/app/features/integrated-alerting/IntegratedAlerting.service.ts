ce430cfab61441f92b067c034d7417748f314b5c
Roman Misyurin <roman.misyurin@percona.com>
2021-02-18 20:27:50 +0300

    PMM-7550 fix request urls

--- public/app/features/integrated-alerting/IntegratedAlerting.service.ts
+++ public/app/features/integrated-alerting/IntegratedAlerting.service.ts
@@ -3,7 +3,7 @@ import { api } from 'app/percona/shared/helpers/api';
 const BASE_URL = `/v1/Settings`;
 
 export const IntegratedAlertingService = {
-  async getSettings() {
+  async getSettings(): Promise<any> {
     return api.post(`${BASE_URL}/Get`, {});
   },
 };
