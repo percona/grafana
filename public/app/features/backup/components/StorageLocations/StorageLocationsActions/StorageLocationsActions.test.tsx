accf1ae696fa125f45e8e77ff9cd84e332cf8a66
Fabio Silva <ffjs1993@gmail.com>
2021-02-18 15:17:22 +0000

    PMM-7455 more tests to StorageLocationsActions

--- public/app/features/backup/components/StorageLocations/StorageLocationsActions/StorageLocationsActions.test.tsx
+++ public/app/features/backup/components/StorageLocations/StorageLocationsActions/StorageLocationsActions.test.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { DBIcon } from '../../DBIcon';
 import { StorageLocationsActions } from './StorageLocationsActions';
-import { StorageLocation } from '../StorageLocations.types';
+import { LocationType, StorageLocation } from '../StorageLocations.types';
 
 describe('StorageLocationsActions', () => {
   it('should have DBIcon', () => {
-    const wrapper = shallow(<StorageLocationsActions location={(null as unknown) as StorageLocation} />);
+    const wrapper = shallow(
+      <StorageLocationsActions onUpdate={jest.fn()} location={(null as unknown) as StorageLocation} />
+    );
     expect(wrapper.find(DBIcon).exists()).toBeTruthy();
+  });
+
+  it('should call onUpdate', () => {
+    const location: StorageLocation = {
+      locationID: 'Location1',
+      name: 'location_1',
+      description: 'first location',
+      type: LocationType.S3,
+      path: 's3://foo',
+    };
+    const onUpdate = jest.fn();
+    const wrapper = shallow(<StorageLocationsActions onUpdate={onUpdate} location={location} />);
+    wrapper
+      .find(DBIcon)
+      .first()
+      .simulate('click');
+
+    expect(onUpdate).toHaveBeenCalledWith(location);
   });
 });
