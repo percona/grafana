88aadb4bb058067aeae6da7585928613ce2eeee4
Fabio Silva <ffjs1993@gmail.com>
2021-02-19 15:00:41 +0000

    PMM-7449 vertically align description and keys

--- public/app/features/backup/components/KeysBlock/KeysBlock.tsx
+++ public/app/features/backup/components/KeysBlock/KeysBlock.tsx
@@ -9,7 +9,7 @@ export const KeysBlock: FC<KeysBlockProps> = ({ accessKey, secretKey }) => {
   const styles = useStyles(getStyles);
 
   return (
-    <div>
+    <div className={styles.keysWrapper}>
       <div data-qa="access-key">
         <span className={styles.keyLabel}>{Messages.accessKey}</span>
         {accessKey}
