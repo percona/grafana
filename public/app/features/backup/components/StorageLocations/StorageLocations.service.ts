b346db8f3c021784b1af87fe151af94522e4452f
Fabio Silva <ffjs1993@gmail.com>
2021-02-21 11:47:08 +0000

    PMM-7453 add remove method

--- public/app/features/backup/components/StorageLocations/StorageLocations.service.ts
+++ public/app/features/backup/components/StorageLocations/StorageLocations.service.ts
@@ -1,10 +1,14 @@
 import { getBackendSrv } from '@grafana/runtime';
+import { api } from 'app/percona/shared/helpers/api';
 import { StorageLocationListReponse } from './StorageLocations.types';
 
-const BASE_URL = `${window.location.origin}/v1/management/backup/Locations`;
+const BASE_URL = '/v1/management/backup/Locations';
 
 export const StorageLocationsService = {
   async list(): Promise<StorageLocationListReponse> {
     return getBackendSrv().post(`${BASE_URL}/List`);
+  },
+  async delete(locationID: string): Promise<void> {
+    api.post(`${BASE_URL}/Remove`, { locationID });
   },
 };
