806491358f25c34a02fd7b7cf28855a2a1eb0ab1
Fabio Silva <ffjs1993@gmail.com>
2021-02-21 13:58:33 +0000

    PMM-7449 change API requests to API helper

--- public/app/features/backup/components/StorageLocations/StorageLocations.service.ts
+++ public/app/features/backup/components/StorageLocations/StorageLocations.service.ts
@@ -1,10 +1,10 @@
-import { getBackendSrv } from '@grafana/runtime';
+import { api } from 'app/percona/shared/helpers/api';
 import { StorageLocationListReponse } from './StorageLocations.types';
 
-const BASE_URL = `${window.location.origin}/v1/management/backup/Locations`;
+const BASE_URL = '/v1/management/backup/Locations';
 
 export const StorageLocationsService = {
   async list(): Promise<StorageLocationListReponse> {
-    return getBackendSrv().post(`${BASE_URL}/List`);
+    return api.post(`${BASE_URL}/List`, {});
   },
 };
