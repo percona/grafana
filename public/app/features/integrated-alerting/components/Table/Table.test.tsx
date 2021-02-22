bd1e32d24e89c8ede7c3444f6a450d7f08c181d2
Fabio Silva <ffjs1993@gmail.com>
2021-02-22 15:31:59 +0000

    PMM-7461 correct tests

--- public/app/features/integrated-alerting/components/Table/Table.test.tsx
+++ public/app/features/integrated-alerting/components/Table/Table.test.tsx
@@ -51,7 +51,6 @@ describe('Table', () => {
     );
 
     expect(wrapper.find(dataQa('table-loading'))).toHaveLength(1);
-    expect(wrapper.find(dataQa('table'))).toHaveLength(0);
     expect(wrapper.find(dataQa('table-no-data'))).toHaveLength(0);
   });
 
