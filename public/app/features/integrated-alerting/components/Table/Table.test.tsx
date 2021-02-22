cfa20b7ef6da5e0acd1b8a0161dc472721f4e490
Fabio Silva <ffjs1993@gmail.com>
2021-02-22 15:50:26 +0000

    PMM-7461 test data is beneath loading

--- public/app/features/integrated-alerting/components/Table/Table.test.tsx
+++ public/app/features/integrated-alerting/components/Table/Table.test.tsx
@@ -51,6 +51,7 @@ describe('Table', () => {
     );
 
     expect(wrapper.find(dataQa('table-loading'))).toHaveLength(1);
+    expect(wrapper.find(dataQa('table'))).toHaveLength(1);
     expect(wrapper.find(dataQa('table-no-data'))).toHaveLength(0);
   });
 
