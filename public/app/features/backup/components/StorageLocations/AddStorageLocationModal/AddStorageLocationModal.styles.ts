9a76189a50985234575f80ea819c4b25ca0a5e1e
Fabio Silva <ffjs1993@gmail.com>
2021-02-25 15:39:26 +0000

    PMM-7452 change focus style on test button

--- public/app/features/backup/components/StorageLocations/AddStorageLocationModal/AddStorageLocationModal.styles.ts
+++ public/app/features/backup/components/StorageLocations/AddStorageLocationModal/AddStorageLocationModal.styles.ts
@@ -9,7 +9,8 @@ export const getStyles = () => ({
     background: linear-gradient(180deg, #04be5b 0%, #04b155 100%);
     border: 1px solid #058943;
 
-    &:hover {
+    &:hover,
+    &:focus {
       background: #04be5b;
     }
   `,
