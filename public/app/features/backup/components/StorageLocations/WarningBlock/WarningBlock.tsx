66a1fcea519e331a2ba3ce36b3c9d041ae890d2c
Fabio Silva <ffjs1993@gmail.com>
2021-02-26 10:19:14 +0000

    PMM-7375 different icon WarningBlock

--- public/app/features/backup/components/StorageLocations/WarningBlock/WarningBlock.tsx
+++ public/app/features/backup/components/StorageLocations/WarningBlock/WarningBlock.tsx
@@ -1,14 +1,19 @@
 import React, { FC } from 'react';
-import { Icon, useStyles } from '@grafana/ui';
-import { WarningBlockProps } from './WarningBlock.types';
+import { Icon, IconName, useStyles } from '@grafana/ui';
+import { WarningBlockProps, WarningType } from './WarningBlock.types';
 import { getStyles } from './WarningBlock.styles';
 
-export const WarningBlock: FC<WarningBlockProps> = ({ message }) => {
+const WarningIconMap: Record<WarningType, IconName> = {
+  info: 'info-circle',
+  warning: 'exclamation-triangle',
+};
+
+export const WarningBlock: FC<WarningBlockProps> = ({ message, type = 'info' }) => {
   const styles = useStyles(getStyles);
 
   return (
     <div className={styles.warningWrapper}>
-      <Icon className={styles.warningIcon} size="xl" name="info-circle" />
+      <Icon className={styles.warningIcon} size="xl" name={WarningIconMap[type]} />
       <span>{message}</span>
     </div>
   );
