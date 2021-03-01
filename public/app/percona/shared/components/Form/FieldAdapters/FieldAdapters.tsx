865ddf2f78c50c8c8355d6af5320aeaa8f881606
Roman Misyurin <roman.misyurin@percona.com>
2021-03-01 15:13:43 +0300

    PMM-7556 fix TS issues

--- public/app/percona/shared/components/Form/FieldAdapters/FieldAdapters.tsx
+++ public/app/percona/shared/components/Form/FieldAdapters/FieldAdapters.tsx
@@ -5,49 +5,6 @@ import { Input, Select, TextArea, useTheme } from '@grafana/ui';
 import { getStyles } from './FieldAdapters.styles';
 import { Field } from './Field';
 
-export const InputFieldAdapter = ({ input, className, label, meta, ...props }) => {
-  const theme = useTheme();
-  const styles = getStyles(theme);
-
-  return (
-    <Field label={label}>
-      <>
-        <Input
-          {...input}
-          {...props}
-          className={cx(className, { invalid: meta.touched && meta.error })}
-          title={meta.touched ? meta.error : ''}
-        />
-        <div data-qa="input-field-error-message" className={styles.errorMessage}>
-          {meta.touched && meta.error}
-        </div>
-      </>
-    </Field>
-  );
-};
-
-export const TextAreaAdapter = ({ input, className, label, meta, ...props }) => {
-  const theme = useTheme();
-  const styles = getStyles(theme);
-
-  return (
-    <Field label={label}>
-      <>
-        <TextArea
-          {...input}
-          {...props}
-          rows={8}
-          title={meta.touched ? meta.error : ''}
-          className={cx(className, { invalid: meta.touched && meta.error })}
-        />
-        <div data-qa="textarea-field-error-message" className={styles.errorMessage}>
-          {meta.touched && meta.error}
-        </div>
-      </>
-    </Field>
-  );
-};
-
 export const SelectFieldAdapter = ({ input, className, options, label, meta, dataQa, noOptionsMessage, ...props }) => {
   const theme = useTheme();
   const styles = getStyles(theme);
