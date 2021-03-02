5cd7dc5545ce3991fbcd035a5ffc2b9deef0e129
Fabio Silva <ffjs1993@gmail.com>
2021-03-02 21:47:59 +0000

    PMM-7387 add moment formats to constants

--- public/app/features/backup/components/BackupInventory/BackupCreation/BackupCreation.tsx
+++ public/app/features/backup/components/BackupInventory/BackupCreation/BackupCreation.tsx
@@ -3,12 +3,13 @@ import { useStyles } from '@grafana/ui';
 import moment from 'moment/moment';
 import { BackupCreationProps } from './BackupCreation.types';
 import { getStyles } from './BackupCreation.styles';
+import { DAY_FORMAT, HOUR_FORMAT } from './BackupCreation.constants';
 
 export const BackupCreation: FC<BackupCreationProps> = ({ date }) => {
   const styles = useStyles(getStyles);
   const momentObj = moment(date);
-  const dayTime = momentObj.format('YYYY[-]MM[-]DD');
-  const hourTime = momentObj.format('HH[:]mm[:]ss');
+  const dayTime = momentObj.format(DAY_FORMAT);
+  const hourTime = momentObj.format(HOUR_FORMAT);
 
   return (
     <div data-qa="backup-creation">
