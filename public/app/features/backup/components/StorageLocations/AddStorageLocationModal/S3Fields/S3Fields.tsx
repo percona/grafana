dedf80f2db363674065c9ae56a6726237118e388
Fabio Silva <ffjs1993@gmail.com>
2021-03-04 11:14:11 +0000

    PMM-7455 add S3 constraints

--- public/app/features/backup/components/StorageLocations/AddStorageLocationModal/S3Fields/S3Fields.tsx
+++ public/app/features/backup/components/StorageLocations/AddStorageLocationModal/S3Fields/S3Fields.tsx
@@ -6,6 +6,8 @@ import { MAX_BUCKET_LENGTH, MAX_ACCESS_KEY_LENGTH, MAX_SECRET_LENGTH } from './S
 import { SecretToggler } from '../../../SecretToggler';
 
 const required = [validators.required];
+const bucketValidators = [validators.required, validators.minLength(3)];
+const accessKeyValidators = [validators.required, validators.minLength(16)];
 
 export const S3Fields: FC<S3FieldsProps> = ({ endpoint, accessKey, secretKey, bucketName }) => (
   <>
@@ -14,14 +16,14 @@ export const S3Fields: FC<S3FieldsProps> = ({ endpoint, accessKey, secretKey, bu
       inputProps={{ maxLength: MAX_BUCKET_LENGTH }}
       name="bucketName"
       label={Messages.bucketName}
-      validators={required}
+      validators={bucketValidators}
       initialValue={bucketName}
     />
     <TextInputField
       inputProps={{ maxLength: MAX_ACCESS_KEY_LENGTH }}
       name="accessKey"
       label={Messages.accessKey}
-      validators={required}
+      validators={accessKeyValidators}
       initialValue={accessKey}
     />
     <SecretToggler
