db4cbfa7e909f2cbc2ac560c9d3197abf8672d22
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-03-09 11:22:19 +0100

    PMM-7451 add seconds to hours conversion

--- public/app/percona/settings/components/Advanced/Advanced.utils.ts
+++ public/app/percona/settings/components/Advanced/Advanced.utils.ts
@@ -1,6 +1,7 @@
-import { HOURS, MINUTES_IN_HOUR, SECONDS_IN_DAY } from './Advanced.constants';
+import { HOURS, MINUTES_IN_HOUR, SECONDS_IN_HOUR, SECONDS_IN_DAY } from './Advanced.constants';
+import { SttCheckIntervalsSettings } from 'app/percona/settings/Settings.types';
 
-export const transformSecondsToDays = (dataRetention: string) => {
+export const convertSecondsToDays = (dataRetention: string) => {
   const [count, units] = [+dataRetention.slice(0, -1), dataRetention.slice(-1)];
 
   switch (units) {
@@ -13,4 +14,19 @@ export const transformSecondsToDays = (dataRetention: string) => {
     default:
       return '';
   }
+};
+
+export const convertSecondsStringToHour = (secondsString: string) => parseInt(secondsString, 10) / SECONDS_IN_HOUR;
+
+export const convertSecondsToHours = (sttCheckIntervals: SttCheckIntervalsSettings) => {
+  const {
+    rareInterval: rawRareInterval,
+    standardInterval: rawStandardInterval,
+    frequentInterval: rawFrequentInterval,
+  } = sttCheckIntervals;
+  return {
+    rareInterval: convertSecondsStringToHour(rawRareInterval),
+    standardInterval: convertSecondsStringToHour(rawStandardInterval),
+    frequentInterval: convertSecondsStringToHour(rawFrequentInterval),
+  };
 };
