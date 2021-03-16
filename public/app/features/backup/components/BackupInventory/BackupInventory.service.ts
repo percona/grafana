a3b05e1a66642d152a58439a0de319b4c2b02b3b
Fabio Silva <ffjs1993@gmail.com>
2021-03-16 16:47:36 +0000

    PMM-7387 change BASE_URL

--- public/app/features/backup/components/BackupInventory/BackupInventory.service.ts
+++ public/app/features/backup/components/BackupInventory/BackupInventory.service.ts
@@ -1,34 +1,54 @@
 import { api } from 'app/percona/shared/helpers/api';
-import { Backup, BackupResponse } from './BackupInventory.types';
+import { Backup, BackupResponse, DataModel, Status } from './BackupInventory.types';
 
-const BASE_URL = '/v1/management/backup/Backups';
+const BASE_URL = '/v1/management/backup/Artifacts';
 
 export const BackupInventoryService = {
   async list(): Promise<Backup[]> {
-    return api.post<BackupResponse, any>(`${BASE_URL}/List`, {}).then(({ backups = [] }) =>
-      backups.map(
+    return api
+      .post<BackupResponse, any>(`${BASE_URL}/List`, {})
+      .then(
         ({
-          backup_id,
-          name,
-          location_id,
-          location_name,
-          created_at,
-          service_id,
-          service_name,
-          data_model,
-          status,
-        }): Backup => ({
-          id: backup_id,
-          name,
-          created: new Date(created_at).getTime(),
-          locationId: location_id,
-          locationName: location_name,
-          serviceId: service_id,
-          serviceName: service_name,
-          dataModel: data_model,
-          status,
-        })
-      )
-    );
+          backups = [
+            {
+              backup_id: 'backup_1',
+              name: 'Backup 1',
+              location_id: 'location_1',
+              location_name: 'Location 1',
+              created_at: '2021-03-16T16:36:20.244Z',
+              service_id: 'service_1',
+              service_name: 'Service 1',
+              data_model: DataModel.LOGICAL,
+              status: Status.SUCCESS,
+              vendor: 'PostgreSQL',
+            },
+          ],
+        }) =>
+          backups.map(
+            ({
+              backup_id,
+              name,
+              location_id,
+              location_name,
+              created_at,
+              service_id,
+              service_name,
+              data_model,
+              status,
+              vendor,
+            }): Backup => ({
+              id: backup_id,
+              name,
+              created: new Date(created_at).getTime(),
+              locationId: location_id,
+              locationName: location_name,
+              serviceId: service_id,
+              serviceName: service_name,
+              dataModel: data_model,
+              status,
+              vendor,
+            })
+          )
+      );
   },
 };
