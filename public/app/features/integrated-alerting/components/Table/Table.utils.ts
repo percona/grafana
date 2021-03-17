72432199011ec0a4dcbd9ccafc1814dde1b69f49
Fabio Silva <ffjs1993@gmail.com>
2021-03-17 10:31:37 +0000

    fix: add pagination constants to index

--- public/app/features/integrated-alerting/components/Table/Table.utils.ts
+++ public/app/features/integrated-alerting/components/Table/Table.utils.ts
@@ -1,4 +1,4 @@
-import { PAGE_SIZES } from './Pagination/Pagination.constants';
+import { PAGE_SIZES } from './Pagination';
 
 export const getProperPageSize = (pageSize: number) =>
   PAGE_SIZES.find(p => p.value === pageSize) ? pageSize : PAGE_SIZES[0].value;
