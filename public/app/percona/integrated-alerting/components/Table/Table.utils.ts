720b1ca1f627667421e7d65efaec88a08d4f0875
Fabio Silva <ffjs1993@gmail.com>
2021-03-18 11:47:37 +0000

    PMM-7574 remove unused Table utils

--- public/app/percona/integrated-alerting/components/Table/Table.utils.ts
+++
@@ -1,4 +0,0 @@
-import { PAGE_SIZES } from './Pagination';
-
-export const getProperPageSize = (pageSize: number) =>
-  PAGE_SIZES.find(p => p.value === pageSize) ? pageSize : PAGE_SIZES[0].value;
--- public/app/percona/integrated-alerting/components/Table/index.ts
+++ public/app/percona/integrated-alerting/components/Table/index.ts
@@ -1,4 +1,2 @@
 export * from './Table';
-export * from './Table.utils';
-export * from './Table.constants';
 export * from './Table.types';
