e5a6e7718ab84991d28b069b0701051a40d933f7
Fabio Silva <ffjs1993@gmail.com>
2021-03-25 16:33:21 +0000

    PMM-7762 change sidebar link logic

--- public/app/core/components/sidemenu/TopSection.tsx
+++ public/app/core/components/sidemenu/TopSection.tsx
@@ -1,14 +1,17 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import _ from 'lodash';
 import TopSectionItem from './TopSectionItem';
 import config from '../../config';
-import { getLocationSrv, getBackendSrv } from '@grafana/runtime';
+import { getLocationSrv } from '@grafana/runtime';
 import { NavModelItem } from '@grafana/data';
 import { buildIntegratedAlertingMenuItem } from './TopSection.utils';
+import { FeatureLoaderService } from 'app/percona/shared/components/Elements/FeatureLoader/FeatureLoader.service';
+import { LinkConfig } from './TopSection.types';
 
 const TopSection: FC<any> = () => {
   const [showDBaaS, setShowDBaaS] = useState(false);
   const [showSTT, setShowSTT] = useState(false);
+  const [showBackup, setShowBackup] = useState(false);
   const navTree = _.cloneDeep(config.bootData.navTree) as NavModelItem[];
   const [mainLinks, setMainLinks] = useState(_.filter(navTree, item => !item.hideFromMenu));
   const searchLink = {
@@ -16,27 +19,47 @@ const TopSection: FC<any> = () => {
     icon: 'search',
   };
 
-  const dbaasLink = {
-    id: 'dbaas',
-    icon: 'database',
-    text: 'DBaaS',
-    url: `${config.appSubUrl}/dbaas`,
-  };
-
-  const sttLink = {
-    id: 'databsase-checks',
-    icon: 'percona-database-checks',
-    text: 'PMM Database checks',
-    url: `${config.appSubUrl}/pmm-database-checks`,
-  };
+  const linksConfig = useMemo<LinkConfig[]>(
+    () => [
+      {
+        linkObject: {
+          id: 'dbaas',
+          icon: 'database',
+          text: 'DBaaS',
+          url: `${config.appSubUrl}/dbaas`,
+        },
+        show: showDBaaS,
+      },
+      {
+        linkObject: {
+          id: 'backup',
+          icon: 'history',
+          text: 'Backup',
+          url: `${config.appSubUrl}/backup`,
+        },
+        show: showBackup,
+      },
+      {
+        linkObject: {
+          id: 'databsase-checks',
+          icon: 'percona-database-checks',
+          text: 'PMM Database checks',
+          url: `${config.appSubUrl}/pmm-database-checks`,
+        },
+        show: showSTT,
+      },
+    ],
+    [showDBaaS, showBackup, showSTT, config]
+  );
 
   const onOpenSearch = () => {
     getLocationSrv().update({ query: { search: 'open' }, partial: true });
   };
   const updateMenu = async () => {
-    const { settings } = await getBackendSrv().post(`${window.location.origin}/v1/Settings/Get`);
+    const { settings } = await FeatureLoaderService.getSettings();
     setShowDBaaS(settings.dbaas_enabled);
     setShowSTT(settings.stt_enabled);
+    setShowBackup(settings.backup_management_enabled);
 
     if (settings.alerting_enabled) {
       setMainLinks([...buildIntegratedAlertingMenuItem(mainLinks)]);
@@ -55,8 +78,7 @@ const TopSection: FC<any> = () => {
       {mainLinks.map((link, index) => {
         return <TopSectionItem link={link} key={`${link.id}-${index}`} />;
       })}
-      {showSTT && <TopSectionItem link={sttLink} />}
-      {showDBaaS && <TopSectionItem link={dbaasLink} />}
+      {linksConfig.map(({ show, linkObject }) => show && <TopSectionItem link={linkObject} />)}
     </div>
   );
 };
