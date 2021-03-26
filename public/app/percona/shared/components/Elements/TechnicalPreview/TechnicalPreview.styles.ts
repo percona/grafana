651fe39240b393d58739957eba0a9c8f92123f01
Roman Misyurin <roman.misyurin@percona.com>
2021-03-26 14:03:57 +0300

    PMM-7642 fix label styling

--- public/app/percona/shared/components/Elements/TechnicalPreview/TechnicalPreview.styles.ts
+++ public/app/percona/shared/components/Elements/TechnicalPreview/TechnicalPreview.styles.ts
@@ -11,9 +11,9 @@ export const getStyles = stylesFactory(
         right: 24px;
 
         h1 {
-          font-size: ${typography.size.lg};
+          font-size: 14px;
           color: #828282;
-          padding: 5px 5px 5px;
+          padding: 5px 8px 5px 8px;
           border: 1px solid #9fa7b3;
           border-radius: ${border.radius.md};
           user-select: none;
