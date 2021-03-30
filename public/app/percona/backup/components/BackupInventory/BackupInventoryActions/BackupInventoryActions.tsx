b11419d0af147a476f17fdb1b9956b2a30749206
Fabio Silva <ffjs1993@gmail.com>
2021-03-30 17:31:26 +0100

    PMM-7712 add tooltip to backup icon

--- public/app/percona/backup/components/BackupInventory/BackupInventoryActions/BackupInventoryActions.tsx
+++ public/app/percona/backup/components/BackupInventory/BackupInventoryActions/BackupInventoryActions.tsx
@@ -3,6 +3,7 @@ import { useStyles } from '@grafana/ui';
 import { DBIcon } from '../../DBIcon';
 import { BackupInventoryActionsProps } from './BackupInventoryActions.types';
 import { getStyles } from './BackupInventoryActions.styles';
+import { Messages } from './BackupInventoryActions.messages';
 
 export const BackupInventoryActions: FC<BackupInventoryActionsProps> = ({ backup, onBackup }) => {
   const styles = useStyles(getStyles);
@@ -10,7 +11,13 @@ export const BackupInventoryActions: FC<BackupInventoryActionsProps> = ({ backup
 
   return (
     <div className={styles.actionsWrapper}>
-      <DBIcon type="backup" data-qa="restore-backup-artifact-button" role="button" onClick={handleBackup} />
+      <DBIcon
+        tooltipText={Messages.addBackup}
+        type="backup"
+        data-qa="restore-backup-artifact-button"
+        role="button"
+        onClick={handleBackup}
+      />
     </div>
   );
 };
