3dc8f849e977dee0fa6965f3771a1978e5464370
Fabio Silva <ffjs1993@gmail.com>
2021-03-30 10:50:58 +0100

    PMM-7712 add tests to AddBackupModal.utils

---
+++ public/app/percona/backup/components/BackupInventory/AddBackupModal/AddBackupModal.utils.test.ts
@@ -0,0 +1,40 @@
+import { Databases } from 'app/percona/shared/core';
+import { stubs as backupStubs } from '../__mocks__/BackupInventory.service';
+import { DataModel } from '../BackupInventory.types';
+import { AddBackupFormProps, RetryMode } from './AddBackupModal.types';
+import { toFormBackup } from './AddBackupModal.utils';
+
+describe('AddBackupModal::utils', () => {
+  describe('toFormBackup', () => {
+    it('should return default values if a null Backup is passed', () => {
+      expect(toFormBackup(null)).toEqual<AddBackupFormProps>({
+        service: { label: '', value: '' },
+        vendor: Databases.mysql,
+        dataModel: DataModel.PHYSICAL,
+        backupName: '',
+        description: '',
+        location: { label: '', value: '' },
+        retryMode: RetryMode.AUTO,
+        retryTimes: 0,
+        retryInterval: 0,
+      });
+    });
+
+    it('should convert to form props', () => {
+      const backup = backupStubs[0];
+      const { serviceName, serviceId, vendor, dataModel, locationName, locationId } = backup;
+
+      expect(toFormBackup(backup)).toEqual<AddBackupFormProps>({
+        service: { label: serviceName, value: serviceId },
+        vendor,
+        dataModel,
+        backupName: '',
+        description: '',
+        location: { label: locationName, value: locationId },
+        retryMode: RetryMode.AUTO,
+        retryTimes: 0,
+        retryInterval: 0,
+      });
+    });
+  });
+});
