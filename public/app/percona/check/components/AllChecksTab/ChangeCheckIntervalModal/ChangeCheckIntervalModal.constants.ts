ea7b8f77396d9f27538c1105581dfccdd848b3de
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-04-02 19:26:23 +0200

    PMM-7772 remove comments

--- public/app/percona/check/components/AllChecksTab/ChangeCheckIntervalModal/ChangeCheckIntervalModal.constants.ts
+++ public/app/percona/check/components/AllChecksTab/ChangeCheckIntervalModal/ChangeCheckIntervalModal.constants.ts
@@ -4,9 +4,3 @@ export const checkIntervalOptions = Object.keys(Interval).map(intervalKey => ({
   value: intervalKey,
   label: Interval[intervalKey as keyof typeof Interval],
 }));
-
-// export const checkIntervalOptions = [
-//   { value: Interval.STANDARD, label: Interval.STANDARD },
-//   { value: Interval.RARE, label: Interval.RARE },
-//   { value: Interval.FREQUENT, label: Interval.FREQUENT },
-// ];
