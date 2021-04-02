e885abd8dbdb8207e8fc2ba200ab91f818395d57
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-04-02 15:48:52 +0200

    Merge branch 'master' into PMM-7451-set-stt-check-intervals * master:   PMM-7556 settings migration (#73)   PMM-7580 Database version (#100)   PMM-7774 do not close modal on error (#101)   PMM-7674 Add allocated and available disk (#92)   PMM-7488 Add expected resources (#90)   PMM-7574: move IA and Backup to percona folder (#96)   PMM-6017 move the logo from pmm-server-packaging (#85)   PMM-7513: dynamic alert rule fields (#91)   Revert "Fix bad PAGE_SIZES import (#93)" (#94)   Fix bad PAGE_SIZES import (#93)   PMM-7543 fix breadcrumb on double click on tabs (#88)   PMM-7452: Test storage location credentials (#78)   PMM 7455: Edit storage location (#71)   PMM-7486 See available and allocated resources (#80)   PMM-7461: add table overlay (#75)   PMM-7510: add pagination to rule templates (#82)   PMM-7511: channels pagination (#84)   Pmm 7449 backup location list (#62)   PMM-7375: Send tempate name while editing (#77)   PMM-7260: edit rule name (#76)

--- public/app/percona/check/components/AllChecksTab/CheckTableRow.styles.ts
+++ public/app/percona/check/components/AllChecksTab/CheckTableRow.styles.ts
@@ -4,6 +4,7 @@ import { GrafanaTheme } from '@grafana/data';
 export const getStyles = (theme: GrafanaTheme) => ({
   actionsWrapper: css`
     align-items: center;
+    justify-content: center;
     display: flex;
 
     > :not(:last-child) {
