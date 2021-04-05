be0bdd50eb48102ef58d983e3b3ac42ec33d55df
Fabio Silva <ffjs1993@gmail.com>
2021-04-05 22:38:53 +0100

    PMM-7616 apply TabbedContent to IA

--- public/app/percona/integrated-alerting/IntegratedAlertingPage.tsx
+++ public/app/percona/integrated-alerting/IntegratedAlertingPage.tsx
@@ -1,56 +1,49 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Tab, TabContent, TabsBar, useStyles } from '@grafana/ui';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import { useStyles } from '@grafana/ui';
 import { logger } from '@percona/platform-core';
 import { Breadcrumb } from 'app/core/components/Breadcrumb';
 import { IntegratedAlertingContent } from './components/IntegratedAlertingContent';
 import { getStyles } from './IntegratedAlerting.styles';
 import { IntegratedAlertingService } from './IntegratedAlerting.service';
-import { DEFAULT_TAB, PAGE_MODEL, PAGE_TABS } from './IntegratedAlerting.constants';
+import { PAGE_MODEL } from './IntegratedAlerting.constants';
 import { TabKeys } from './IntegratedAlerting.types';
 import { AlertRules, AlertRuleTemplate, Alerts, NotificationChannel } from './components';
-import { getLocationSrv } from '@grafana/runtime';
-import { UrlQueryValue } from '@grafana/data';
-import { useSelector } from 'react-redux';
-import { StoreState } from 'app/types';
 import { TechnicalPreview } from '../shared/components/Elements/TechnicalPreview/TechnicalPreview';
-
-const tabComponentMap = [
-  {
-    id: TabKeys.alerts,
-    component: <Alerts key={TabKeys.alerts} />,
-  },
-  {
-    id: TabKeys.alertRules,
-    component: <AlertRules key={TabKeys.alertRules} />,
-  },
-  {
-    id: TabKeys.alertRuleTemplates,
-    component: <AlertRuleTemplate key={TabKeys.alertRuleTemplates} />,
-  },
-  {
-    id: TabKeys.notificationChannels,
-    component: <NotificationChannel key={TabKeys.notificationChannels} />,
-  },
-];
+import { TabbedContent, ContentTab } from '../shared/components/Elements/TabbedContent';
+import { Messages } from './IntegratedAlerting.messages';
 
 const IntegratedAlertingPage: FC = () => {
   const styles = useStyles(getStyles);
-  const tabKey = useSelector((state: StoreState) => state.location.routeParams.tab);
   const [loadingSettings, setLoadingSettings] = useState(true);
   const [alertingEnabled, setAlertingEnabled] = useState(false);
-  const [activeTab, setActiveTab] = useState(DEFAULT_TAB);
+
+  const tabs: ContentTab[] = useMemo(
+    (): ContentTab[] => [
+      {
+        label: Messages.tabs.alerts,
+        key: TabKeys.alerts,
+        component: () => <Alerts key={TabKeys.alerts} />,
+      },
+      {
+        label: Messages.tabs.alertRules,
+        key: TabKeys.alertRules,
+        component: () => <AlertRules key={TabKeys.alertRules} />,
+      },
+      {
+        label: Messages.tabs.alertRuleTemplates,
+        key: TabKeys.alertRuleTemplates,
+        component: () => <AlertRuleTemplate key={TabKeys.alertRuleTemplates} />,
+      },
+      {
+        label: Messages.tabs.notificationChannels,
+        key: TabKeys.notificationChannels,
+        component: () => <NotificationChannel key={TabKeys.notificationChannels} />,
+      },
+    ],
+    []
+  );
 
   const { path: basePath } = PAGE_MODEL;
-
-  const isValidTab = (tab: UrlQueryValue) => Object.values(TabKeys).includes(tab as TabKeys);
-
-  const selectTab = (selectedTabKey: string) => {
-    if (selectedTabKey !== tabKey) {
-      getLocationSrv().update({
-        path: `${basePath}/${selectedTabKey}`,
-      });
-    }
-  };
 
   const getSettings = async () => {
     setLoadingSettings(true);
@@ -68,14 +61,6 @@ const IntegratedAlertingPage: FC = () => {
   };
 
   useEffect(() => {
-    isValidTab(tabKey) || selectTab(DEFAULT_TAB);
-  }, []);
-
-  useEffect(() => {
-    setActiveTab(isValidTab(tabKey) ? (tabKey as TabKeys) : DEFAULT_TAB);
-  }, [tabKey]);
-
-  useEffect(() => {
     getSettings();
   }, []);
 
@@ -83,14 +68,15 @@ const IntegratedAlertingPage: FC = () => {
     <div className={styles.integratedAlertingWrapper}>
       <Breadcrumb pageModel={PAGE_MODEL} />
       <TechnicalPreview />
-      <TabsBar>
-        {PAGE_TABS.map(tab => (
-          <Tab key={tab.id} label={tab.title} active={tab.id === activeTab} onChangeTab={() => selectTab(tab.id)} />
-        ))}
-      </TabsBar>
-      <IntegratedAlertingContent loadingSettings={loadingSettings} alertingEnabled={alertingEnabled}>
-        <TabContent>{tabComponentMap.find(tab => tab.id === activeTab)?.component || null}</TabContent>
-      </IntegratedAlertingContent>
+      <TabbedContent
+        tabs={tabs}
+        basePath={basePath}
+        renderTab={({ Content }) => (
+          <IntegratedAlertingContent loadingSettings={loadingSettings} alertingEnabled={alertingEnabled}>
+            <Content />
+          </IntegratedAlertingContent>
+        )}
+      />
     </div>
   );
 };
