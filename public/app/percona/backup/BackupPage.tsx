cb44875ed8c4cba69f24d8442c5996dfd8a589c1
Fabio Silva <ffjs1993@gmail.com>
2021-04-05 22:50:28 +0100

    PMM-7616 apply TabbedContent to BackupPage

--- public/app/percona/backup/BackupPage.tsx
+++ public/app/percona/backup/BackupPage.tsx
@@ -1,25 +1,22 @@
-import React, { FC, useEffect, useMemo, useState } from 'react';
-import { Tab, TabContent, TabsBar, useStyles } from '@grafana/ui';
-import { getLocationSrv } from '@grafana/runtime';
-import { UrlQueryValue } from '@grafana/data';
-import { useSelector } from 'react-redux';
-import { StoreState } from 'app/types';
+import React, { FC, useMemo } from 'react';
+import { useStyles } from '@grafana/ui';
 import { Breadcrumb } from 'app/core/components/Breadcrumb';
+import { TabbedContent, ContentTab } from '../shared/components/Elements/TabbedContent';
+import { TechnicalPreview } from '../shared/components/Elements/TechnicalPreview/TechnicalPreview';
 import { TabKeys } from './Backup.types';
 import { getStyles } from './Backup.styles';
 import { StorageLocations } from './components/StorageLocations';
-import { DEFAULT_TAB, PAGE_MODEL, PAGE_TABS } from './BackupPage.constants';
-import { TechnicalPreview } from '../shared/components/Elements/TechnicalPreview/TechnicalPreview';
+import { PAGE_MODEL } from './BackupPage.constants';
+import { Messages } from './Backup.messages';
 
 const BackupPage: FC = () => {
-  const [activeTab, setActiveTab] = useState(TabKeys.locations);
-  const tabKey = useSelector((state: StoreState) => state.location.routeParams.tab);
   const styles = useStyles(getStyles);
-  const tabComponentMap = useMemo(
-    () => [
+  const tabs: ContentTab[] = useMemo(
+    (): ContentTab[] => [
       {
-        id: TabKeys.locations,
-        component: <StorageLocations />,
+        key: TabKeys.locations,
+        label: Messages.tabs.locations,
+        component: () => <StorageLocations />,
       },
     ],
     []
@@ -27,32 +24,11 @@ const BackupPage: FC = () => {
 
   const { path: basePath } = PAGE_MODEL;
 
-  const isValidTab = (tab: UrlQueryValue) => Object.values(TabKeys).includes(tab as TabKeys);
-
-  const selectTab = (tabKey: string) => {
-    getLocationSrv().update({
-      path: `${basePath}/${tabKey}`,
-    });
-  };
-
-  useEffect(() => {
-    isValidTab(tabKey) || selectTab(DEFAULT_TAB);
-  }, []);
-
-  useEffect(() => {
-    setActiveTab(isValidTab(tabKey) ? (tabKey as TabKeys) : DEFAULT_TAB);
-  }, [tabKey]);
-
   return (
     <div className={styles.backupWrapper}>
       <Breadcrumb pageModel={PAGE_MODEL} />
       <TechnicalPreview />
-      <TabsBar>
-        {PAGE_TABS.map(tab => (
-          <Tab key={tab.id} label={tab.title} active={tab.id === activeTab} onChangeTab={() => setActiveTab(tab.id)} />
-        ))}
-      </TabsBar>
-      <TabContent>{tabComponentMap.find(tab => tab.id === activeTab)?.component}</TabContent>
+      <TabbedContent tabs={tabs} basePath={basePath} />;
     </div>
   );
 };
