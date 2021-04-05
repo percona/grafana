0816278b199d3e6e00ab686b55e249b8457beb8e
Fabio Silva <ffjs1993@gmail.com>
2021-04-05 22:52:20 +0100

    PMM-7616 apply PageWrapper to backups

--- public/app/percona/backup/BackupPage.tsx
+++ public/app/percona/backup/BackupPage.tsx
@@ -1,16 +1,13 @@
 import React, { FC, useMemo } from 'react';
-import { useStyles } from '@grafana/ui';
-import { Breadcrumb } from 'app/core/components/Breadcrumb';
 import { TabbedContent, ContentTab } from '../shared/components/Elements/TabbedContent';
 import { TechnicalPreview } from '../shared/components/Elements/TechnicalPreview/TechnicalPreview';
+import PageWrapper from '../shared/components/PageWrapper/PageWrapper';
 import { TabKeys } from './Backup.types';
-import { getStyles } from './Backup.styles';
 import { StorageLocations } from './components/StorageLocations';
 import { PAGE_MODEL } from './BackupPage.constants';
 import { Messages } from './Backup.messages';
 
 const BackupPage: FC = () => {
-  const styles = useStyles(getStyles);
   const tabs: ContentTab[] = useMemo(
     (): ContentTab[] => [
       {
@@ -25,11 +22,10 @@ const BackupPage: FC = () => {
   const { path: basePath } = PAGE_MODEL;
 
   return (
-    <div className={styles.backupWrapper}>
-      <Breadcrumb pageModel={PAGE_MODEL} />
+    <PageWrapper pageModel={PAGE_MODEL}>
       <TechnicalPreview />
-      <TabbedContent tabs={tabs} basePath={basePath} />;
-    </div>
+      <TabbedContent tabs={tabs} basePath={basePath} />
+    </PageWrapper>
   );
 };
 
