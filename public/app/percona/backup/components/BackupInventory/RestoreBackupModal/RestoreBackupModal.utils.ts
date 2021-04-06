8ef56b036f74072f8568913532247be6d1b012d2
Fabio Silva <ffjs1993@gmail.com>
2021-04-06 16:47:26 +0100

    PMM-7518 show database label as vendor

--- public/app/percona/backup/components/BackupInventory/RestoreBackupModal/RestoreBackupModal.utils.ts
+++ public/app/percona/backup/components/BackupInventory/RestoreBackupModal/RestoreBackupModal.utils.ts
@@ -1,10 +1,11 @@
+import { DATABASE_LABELS } from 'app/percona/shared/core';
 import { Backup } from '../BackupInventory.types';
 import { formatDataModel } from '../BackupInventory.utils';
 import { RestoreBackupFormProps, ServiceTypeSelect } from './RestoreBackupModal.types';
 
 export const toFormProps = ({ vendor, serviceId, serviceName, dataModel }: Backup): RestoreBackupFormProps => ({
   serviceType: ServiceTypeSelect.SAME,
-  vendor: vendor,
+  vendor: DATABASE_LABELS[vendor],
   service: { label: serviceName, value: serviceId },
   dataModel: formatDataModel(dataModel),
 });
