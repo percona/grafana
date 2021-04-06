243dda7d8fdd75ffd8421c1004e2e3ceefad76a9
Fabio Silva <ffjs1993@gmail.com>
2021-04-06 10:06:43 +0100

    PMM-7616 location as prop to breadcrumbs

--- public/app/percona/shared/components/PageWrapper/PageWrapper.tsx
+++ public/app/percona/shared/components/PageWrapper/PageWrapper.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
 import { useStyles } from '@grafana/ui';
+import { useSelector } from 'react-redux';
+import { StoreState } from 'app/types';
 import { Breadcrumb } from 'app/core/components/Breadcrumb';
 import { getStyles } from './PageWrapper.styles';
 import { PageWrapperProps } from './PageWrapper.types';
 
 const PageWrapper: FC<PageWrapperProps> = ({ children, pageModel }) => {
   const styles = useStyles(getStyles);
+  const locationPath = useSelector((state: StoreState) => state.location.path);
+  const currentLocation = locationPath.slice(1);
 
   return (
     <div className={styles.wrapper}>
-      <Breadcrumb pageModel={pageModel} />
+      <Breadcrumb pageModel={pageModel} currentLocation={currentLocation} />
       {children}
     </div>
   );
