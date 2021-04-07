4a4a4596332c5acee22ed23805f88f01891f0345
Fabio Silva <ffjs1993@gmail.com>
2021-04-07 14:38:33 +0100

    PMM-7616 CheckPanel: use FeatureLoader

--- public/app/percona/check/CheckPanel.tsx
+++ public/app/percona/check/CheckPanel.tsx
@@ -1,42 +1,23 @@
-import React, { FC, useEffect, useMemo, useState } from 'react';
-import { Spinner, useStyles } from '@grafana/ui';
-import { PMM_SETTINGS_URL } from 'app/percona/check/CheckPanel.constants';
-import { Settings, TabKeys } from './types';
-import { CheckService } from './Check.service';
+import React, { FC, useMemo, useState } from 'react';
+import { useStyles } from '@grafana/ui';
+import { TabKeys } from './types';
 import { getStyles } from './CheckPanel.styles';
 import { Messages } from './CheckPanel.messages';
 import { AllChecksTab, FailedChecksTab } from './components';
 import { PAGE_MODEL } from './CheckPanel.constants';
 import PageWrapper from '../shared/components/PageWrapper/PageWrapper';
 import { TabbedContent, ContentTab } from '../shared/components/Elements/TabbedContent';
+import { FeatureLoader } from '../shared/components/Elements/FeatureLoader';
 
 export const CheckPanel: FC = () => {
   const { path: basePath } = PAGE_MODEL;
 
   const [hasNoAccess, setHasNoAccess] = useState(false);
-  const [isSttEnabled, setIsSttEnabled] = useState(false);
-  const [getSettingsPending, setGetSettingsPending] = useState(false);
   const styles = useStyles(getStyles);
 
-  const getSettings = async () => {
-    try {
-      setGetSettingsPending(true);
-      const resp = (await CheckService.getSettings()) as Settings;
-
-      setIsSttEnabled(!!resp.settings?.stt_enabled);
-      setHasNoAccess(false);
-    } catch (err) {
-      setHasNoAccess(err.response?.status === 401);
-
-      console.error(err);
-    } finally {
-      setGetSettingsPending(false);
-    }
+  const handleError = (error: any) => {
+    setHasNoAccess(error.response?.status === 401);
   };
-
-  useEffect(() => {
-    getSettings();
-  }, []);
 
   const tabs = useMemo<ContentTab[]>(
     (): ContentTab[] => [
@@ -51,7 +32,7 @@ export const CheckPanel: FC = () => {
         component: () => <AllChecksTab key="all-checks" />,
       },
     ],
-    [hasNoAccess, isSttEnabled]
+    [hasNoAccess]
   );
 
   if (hasNoAccess) {
@@ -66,27 +47,17 @@ export const CheckPanel: FC = () => {
     );
   }
 
-  // TODO replace with FeatureLoader when available
   return (
     <PageWrapper pageModel={PAGE_MODEL}>
-      <div className={styles.panel} data-qa="db-check-panel">
-        {getSettingsPending && (
-          <div className={styles.spinner} data-qa="db-check-spinner">
-            <Spinner />
-          </div>
+      <TabbedContent
+        tabs={tabs}
+        basePath={basePath}
+        renderTab={({ Content }) => (
+          <FeatureLoader featureName={Messages.stt} featureFlag="sttEnabled" onError={handleError}>
+            <Content />
+          </FeatureLoader>
         )}
-        {!getSettingsPending &&
-          (isSttEnabled ? (
-            <TabbedContent tabs={tabs} basePath={basePath} />
-          ) : (
-            <div className={styles.empty}>
-              {Messages.sttDisabled}{' '}
-              <a className={styles.link} href={PMM_SETTINGS_URL} data-qa="db-check-panel-settings-link">
-                {Messages.pmmSettings}
-              </a>
-            </div>
-          ))}
-      </div>
+      />
     </PageWrapper>
   );
 };
