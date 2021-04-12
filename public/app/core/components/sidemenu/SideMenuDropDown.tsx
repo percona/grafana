fd73856c4632813036898b4adbfa3e2828b21cef
Fabio Silva <ffjs1993@gmail.com>
2021-04-12 11:15:23 +0100

    PMM-7867 remove useless fragment

--- public/app/core/components/sidemenu/SideMenuDropDown.tsx
+++ public/app/core/components/sidemenu/SideMenuDropDown.tsx
@@ -21,13 +21,9 @@ const SideMenuDropDown: FC<Props> = props => {
           <span className="sidemenu-item-text">{link.text}</span>
         </a>
       </li>
-      {childrenLinks.map((child, index) => {
-        return (
-          <React.Fragment key={`${child.url}-${index}`}>
-            <DropDownChild child={child} />
-          </React.Fragment>
-        );
-      })}
+      {childrenLinks.map((child, index) => (
+        <DropDownChild child={child} key={`${child.url}-${index}`} />
+      ))}
     </ul>
   );
 };
