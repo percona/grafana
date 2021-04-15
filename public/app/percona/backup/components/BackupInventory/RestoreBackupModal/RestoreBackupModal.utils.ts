a8415a7dc6fed1ce2d154a7760cf2b535403c168
Fabio Silva <ffjs1993@gmail.com>
2021-04-15 10:49:30 +0100

    PMM-7518 add type to toFormProps

--- public/app/percona/backup/components/BackupInventory/RestoreBackupModal/RestoreBackupModal.utils.ts
+++ public/app/percona/backup/components/BackupInventory/RestoreBackupModal/RestoreBackupModal.utils.ts
@@ -3,7 +3,9 @@ import { Backup } from '../BackupInventory.types';
 import { formatDataModel } from '../BackupInventory.utils';
 import { RestoreBackupFormProps, ServiceTypeSelect } from './RestoreBackupModal.types';
 
-export const toFormProps = ({ vendor, serviceId, serviceName, dataModel }: Backup): RestoreBackupFormProps => ({
+type ToFormProps = (props: Backup) => RestoreBackupFormProps;
+
+export const toFormProps: ToFormProps = ({ vendor, serviceId, serviceName, dataModel }) => ({
   serviceType: ServiceTypeSelect.SAME,
   vendor: DATABASE_LABELS[vendor],
   service: { label: serviceName, value: serviceId },
