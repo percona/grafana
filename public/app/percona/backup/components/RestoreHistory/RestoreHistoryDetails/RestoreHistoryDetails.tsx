24fcd8123e11899f33e82427bfe236e198374bc9
Fabio Silva <ffjs1993@gmail.com>
2021-04-19 10:26:52 +0100

    PMM-7873 use DetailedDate on restore details

--- public/app/percona/backup/components/RestoreHistory/RestoreHistoryDetails/RestoreHistoryDetails.tsx
+++ public/app/percona/backup/components/RestoreHistory/RestoreHistoryDetails/RestoreHistoryDetails.tsx
@@ -1,18 +1,14 @@
 import React, { FC } from 'react';
 import { useStyles } from '@grafana/ui';
-import moment from 'moment/moment';
 import { formatDataModel } from 'app/percona/backup/Backup.utils';
+import { DetailedDate } from '../../DetailedDate';
 import { RestoreHistoryDetailsProps } from './RestoreHistoryDetails.types';
 import { Messages } from './RestoreHistoryDetails.Messages';
 import { getStyles } from './RestoreHistoryDetails.styles';
-import { DAY_FORMAT, HOUR_FORMAT } from '../../DetailedDate/DetailedDate.constants';
 
 export const RestoreHistoryDetails: FC<RestoreHistoryDetailsProps> = ({ name, finished, dataModel }) => {
   const styles = useStyles(getStyles);
   const dataModelMsg = formatDataModel(dataModel);
-  const momentObj = moment(finished);
-  const dayTime = momentObj.format(DAY_FORMAT);
-  const hourTime = momentObj.format(HOUR_FORMAT);
 
   return (
     <div className={styles.detailsWrapper} data-qa="restore-details-wrapper">
@@ -21,8 +17,7 @@ export const RestoreHistoryDetails: FC<RestoreHistoryDetailsProps> = ({ name, fi
       </span>
       <span data-qa="restore-details-finished">
         <span className={styles.detailLabel}>{Messages.finished}</span>
-        <span>{dayTime}</span>
-        <span>{hourTime}</span>
+        <DetailedDate dataQa="restore-details-finished" date={finished} />
       </span>
       <span data-qa="restore-details-data-model">
         <span className={styles.detailLabel}>{Messages.dataModel}</span> <span>{dataModelMsg}</span>
