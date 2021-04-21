0774f80db207fe490c9d93afb691452c5b8892ee
Fabio Silva <ffjs1993@gmail.com>
2021-04-21 10:17:19 +0100

    PMM-7922 MultiSelectField

---
+++ public/app/percona/shared/components/Form/MultiSelect/MultiSelect.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { MultiSelect } from '@grafana/ui';
+import { MultiSelectCommonProps } from '@grafana/ui/src/components/Select/types';
+import { withSelectStyles } from '../withSelectStyles/withSelectStyles';
+import { LabelWrapper } from '../LabelWrapper';
+import { MultiSelectFieldProps } from './MultiSelect.types';
+
+const MultiSelectFieldWrapper: FC<MultiSelectFieldProps & MultiSelectCommonProps<any>> = ({
+  label,
+  name,
+  ...props
+}) => (
+  <>
+    <LabelWrapper label={label} dataQa={`${name}-select-label`} />
+    <MultiSelect {...props} />
+  </>
+);
+
+export const MultiSelectField = withSelectStyles(MultiSelectFieldWrapper);
