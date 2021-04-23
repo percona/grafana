2642c084fbc72197aba5525b665d32ece5481053
Fabio Silva <ffjs1993@gmail.com>
2021-04-23 14:15:29 +0100

    PMM-7922 expand AsyncSelectFieldProps

--- public/app/percona/shared/components/Form/AsyncSelectField/AsyncSelectField.tsx
+++ public/app/percona/shared/components/Form/AsyncSelectField/AsyncSelectField.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
-import { AsyncSelect, AsyncSelectProps } from '@grafana/ui';
+import { AsyncSelect } from '@grafana/ui';
 import { withSelectStyles } from '../withSelectStyles/withSelectStyles';
 import { LabelWrapper } from '../LabelWrapper';
 import { AsyncSelectFieldProps } from './AsyncSelectField.types';
 
-const AsyncSelectFieldWrapper: FC<AsyncSelectFieldProps & AsyncSelectProps<any>> = ({ label, name, ...props }) => (
+const AsyncSelectFieldWrapper: FC<AsyncSelectFieldProps<any>> = ({ label, name, ...props }) => (
   <>
     <LabelWrapper label={label} dataQa={`${name}-select-label`} />
     <AsyncSelect {...props} />
