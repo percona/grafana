7d02f136d1a9f1f6c6430e2cd4dd0a1040b7f4cd
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-04-30 16:19:33 +0200

    PMM-8009 Add name length validation in sign up

--- public/app/percona/settings/components/PlatformLogin/PlatformLogin.constants.ts
+++ public/app/percona/settings/components/PlatformLogin/PlatformLogin.constants.ts
@@ -1,3 +1,5 @@
 export const TERMS_OF_SERVICE_URL = 'https://per.co.na/pmm/platform-terms';
 
 export const PRIVACY_POLICY_URL = 'https://per.co.na/pmm/platform-privacy';
+
+export const MAX_NAME_LENGTH = 50;
