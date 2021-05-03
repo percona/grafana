c5e4b774f169b1234e33cc75a023ffdec170aae4
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-05-03 12:59:26 +0200

    SAAS-508 fix reset password link
    Co-authored-by: Alex Tymchuk <alexander.tymchuk@percona.com>

--- public/app/percona/settings/components/PlatformLogin/PlatformLogin.constants.ts
+++ public/app/percona/settings/components/PlatformLogin/PlatformLogin.constants.ts
@@ -3,6 +3,6 @@ export const TERMS_OF_SERVICE_URL = 'https://per.co.na/pmm/platform-terms';
 export const PRIVACY_POLICY_URL = 'https://per.co.na/pmm/platform-privacy';
 
 export const RESET_PASSWORD_URL =
-  process.env.NODE_ENV !== 'production'
+  process.env.NODE_ENV === 'production'
     ? 'https://okta.percona.com/signin/forgot-password'
     : 'https://okta-dev.percona.com/signin/forgot-password';
