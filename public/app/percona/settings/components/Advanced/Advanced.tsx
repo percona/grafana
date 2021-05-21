b0cc1052c7d4aad360ab34893f1b61318677a4a1
Fabio Silva <ffjs1993@gmail.com>
2021-05-21 14:41:07 +0100

    PMM-8135: uncomment backup feature

--- public/app/percona/settings/components/Advanced/Advanced.tsx
+++ public/app/percona/settings/components/Advanced/Advanced.tsx
@@ -77,6 +77,9 @@ export const Advanced: FC<AdvancedProps> = ({
       technicalPreviewLegend,
       technicalPreviewDescription,
       technicalPreviewLinkText,
+      backupLabel,
+      backupLink,
+      backupTooltip,
     },
     tooltipLinkText,
   } = Messages;
@@ -272,8 +275,7 @@ export const Advanced: FC<AdvancedProps> = ({
                 dataQa="advanced-dbaas"
                 component={SwitchRow}
               />
-              {/* TODO remove comment when feature is ready to come out */}
-              {/* <Field
+              <Field
                 name="backup"
                 type="checkbox"
                 label={backupLabel}
@@ -284,7 +286,7 @@ export const Advanced: FC<AdvancedProps> = ({
                 disabled={!values.backup}
                 dataQa="advanced-backup"
                 component={SwitchRow}
-              /> */}
+              />
               <Field
                 name="alerting"
                 type="checkbox"
