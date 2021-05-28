b11c6df3182be60f74d8b6d3c209e42cf76d61d5
Fabio Silva <ffjs1993@gmail.com>
2021-05-28 11:33:46 +0100

    PMM-8157 add mocked backups to list

---
+++ public/app/percona/backup/components/ScheduledBackups/ScheduledBackups.service.ts
@@ -0,0 +1,25 @@
+import { CancelToken } from 'axios';
+import { Databases } from 'app/percona/shared/core';
+import { Frequency, ScheduledBackup } from './ScheduledBackups.types';
+
+export const ScheduledBackupsService = {
+  async list(token?: CancelToken): Promise<ScheduledBackup[]> {
+    return [
+      {
+        name: 'Backup 1',
+        vendor: Databases.postgresql,
+        start: Date.now(),
+        retention: {
+          daily: 10,
+          weekly: 10,
+        },
+        frequency: {
+          value: 1,
+          unit: Frequency.DAY,
+        },
+        location: 'bucket 1',
+        lastBackup: Date.now(),
+      },
+    ];
+  },
+};
