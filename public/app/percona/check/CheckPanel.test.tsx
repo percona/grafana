6ed65f5b3f11d01a5d740f93d0edcb5fd14ae1a7
Tiago Mota Santos <tiago.mota@percona.com>
2021-05-28 14:41:47 +0100

    PMM-7649 Show unauthorized messaged in FeatureLoader

--- public/app/percona/check/CheckPanel.test.tsx
+++ public/app/percona/check/CheckPanel.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { dataQa } from '@percona/platform-core';
 import { ReactWrapper, mount } from 'enzyme';
 import { CheckPanel } from './CheckPanel';
-import { Messages } from './CheckPanel.messages';
 import { useSelector } from 'react-redux';
-import { SettingsService } from '../settings/Settings.service';
+import { asyncAct } from '../shared/helpers/testUtils';
 
 const fakeLocationUpdate = jest.fn();
 
@@ -36,9 +34,6 @@ jest.mock('@percona/platform-core', () => {
   };
 });
 
-// immediately resolves all pending promises: allows to run expectations after a promise
-const runAllPromises = () => new Promise(setImmediate);
-
 describe('CheckPanel::', () => {
   beforeEach(() => {
     (useSelector as jest.Mock).mockImplementation(callback => {
@@ -50,26 +45,25 @@ describe('CheckPanel::', () => {
     jest.resetAllMocks();
   });
 
-  it('should show a message to unauthorized users', async () => {
-    const UnauthorizedError = () => ({
-      response: {
-        status: 401,
-      },
+  it('should show tabs for all checks and for failed checks', async () => {
+    let wrapper: ReactWrapper = {} as ReactWrapper;
+
+    await asyncAct(() => {
+      wrapper = mount(<CheckPanel />);
     });
 
-    const spy = jest.spyOn(SettingsService, 'getSettings').mockImplementation(() => {
-      throw UnauthorizedError();
-    });
-
-    const wrapper: ReactWrapper<{}, {}, any> = mount(<CheckPanel />);
-
-    await runAllPromises();
-    wrapper.update();
-
-    expect(wrapper.find(dataQa('db-check-panel-unauthorized'))).toHaveLength(1);
-    expect(wrapper.find(dataQa('db-check-panel-unauthorized')).text()).toEqual(Messages.unauthorized);
-
-    spy.mockClear();
+    expect(
+      wrapper
+        .find('li')
+        .at(0)
+        .text()
+    ).toBe('Failed Checks');
+    expect(
+      wrapper
+        .find('li')
+        .at(1)
+        .text()
+    ).toBe('All Checks');
     wrapper.unmount();
   });
 });
