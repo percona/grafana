3840e3290d30a3e264bd7507a3ba3a5f07eff1f9
Fabio Silva <ffjs1993@gmail.com>
2021-06-14 18:43:33 +0100

    PMM-8192 test disabled on RetryModeSelector

--- public/app/percona/backup/components/AddBackupModal/RetryModeSelector/RetryModeSelector.test.tsx
+++ public/app/percona/backup/components/AddBackupModal/RetryModeSelector/RetryModeSelector.test.tsx
@@ -20,18 +20,38 @@ describe('RetryModeSelector', () => {
         .prop('disabled')
     ).toBeFalsy();
   });
-});
 
-it('should disable number inputs when retry mode is AUTO', () => {
-  const wrapper = mount(
-    <FormWrapper>
-      <RetryModeSelector retryMode={RetryMode.AUTO} />
-    </FormWrapper>
-  );
-  expect(
-    wrapper
-      .find(NumberInputField)
-      .at(0)
-      .prop('disabled')
-  ).toBeTruthy();
+  it('should disable number inputs when retry mode is AUTO', () => {
+    const wrapper = mount(
+      <FormWrapper>
+        <RetryModeSelector retryMode={RetryMode.AUTO} />
+      </FormWrapper>
+    );
+    expect(
+      wrapper
+        .find(NumberInputField)
+        .at(0)
+        .prop('disabled')
+    ).toBeTruthy();
+  });
+
+  it('should disable all fields when disabled is passed, even if retry mode is MANUAL', () => {
+    const wrapper = mount(
+      <FormWrapper>
+        <RetryModeSelector disabled retryMode={RetryMode.MANUAL} />
+      </FormWrapper>
+    );
+    expect(
+      wrapper
+        .find(RadioButtonGroupField)
+        .at(0)
+        .prop('disabled')
+    ).toBeTruthy();
+    expect(
+      wrapper
+        .find(NumberInputField)
+        .at(0)
+        .prop('disabled')
+    ).toBeTruthy();
+  });
 });
