a3733f404adee92d510915d4d2ac2dfa2f0f73ba
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2021-06-16 12:00:49 +0300

    PMM-7194 force refresh share panels (#136)
    * PMM-7932 fix typo
    
    * PMM-7194 fix type
    
    * PMM-7932 fix typo
    
    * PMM-7194 add force refresh event
    
    * PMM-7194 add force refresh event
    
    * PMM-7194 Fix review comments
    
    * Fix snapshotCreated event
    
    * Revert "Fix snapshotCreated event"
    
    This reverts commit fe369fd5
    
    * Revert "PMM-7194 Fix review comments"
    
    This reverts commit 9d25e39a
    
    Co-authored-by: Tiago Mota Santos <tiago.mota@percona.com>
    Co-authored-by: Tiago Mota Santos <tiago.a.mota@hotmail.com>

--- public/app/features/dashboard/state/PanelModel.ts
+++ public/app/features/dashboard/state/PanelModel.ts
@@ -28,6 +28,7 @@ import { CoreEvents } from '../../../types';
 
 export const panelAdded = eventFactory<PanelModel | undefined>('panel-added');
 export const panelRemoved = eventFactory<PanelModel | undefined>('panel-removed');
+export const snapshotCreated = eventFactory('snapshot-created');
 
 export interface GridPos {
   x: number;
