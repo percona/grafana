1e6e82084b86afb1608333b07fd8ec7731f710d7
Tiago Mota Santos <tiago.mota@percona.com>
2021-07-05 12:41:12 +0100

    PMM-8077 Show operator version

--- public/app/percona/dbaas/components/Kubernetes/OperatorStatusItem/OperatorStatusItem.tsx
+++ public/app/percona/dbaas/components/Kubernetes/OperatorStatusItem/OperatorStatusItem.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
 import { useStyles } from '@grafana/ui';
-import { OPERATOR_LABELS, OPERATOR_FULL_LABELS } from 'app/percona/shared/core';
+import { OPERATOR_FULL_LABELS } from 'app/percona/shared/core';
 import { getStyles } from './OperatorStatusItem.styles';
 import { DBClusterConnectionItemProps } from './OperatorStatusItem.types';
 import { KubernetesOperatorStatus } from './KubernetesOperatorStatus/KubernetesOperatorStatus';
+import { buildOperatorLabel } from './OperatorStatusItem.utils';
 
 export const OperatorStatusItem: FC<DBClusterConnectionItemProps> = ({ operator, databaseType, dataQa }) => {
   const styles = useStyles(getStyles);
@@ -11,7 +12,7 @@ export const OperatorStatusItem: FC<DBClusterConnectionItemProps> = ({ operator,
   return (
     <div className={styles.connectionItemWrapper} data-qa={dataQa}>
       <span className={styles.connectionItemLabel} title={OPERATOR_FULL_LABELS[databaseType]}>
-        {OPERATOR_LABELS[databaseType]}:
+        {buildOperatorLabel(operator, databaseType)}:
       </span>
       <span className={styles.connectionItemValue}>
         <KubernetesOperatorStatus operator={operator} databaseType={databaseType} />
