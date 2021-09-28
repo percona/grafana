957535354d29a775f87784cb7d223dd6d2d41f5b
Fabio Silva <ffjs1993@gmail.com>
2021-09-28 16:25:06 +0100

    PMM-8670 add subsections to IA menu

--- public/app/core/components/sidemenu/TopSection.utils.ts
+++ public/app/core/components/sidemenu/TopSection.utils.ts
@@ -2,12 +2,28 @@ import { NavModelItem } from '@grafana/data';
 import config from '../../config';
 
 export const buildIntegratedAlertingMenuItem = (mainLinks: NavModelItem[]): NavModelItem[] => {
-  const integratedAlertingLink = {
-    id: 'integrated-alerting',
-    text: 'Integrated Alerting',
-    icon: 'list-ul',
-    url: `${config.appSubUrl}/integrated-alerting`,
-  };
+  const integratedAlertingLinks = [
+    {
+      id: 'integrated-alerting-alerts',
+      text: 'Alerts',
+      url: `${config.appSubUrl}/integrated-alerting/alerts`,
+    },
+    {
+      id: 'integrated-alerting-rules',
+      text: 'Alert Rules',
+      url: `${config.appSubUrl}/integrated-alerting/alert-rules`,
+    },
+    {
+      id: 'integrated-alerting-templates',
+      text: 'Alert Rule Templates',
+      url: `${config.appSubUrl}/integrated-alerting/alert-rule-templates`,
+    },
+    {
+      id: 'integrated-alerting-notification-channels',
+      text: 'Notification Channels',
+      url: `${config.appSubUrl}/integrated-alerting/notification-channels`,
+    },
+  ];
   const divider = {
     id: 'divider',
     text: 'Divider',
@@ -16,8 +32,16 @@ export const buildIntegratedAlertingMenuItem = (mainLinks: NavModelItem[]): NavM
   };
   const alertingIndex = mainLinks.findIndex(({ id }) => id === 'alerting');
 
-  if (alertingIndex >= 0) {
-    mainLinks[alertingIndex].children?.unshift(integratedAlertingLink, divider);
+  if (alertingIndex === -1) {
+    mainLinks.push({
+      id: 'alerting',
+      text: 'Alerting',
+      icon: 'bell',
+      subTitle: 'Alert rules & notifications',
+      children: integratedAlertingLinks,
+    });
+  } else {
+    mainLinks[alertingIndex].children?.unshift(...integratedAlertingLinks, divider);
   }
 
   return mainLinks;
