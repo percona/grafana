a4f23ee9588874bb39eaec0b783f0d42ea63dadb
fabio-silva <ffjs1993@gmail.com>
2021-12-15 10:27:27 +0000

    PMM-9200: Test SMTP configuration (#277)
    * PMM-9200 TestEmailSettings
    
    * PMM-9200 CommunicationService
    
    * PMM-9200 use portals to render test section
    
    * PMM-9200 adjust margin for button
    
    * PMM-9200 keep test email after submitting
    
    * PMM-9200 flex-grow to test email input
    
    * PMM-9200 separate types to external file
    
    * PMM-9200 change "email sent" notification
    
    * PMM-9200 add comment explaining test_email
    
    * PMM-9200 add type to test button
    
    * PMM-9200 check email settings before access

--- public/app/percona/settings/Settings.tsx
+++ public/app/percona/settings/Settings.tsx
@@ -33,8 +33,13 @@ export const SettingsPanel: FC = () => {
     refresh?: boolean,
     onError = () => {}
   ) => {
+    // we save the test email here so that we can sent it all the way down to the form again after re-render
+    // the field is deleted from the payload so as not to be sent to the API
+    const { email_alerting_settings: { password = '', test_email: testEmail = '' } = {} } = body;
+    if (testEmail) {
+      body.email_alerting_settings.test_email = undefined;
+    }
     const response = await SettingsService.setSettings(body, callback, generateToken(SET_SETTINGS_CANCEL_TOKEN));
-    const { email_alerting_settings: { password = '' } = {} } = body;
 
     if (refresh && response) {
       window.location.reload();
@@ -46,7 +51,10 @@ export const SettingsPanel: FC = () => {
       // password is not being returned by the API, hence this construction
       const newSettings: Settings = {
         ...response,
-        alertingSettings: { ...response.alertingSettings, email: { ...response.alertingSettings.email, password } },
+        alertingSettings: {
+          ...response.alertingSettings,
+          email: { ...response.alertingSettings.email, password, test_email: testEmail },
+        },
       };
       setSettings(newSettings);
     } else {
