fe12112a48e096b4df5206a3d30cf9aa6244a7ba
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2021-12-15 18:36:11 +0400

    PMM-9237-re-enabling-tests-migrate-from-enzyme: fix unit tests in add-instance folder

--- public/app/percona/add-instance/components/AddRemoteInstance/FormParts/HAProxyConnectionDetails/HAProxyConnectionDetails.test.tsx
+++ public/app/percona/add-instance/components/AddRemoteInstance/FormParts/HAProxyConnectionDetails/HAProxyConnectionDetails.test.tsx
@@ -1,19 +1,18 @@
-import { mount } from 'enzyme';
 import React from 'react';
 import { Form } from 'react-final-form';
-import { dataTestId } from '@percona/platform-core';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { HAProxyConnectionDetails } from './HAProxyConnectionDetails';
 
-xdescribe('HAProxy connection details:: ', () => {
+describe('HAProxy connection details:: ', () => {
   it('should trim username and password values right', async () => {
-    const root = mount(
-      <Form onSubmit={jest.fn()} render={() => <HAProxyConnectionDetails remoteInstanceCredentials={{}} />} />
-    );
+    render(<Form onSubmit={jest.fn()} render={() => <HAProxyConnectionDetails remoteInstanceCredentials={{}} />} />);
 
-    root.find(dataTestId('username-text-input')).simulate('change', { target: { value: '    test    ' } });
-    root.find(dataTestId('password-password-input')).simulate('change', { target: { value: '    test    ' } });
+    const userNameTextInput = await screen.getByTestId('username-text-input');
+    fireEvent.change(userNameTextInput, { target: { value: '    test     ' } });
+    const passwordInput = await screen.getByTestId('password-password-input');
+    fireEvent.change(passwordInput, { target: { value: '    test    ' } });
 
-    expect(root.find(dataTestId('username-text-input')).props().value).toEqual('test');
-    expect(root.find(dataTestId('password-password-input')).props().value).toEqual('test');
+    expect(((await screen.findByTestId('username-text-input')) as HTMLInputElement).value).toEqual('test');
+    expect(((await screen.findByTestId('password-password-input')) as HTMLInputElement).value).toEqual('test');
   });
 });
