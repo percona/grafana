76a57587aeadeaf302c3168b4485e87868f36079
Tiago Mota Santos <tiago.mota@percona.com>
2022-01-05 15:11:46 +0000

    PMM-8045 Delete API keys warning

---
+++ public/app/features/api-keys/ApiKeys.messages.ts
@@ -0,0 +1,5 @@
+export const Messages = {
+  apiKeysDeleteWarning: `If deleting an API key being used by some resource (e.g. DB cluster), be aware
+    that functionality related to that resource might stop working.
+  `,
+};
