0ec2bd2754daeb1a330174834fcdb8d399d44dff
Tiago Mota Santos <tiago.mota@percona.com>
2022-01-06 09:40:30 +0000

    PMM-8045 Improve message

--- public/app/features/api-keys/ApiKeys.messages.ts
+++ public/app/features/api-keys/ApiKeys.messages.ts
@@ -1,5 +1,5 @@
 export const Messages = {
-  apiKeysDeleteWarning: `If deleting an API key being used by some resource (e.g. DB cluster), be aware
-    that functionality related to that resource might stop working.
+  apiKeysDeleteWarning: `If a resource (e.g. DB cluster) uses an API key, deleting that API key 
+  might affect the functionality of that resource.
   `,
 };
