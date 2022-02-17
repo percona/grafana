ac5a03759840aafa82086e815e93fe4339758161
Fabio Silva <ffjs1993@gmail.com>
2022-02-17 15:55:33 +0000

    PMM-7971 fix tests

--- public/app/percona/shared/components/Elements/FeatureLoader/FeatureLoader.test.tsx
+++ public/app/percona/shared/components/Elements/FeatureLoader/FeatureLoader.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { dataTestId } from '@percona/platform-core';
+import { useSelector } from 'react-redux';
 import { FeatureLoader } from './FeatureLoader';
 import { Messages } from './FeatureLoader.messages';
-import { SettingsService } from 'app/percona/settings/Settings.service';
 import { EmptyBlock } from '../EmptyBlock';
 import { getMount } from 'app/percona/shared/helpers/testUtils';
 
@@ -16,9 +16,36 @@ jest.mock('@percona/platform-core', () => {
     },
   };
 });
+jest.mock('react-redux', () => {
+  const original = jest.requireActual('react-redux');
+  return {
+    ...original,
+    useSelector: jest.fn(),
+  };
+});
 
 describe('FeatureLoader', () => {
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockImplementation((callback) => {
+      return callback({
+        perconaUser: { isAuthorized: true },
+        perconaSettings: { isLoading: false, alertingEnabled: true },
+      });
+    });
+  });
+
+  afterEach(() => {
+    (useSelector as jest.Mock).mockClear();
+  });
+
   it('should not have children initially', async () => {
+    (useSelector as jest.Mock).mockImplementation((callback) => {
+      return callback({
+        perconaUser: { isAuthorized: true },
+        perconaSettings: { isLoading: false, alertingEnabled: false },
+      });
+    });
+
     const Dummy = () => <></>;
     const wrapper = await getMount(
       <FeatureLoader featureName="IA" featureSelector={(state) => !!state.perconaSettings.alertingEnabled}>
@@ -42,9 +69,11 @@ describe('FeatureLoader', () => {
   });
 
   it('should show insufficient access permissions message', async () => {
-    const errorObj = { response: { status: 401 } };
-    jest.spyOn(SettingsService, 'getSettings').mockImplementationOnce(() => {
-      throw errorObj;
+    (useSelector as jest.Mock).mockImplementation((callback) => {
+      return callback({
+        perconaUser: { isAuthorized: false },
+        perconaSettings: { isLoading: false, alertingEnabled: false },
+      });
     });
 
     const wrapper = await getMount(
