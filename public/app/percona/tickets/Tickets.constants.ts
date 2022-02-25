3176cb26f47b3f6257eeeacc6135702bcb4c858e
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-02-25 16:26:10 +0100

    PMM-9309 Added row click, fixed issue with column

--- public/app/percona/tickets/Tickets.constants.ts
+++ public/app/percona/tickets/Tickets.constants.ts
@@ -1,10 +1,9 @@
 import { PageModel } from 'app/core/components/Breadcrumb';
 
+export const LIST_TICKETS_CANCEL_TOKEN = 'listTickets';
+export const DATA_INTERVAL = 5000;
 export const PAGE_MODEL: PageModel = {
   title: 'Tickets',
   path: 'tickets',
   id: 'tickets',
 };
-
-export const LIST_TICKETS_CANCEL_TOKEN = 'listTickets';
-export const DATA_INTERVAL = 5000;
