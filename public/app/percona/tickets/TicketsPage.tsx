e35ffb84ec841e1d58ee298292f240542d067870
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-02-25 16:26:10 +0100

    PMM-9309 Added row click, fixed issue with column

--- public/app/percona/tickets/TicketsPage.tsx
+++ public/app/percona/tickets/TicketsPage.tsx
@@ -1,50 +1,18 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
-import { Column } from 'react-table';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { Column, Row } from 'react-table';
 import PageWrapper from '../shared/components/PageWrapper/PageWrapper';
 import { DATA_INTERVAL, LIST_TICKETS_CANCEL_TOKEN, PAGE_MODEL } from './Tickets.constants';
-import { logger } from '@percona/platform-core';
-import { Table } from '../integrated-alerting/components/Table';
+import { logger, Table } from '@percona/platform-core';
 import { isApiCancelError } from '../shared/helpers/api';
 import { useCancelToken } from '../shared/components/hooks/cancelToken.hook';
 import { TicketsService } from './Tickets.service';
 import { Ticket } from './Tickets.types';
 import { useRecurringCall } from '../backup/hooks/recurringCall.hook';
-import { Link, useStyles } from '@grafana/ui';
+import { useStyles } from '@grafana/ui';
 import { getStyles } from './Tickets.styles';
 import { Messages } from './Tickets.messages';
-
-const columns: Array<Column<Ticket>> = [
-  {
-    Header: Messages.table.columns.number,
-    accessor: 'number',
-    // eslint-disable-next-line react/display-name
-    Cell: ({ value, row }) => <Link href={row.values.url}>{value}</Link>,
-  },
-  {
-    Header: Messages.table.columns.description,
-    accessor: 'shortDescription',
-  },
-  {
-    Header: Messages.table.columns.priority,
-    accessor: 'priority',
-  },
-  {
-    Header: Messages.table.columns.state,
-    accessor: 'state',
-  },
-  {
-    Header: Messages.table.columns.createDate,
-    accessor: 'createTime',
-  },
-  {
-    Header: Messages.table.columns.department,
-    accessor: 'department',
-  },
-  {
-    Header: Messages.table.columns.type,
-    accessor: 'taskType',
-  },
-];
+import { css } from '@emotion/css';
+import { palette } from '@grafana/data/src/themes/palette';
 
 export const TicketsPage: FC = () => {
   const [pending, setPending] = useState(true);
@@ -52,6 +20,40 @@ export const TicketsPage: FC = () => {
   const [generateToken] = useCancelToken();
   const [triggerTimeout] = useRecurringCall();
   const styles = useStyles(getStyles);
+
+  const columns = useMemo(
+    (): Column[] => [
+      {
+        Header: Messages.table.columns.number,
+        accessor: 'number',
+      },
+      {
+        Header: Messages.table.columns.description,
+        accessor: 'shortDescription',
+      },
+      {
+        Header: Messages.table.columns.priority,
+        accessor: 'priority',
+      },
+      {
+        Header: Messages.table.columns.state,
+        accessor: 'state',
+      },
+      {
+        Header: Messages.table.columns.createDate,
+        accessor: 'createTime',
+      },
+      {
+        Header: Messages.table.columns.department,
+        accessor: 'department',
+      },
+      {
+        Header: Messages.table.columns.type,
+        accessor: 'taskType',
+      },
+    ],
+    []
+  );
 
   const getData = useCallback(async (showLoading = false) => {
     showLoading && setPending(true);
@@ -74,6 +76,28 @@ export const TicketsPage: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const redirect = (url: string) => {
+    window.open(url, '_blank');
+  };
+
+  const getRowProps = (row: Row<Ticket>) => ({
+    key: 'row',
+    onClick: () => redirect(row.original.url),
+    className: css`
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray15};
+      }
+    `,
+  });
+
+  const getCellProps = () => ({
+    key: 'cell',
+    className: css`
+      background-color: transparent !important;
+    `,
+  });
+
   return (
     <PageWrapper pageModel={PAGE_MODEL}>
       <div className={styles.pageWrapper}>
@@ -83,6 +107,8 @@ export const TicketsPage: FC = () => {
           totalItems={data.length}
           pendingRequest={pending}
           emptyMessage={Messages.table.noData}
+          getRowProps={getRowProps}
+          getCellProps={getCellProps}
         ></Table>
       </div>
     </PageWrapper>
