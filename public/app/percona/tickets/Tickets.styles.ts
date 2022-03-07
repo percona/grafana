af152fa56793b27bc58046af5fe25054d4c74710
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-03-07 16:58:45 +0100

    PMM-9309 fixed review requirements

--- public/app/percona/tickets/Tickets.styles.ts
+++ public/app/percona/tickets/Tickets.styles.ts
@@ -1,8 +1,18 @@
 import { GrafanaTheme } from '@grafana/data';
 import { css } from '@emotion/css';
+import { palette } from '@grafana/data/src/themes/palette';
 
 export const getStyles = ({ spacing }: GrafanaTheme) => ({
   pageWrapper: css`
     margin-bottom: ${spacing.md};
+  `,
+  rowProps: css`
+    cursor: pointer;
+    &:hover {
+      background-color: ${palette.gray15};
+    }
+  `,
+  cellProps: css`
+    background-color: transparent !important;
   `,
 });
