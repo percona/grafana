f4cedae7e24c966937b7aa9d1e7c850175e81dcc
Fabio Silva <ffjs1993@gmail.com>
2022-03-14 15:44:58 +0000

    PMM-9637 use SilenceBell on Alerts

--- public/app/percona/integrated-alerting/components/Alerts/Alerts.constants.ts
+++ public/app/percona/integrated-alerting/components/Alerts/Alerts.constants.ts
@@ -1,5 +1,6 @@
 export const GET_ALERTS_CANCEL_TOKEN = 'getAlerts';
 export const ALERT_RULE_TEMPLATES_TABLE_ID = 'alerts';
+export const TOGGLE_ALERT_CANCEL_TOKEN = 'toggleAlert';
 export const PRIMARY_LABELS = [
   'node_name',
   'type',
