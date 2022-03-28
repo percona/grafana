7a55a1408fbb729d5b92888ed85faecf9f3da2f5
Fabio Silva <ffjs1993@gmail.com>
2022-03-28 22:41:27 +0100

    PMM-9723 change connection title

--- public/app/percona/settings/components/Platform/Platform.messages.ts
+++ public/app/percona/settings/components/Platform/Platform.messages.ts
@@ -1,6 +1,6 @@
 export const Messages = {
-  title: 'Connect PMM to Percona Portal',
-  connectSucceeded: 'Successfully connected PMM to Percona Portal',
+  title: 'Connect PMM to Percona Platform',
+  connectSucceeded: 'Successfully connected PMM to Percona Platform',
   pmmServerName: 'PMM Server Name',
   emailLabel: 'Percona Account (email)',
   passwordLabel: 'Password',
