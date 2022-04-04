4dcbacab9dd6c1fdc43bf277a853e18e45898da8
Fabio Silva <ffjs1993@gmail.com>
2022-04-04 16:02:38 +0100

    PMM-9672 fix tests for Platform

--- public/app/percona/settings/components/Platform/Platform.test.tsx
+++ public/app/percona/settings/components/Platform/Platform.test.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from 'app/store/configureStore';
+import { StoreState } from 'app/types';
 import { Platform } from './Platform';
 
 describe('Platform::', () => {
   it('shows form to connect if not connected', () => {
-    const store = configureStore();
     render(
-      <Provider store={store}>
+      <Provider
+        store={configureStore({
+          percona: {
+            user: { isAuthorized: true },
+            settings: { loading: false, result: { isConnectedToPortal: false } },
+          },
+        } as StoreState)}
+      >
         <Platform />
       </Provider>
     );
@@ -17,9 +24,15 @@ describe('Platform::', () => {
   });
 
   it('shows connected message if connected', () => {
-    const store = configureStore();
     render(
-      <Provider store={store}>
+      <Provider
+        store={configureStore({
+          percona: {
+            user: { isAuthorized: true },
+            settings: { loading: false, result: { isConnectedToPortal: true } },
+          },
+        } as StoreState)}
+      >
         <Platform />
       </Provider>
     );
