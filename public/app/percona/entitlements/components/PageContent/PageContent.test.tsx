6194a36e832b9cb02f1ca4c955cbbd2a5ce37501
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-04-07 15:20:27 +0200

    PMM-9493 Changed tests to use RTL insted of enzyme

--- public/app/percona/entitlements/components/PageContent/PageContent.test.tsx
+++ public/app/percona/entitlements/components/PageContent/PageContent.test.tsx
@@ -1,34 +1,29 @@
-import { mount } from 'enzyme';
-import { dataTestId } from '@percona/platform-core';
+import { render, screen } from '@testing-library/react';
 import { PageContent } from './PageContent';
 import React from 'react';
 
 describe('PageContent', () => {
   it('should display the noData section when no data is passed', async () => {
-    const wrapper = mount(<PageContent hasData={false} emptyMessage="empty" />);
-    const noData = wrapper.find(dataTestId('page-no-data'));
-
-    expect(noData).toHaveLength(1);
-    expect(noData.text()).toEqual('empty');
+    await render(<PageContent hasData={false} emptyMessage="empty" />);
+    expect(screen.getByTestId('page-no-data')).toBeInTheDocument();
+    expect(screen.getByText('empty')).toBeInTheDocument();
   });
 
   it('should not display the noData section when no data is passed and it is still loading', async () => {
-    const wrapper = mount(<PageContent loading={true} hasData={false} emptyMessage="empty" />);
-    const noData = wrapper.find(dataTestId('page-no-data'));
-
-    expect(noData).toHaveLength(1);
-    expect(noData.text()).toHaveLength(0);
+    render(<PageContent loading={true} hasData={false} emptyMessage="empty" />);
+    expect(screen.getByTestId('page-no-data')).toBeInTheDocument();
+    expect(screen.queryByText('empty')).not.toBeInTheDocument();
   });
 
   it('should display the page when there is data', async () => {
-    const Dummy = () => <span></span>;
-    const wrapper = mount(
+    const Dummy = () => <span data-testid="dummy"></span>;
+    render(
       <PageContent hasData={true} emptyMessage="no data">
         <Dummy />
       </PageContent>
     );
 
-    expect(wrapper.find(dataTestId('page-no-data'))).toHaveLength(0);
-    expect(wrapper.find(Dummy).exists()).toBeTruthy();
+    expect(screen.queryByTestId('page-no-data')).not.toBeInTheDocument();
+    expect(screen.getByTestId('dummy')).toBeInTheDocument();
   });
 });
