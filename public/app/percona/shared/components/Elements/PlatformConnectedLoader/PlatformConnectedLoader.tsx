d7348b12e3ddc399ad02649fd01d20df3facabf9
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-04-08 12:57:22 +0200

    PMM-9803 Added check for portal account

--- public/app/percona/shared/components/Elements/PlatformConnectedLoader/PlatformConnectedLoader.tsx
+++ public/app/percona/shared/components/Elements/PlatformConnectedLoader/PlatformConnectedLoader.tsx
@@ -5,23 +5,36 @@ import { Messages } from './PlatformConnectedLoader.messages';
 import { PLATFORM_SETTINGS_URL } from './PlatformConnectedLoader.constants';
 import { getStyles } from './PlatformConnectedLoader.styles';
 import { PermissionLoader } from '../PermissionLoader';
+import { useSelector } from 'react-redux';
+import { getPerconaUser } from 'app/percona/shared/core/selectors';
+import { EmptyBlock } from '../EmptyBlock';
 
 export const PlatformConnectedLoader: FC = ({ children }) => {
   const styles = useStyles(getStyles);
-  const featureSelector = useCallback((state: StoreState) => !!state.perconaUser.isPlatformUser, []);
+  const featureSelector = useCallback((state: StoreState) => !!state.perconaSettings.isConnectedToPortal, []);
+  const { isPlatformUser } = useSelector(getPerconaUser);
+
+  const checkForPlatformUser = () => {
+    if (isPlatformUser) {
+      return children;
+    }
+    return <EmptyBlock>{Messages.platformUser}</EmptyBlock>;
+  };
+
+  const ErrorMessage = () => (
+    <>
+      {Messages.notConnected}&nbsp;
+      <a data-testid="platform-link" className={styles.link} href={PLATFORM_SETTINGS_URL}>
+        {Messages.portalSettings}
+      </a>
+    </>
+  );
 
   return (
     <PermissionLoader
       featureSelector={featureSelector}
-      renderSuccess={() => children}
-      renderError={() => (
-        <>
-          {Messages.notConnected}&nbsp;
-          <a data-testid="platform-link" className={styles.link} href={PLATFORM_SETTINGS_URL}>
-            {Messages.portalSettings}
-          </a>
-        </>
-      )}
+      renderSuccess={checkForPlatformUser}
+      renderError={ErrorMessage}
     />
   );
 };
