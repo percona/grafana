54436ea0f810244c057c100386341e6850e1a45b
Fabio Silva <ffjs1993@gmail.com>
2022-04-19 11:23:24 +0100

    PMM-9363 logout after disconnect

--- public/app/percona/settings/components/Platform/Connected/Connected.messages.ts
+++ public/app/percona/settings/components/Platform/Connected/Connected.messages.ts
@@ -2,5 +2,6 @@ export const Messages = {
   title: 'Percona Platform',
   connected: 'This PMM instance is connected to Percona Platform.',
   disconnect: 'Disconnect',
-  disconnectSucceeded: 'Successfully disconnected PMM from Percona Platform',
+  modalTitle: 'Disconnect from Portal',
+  modalBody: 'Are you sure you want to disconnect from Portal? You will be automatically logged out.',
 };
