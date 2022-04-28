0329301a1e21f5b11209c7babee88f20e7d6e3c5
Fabio Silva <ffjs1993@gmail.com>
2022-04-28 10:38:50 +0100

    PMM-9363 update modal texts

--- public/app/percona/settings/components/Platform/Connected/Connected.messages.ts
+++ public/app/percona/settings/components/Platform/Connected/Connected.messages.ts
@@ -2,6 +2,6 @@ export const Messages = {
   title: 'Percona Platform',
   connected: 'This PMM instance is connected to Percona Platform.',
   disconnect: 'Disconnect',
-  modalTitle: 'Disconnect from Portal',
-  modalBody: 'Are you sure you want to disconnect from Portal? You will be automatically logged out.',
+  modalTitle: 'Disconnect PMM from Percona Platform',
+  modalBody: 'Are you sure you want to disconnect from Percona Platform? This will automatically log you out from PMM.',
 };
