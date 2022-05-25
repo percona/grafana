d9fe9b2e26c060ee58b8fd870990b304a6d1b9bf
Fabio Silva <ffjs1993@gmail.com>
2022-05-25 12:01:39 +0100

    PMM-10059 fix tests on getValuesFromQueryParams

--- public/app/percona/shared/helpers/getValuesFromQueryParams.test.ts
+++ public/app/percona/shared/helpers/getValuesFromQueryParams.test.ts
@@ -1,29 +1,29 @@
 import { getValuesFromQueryParams } from './getValuesFromQueryParams';
 
 describe('getValuesFromQueryParams', () => {
-  it('should return empty array if no keys passed', () => {
+  it('should return empty string if no keys passed', () => {
     expect(getValuesFromQueryParams({}, [])).toEqual([]);
   });
 
-  it('should return empty array if key is not on params', () => {
-    expect(getValuesFromQueryParams({}, [{ key: 'category' }])).toEqual([]);
+  it('should return array with empty string if key is not on params', () => {
+    expect(getValuesFromQueryParams({}, [{ key: 'category' }])).toEqual(['']);
   });
 
   it('should use the default transform if none is passed', () => {
-    const [categories, types] = getValuesFromQueryParams<[string[], string[]]>({ category: 'perf', type: 'foo' }, [
+    const [categories, types] = getValuesFromQueryParams<[string, string]>({ category: 'perf', type: 'foo' }, [
       { key: 'category' },
       { key: 'type' },
     ]);
-    expect(categories).toEqual(['perf']);
-    expect(types).toEqual(['foo']);
+    expect(categories).toEqual('perf');
+    expect(types).toEqual('foo');
   });
 
   it('should use custom transforms', () => {
-    const [hasType, valid] = getValuesFromQueryParams<[string[], boolean]>({ hasType: true, valid: true }, [
+    const [hasType, valid] = getValuesFromQueryParams<[string, boolean]>({ hasType: true, valid: true }, [
       { key: 'hasType' },
       { key: 'valid', transform: (param) => !!param },
     ]);
-    expect(hasType).toEqual(['true']);
+    expect(hasType).toEqual('true');
     expect(valid).toEqual(true);
   });
 });
