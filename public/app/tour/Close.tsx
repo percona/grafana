ec90e5963b24465913eae26e0253e94bb4be6fba
Fabio Silva <ffjs1993@gmail.com>
2022-05-27 13:02:48 +0100

    fix: improve close button for steps

--- public/app/tour/Close.tsx
+++ public/app/tour/Close.tsx
@@ -3,7 +3,12 @@ import { IconButton } from '@grafana/ui';
 import { CloseProps } from '@reactour/tour/dist/components/Close';
 
 const Close: FC<CloseProps> = ({ onClick }) => (
-  <IconButton style={{ position: 'absolute', right: 0, top: 5, outline: 'none' }} onClick={onClick} name="times" />
+  <IconButton
+    style={{ position: 'absolute', right: 10, top: 10, outline: 'none', margin: 0 }}
+    onClick={onClick}
+    name="times"
+    size="lg"
+  />
 );
 
 export default Close;
