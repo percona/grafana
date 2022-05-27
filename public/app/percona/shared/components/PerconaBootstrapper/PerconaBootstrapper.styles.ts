211b957d208935872d1d07334c3759c56df36dd1
Fabio Silva <ffjs1993@gmail.com>
2022-05-27 11:06:47 +0100

    feat: add welcome modal

---
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaBootstrapper.styles.ts
@@ -0,0 +1,20 @@
+import { css } from '@emotion/css';
+import { GrafanaTheme2 } from '@grafana/data';
+
+export const getStyles = ({ v1: { spacing } }: GrafanaTheme2) => ({
+  iconContainer: css`
+    text-align: center;
+    margin-bottom: ${spacing.md};
+  `,
+  svg: css`
+    width: 100px;
+    height: 100px;
+  `,
+  list: css`
+    padding-left: ${spacing.lg};
+    margin-bottom: ${spacing.lg};
+  `,
+  docsLink: css`
+    text-decoration: underline;
+  `,
+});
--- public/app/percona/shared/components/PerconaBootstrapper/PerconaBootstrapper.tsx
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaBootstrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTour } from '@reactour/tour';
 import { useAppDispatch } from 'app/store/store';
 import {
@@ -9,11 +9,18 @@ import {
   fetchUserStatusAction,
 } from 'app/percona/shared/core/reducers';
 import { contextSrv } from 'app/core/services/context_srv';
+import { Button, HorizontalGroup, Icon, Modal, useStyles2 } from '@grafana/ui';
+import { getStyles } from './PerconaBootstrapper.styles';
 
 // This component is only responsible for populating the store with Percona's settings initially
 export const PerconaBootstrapper = () => {
   const dispatch = useAppDispatch();
   const { setCurrentStep, setIsOpen } = useTour();
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const styles = useStyles2(getStyles);
+  const isLoggedIn = !!contextSrv.user.isSignedIn;
+
+  const dismissModal = () => setModalIsOpen(false);
 
   useEffect(() => {
     setCurrentStep(0);
@@ -34,13 +41,53 @@ export const PerconaBootstrapper = () => {
       await dispatch(fetchUserStatusAction());
       await dispatch(fetchServerInfoAction());
       await dispatch(fetchServerSaasHostAction());
-      setTimeout(() => setIsOpen(true), 2000);
+      // setTimeout(() => setIsOpen(true), 2000);
     };
 
-    if (contextSrv.user.isSignedIn) {
+    if (isLoggedIn) {
       bootstrap();
     }
-  }, [dispatch, setCurrentStep, setIsOpen]);
+  }, [dispatch, isLoggedIn, setCurrentStep, setIsOpen]);
 
-  return <></>;
+  return isLoggedIn ? (
+    <Modal onDismiss={dismissModal} isOpen={modalIsOpen} title=" Welcome to Percona Monitoring and Management">
+      <div className={styles.iconContainer}>
+        <Icon type="mono" name="pmm-logo" className={styles.svg} />
+      </div>
+      <p>
+        <strong>Percona Monitoring and Management</strong> Percona Monitoring and Management (PMM) is an open source
+        database monitoring, management, and observability solution for MySQL, PostgreSQL, and MongoDB.
+      </p>
+      <p>
+        It allows you to observe the health of your database systems, explore new patterns in their behavior,
+        troubleshoot them and perform database management operations no matter where they are located - on-prem or in
+        the cloud.
+      </p>
+      <ul className={styles.list}>
+        <li>PMM collects thousands of out-of-the-box performance metrics from databases and their hosts.</li>
+        <li>The PMM web UI visualizes data in dashboards.</li>
+        <li>Additional features include advisors for database health assessments.</li>
+      </ul>
+      <p>
+        For more information, please check our{' '}
+        <a
+          href="https://docs.percona.com/percona-monitoring-and-management/index.html"
+          target="_blank"
+          rel="noreferrer noopener"
+          className={styles.docsLink}
+        >
+          documentation
+        </a>
+        .
+      </p>
+      <HorizontalGroup justify="center" spacing="md">
+        <Button>Start tour</Button>
+        <Button variant="secondary" onClick={dismissModal}>
+          Skip
+        </Button>
+      </HorizontalGroup>
+    </Modal>
+  ) : (
+    <></>
+  );
 };
