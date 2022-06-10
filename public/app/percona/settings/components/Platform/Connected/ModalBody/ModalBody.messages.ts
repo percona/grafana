dd676344cdc7c78b5e559dad0acf3629de23456b
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-06-10 09:47:30 +0200

    PMM-9924 Removed spaces in texts

--- public/app/percona/settings/components/Platform/Connected/ModalBody/ModalBody.messages.ts
+++ public/app/percona/settings/components/Platform/Connected/ModalBody/ModalBody.messages.ts
@@ -4,6 +4,6 @@ export const Messages = {
   modalBody:
     'Are you sure you want to force-disconnect this PMM instance? This will unlink the instance from its current organization and stop all synchronizations with Percona Platform.',
   modalBodySecondMessage:
-    'After disconnecting, make sure to also remove this lingering server from the list of connected PMM instances in  ',
-  portalInstances: ' Portal instances',
+    'After disconnecting, make sure to also remove this lingering server from the list of connected PMM instances in ',
+  portalInstances: 'Portal instances',
 };
