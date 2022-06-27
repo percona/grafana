8a0e4a1c17cd32c12065cd4710cfc49683bb92f7
Fabio Silva <ffjs1993@gmail.com>
2022-06-27 11:42:47 +0100

    PMM-10171 destructure all severities

--- public/app/percona/check/Check.service.ts
+++ public/app/percona/check/Check.service.ts
@@ -38,20 +38,33 @@ export const CheckService = {
       token
     );
 
-    return result.map(({ service_name, service_id, ...counts }) => ({
-      serviceName: service_name,
-      serviceId: service_id,
-      counts: {
-        emergency: parseInt(counts.emergency_count, 10),
-        alert: parseInt(counts.alert_count, 10),
-        critical: parseInt(counts.critical_count, 10),
-        error: parseInt(counts.error_count, 10),
-        warning: parseInt(counts.warning_count, 10),
-        notice: parseInt(counts.notice_count, 10),
-        info: parseInt(counts.info_count, 10),
-        debug: parseInt(counts.debug_count, 10),
-      },
-    }));
+    return result.map(
+      ({
+        service_name,
+        service_id,
+        emergency_count = '0',
+        alert_count = '0',
+        critical_count = '0',
+        error_count = '0',
+        warning_count = '0',
+        notice_count = '0',
+        info_count = '0',
+        debug_count = '0',
+      }) => ({
+        serviceName: service_name,
+        serviceId: service_id,
+        counts: {
+          emergency: parseInt(emergency_count, 10),
+          alert: parseInt(alert_count, 10),
+          critical: parseInt(critical_count, 10),
+          error: parseInt(error_count, 10),
+          warning: parseInt(warning_count, 10),
+          notice: parseInt(notice_count, 10),
+          info: parseInt(info_count, 10),
+          debug: parseInt(debug_count, 10),
+        },
+      })
+    );
   },
   async getFailedCheckForService(
     serviceId: string,
