aca70f1f2405c239cf3b61013a38c7158bb36931
Filip Mikes <filip.mikes@3pillarglobal.com>
2022-08-02 16:20:08 +0200

    PMM-10369 improved logic, added done notif.

--- public/app/percona/shared/websockets/notifications.ts
+++ public/app/percona/shared/websockets/notifications.ts
@@ -4,26 +4,34 @@ import appEvents from 'app/core/app_events';
 import { CheckStreamData } from './notifications.types';
 
 export const checkNotification = (data: CheckStreamData) => {
-  if (data.result.topic === 'checks' && data.result.results) {
-    data.result.results.forEach((value) => {
-      if (
-        Severity[value.severity] === Severity.SEVERITY_WARNING ||
-        Severity[value.severity] === Severity.SEVERITY_NOTICE ||
-        Severity[value.severity] === Severity.SEVERITY_DEBUG
-      ) {
-        appEvents.emit(AppEvents.alertWarning, [`${value.service_name}: ${value.summary}`]);
+  data.result.topic === 'checks' &&
+    data.result.results?.forEach((value) => {
+      const message = `${value.check_name} - ${value.service_name}: ${value.summary}`;
+
+      if (!value.summary && value.service_name) {
+        appEvents.emit(AppEvents.alertSuccess, [
+          `${value.check_name} - ${value.service_name}: Finished without finding error.`,
+        ]);
+        return;
       }
+
       if (
         Severity[value.severity] === Severity.SEVERITY_ALERT ||
         Severity[value.severity] === Severity.SEVERITY_CRITICAL ||
         Severity[value.severity] === Severity.SEVERITY_ERROR ||
         Severity[value.severity] === Severity.SEVERITY_EMERGENCY
       ) {
-        appEvents.emit(AppEvents.alertError, [`${value.service_name}: ${value.summary}`]);
+        appEvents.emit(AppEvents.alertError, [message]);
+      }
+      if (
+        Severity[value.severity] === Severity.SEVERITY_WARNING ||
+        Severity[value.severity] === Severity.SEVERITY_NOTICE ||
+        Severity[value.severity] === Severity.SEVERITY_DEBUG
+      ) {
+        appEvents.emit(AppEvents.alertWarning, [message]);
       }
       if (Severity[value.severity] === Severity.SEVERITY_INFO) {
-        appEvents.emit(AppEvents.alertSuccess, [`${value.service_name}: ${value.summary}`]);
+        appEvents.emit(AppEvents.alertSuccess, [message]);
       }
     });
-  }
 };
