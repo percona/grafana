ac39cf35a190af98589d995130dee2d61d20d249
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2022-08-10 16:08:41 +0200

    PMM-10064 Add inventory pagination tests

--- public/app/percona/inventory/Inventory.test.tsx
+++ public/app/percona/inventory/Inventory.test.tsx
@@ -53,6 +53,46 @@ describe('Inventory tables', () => {
     expect(container.querySelectorAll('tr')).toHaveLength(5);
   });
 
+  it('Agents table only first page', () => {
+    const response = {
+      pmm_agent: [{ agent_id: 'pmm-server', runs_on_node_id: 'pmm-server', connected: true }],
+      node_exporter: new Array(50).fill({
+        agent_id: '/agent_id/262189d8-e10f-41c2-b0ae-73cc76be6968',
+        pmm_agent_id: 'pmm-server',
+        status: 'RUNNING',
+        listen_port: 42000,
+      }),
+      postgres_exporter: new Array(50).fill({
+        agent_id: '/agent_id/8b74c54e-4307-4a10-9a6f-1646215cbe07',
+        pmm_agent_id: 'pmm-server',
+        service_id: '/service_id/ab477624-4ee9-49cd-8bd4-9bf3b91628b2',
+        username: 'pmm-managed',
+        status: 'RUNNING',
+        listen_port: 42001,
+      }),
+
+      qan_postgresql_pgstatements_agent: new Array(50).fill({
+        agent_id: '/agent_id/ac55153c-5211-4072-a5de-59eb2a136a5c',
+        pmm_agent_id: 'pmm-server',
+        service_id: '/service_id/ab477624-4ee9-49cd-8bd4-9bf3b91628b2',
+        username: 'pmm-managed',
+        status: 'RUNNING',
+      }),
+    };
+
+    const { container } = render(
+      <Table
+        data={InventoryDataService.getAgentModel(response as any)}
+        rowKey={(rec) => rec.agent_id}
+        columns={AGENTS_COLUMNS}
+        loading={false}
+      />
+    );
+
+    // default page size is 25
+    expect(container.querySelectorAll('tbody tr')).toHaveLength(25);
+  });
+
   it('Services table renders correct with right data', () => {
     const response = {
       postgresql: [
@@ -78,6 +118,29 @@ describe('Inventory tables', () => {
     expect(container.querySelectorAll('tr')).toHaveLength(2);
   });
 
+  it('Services table renders only first page', () => {
+    const response = {
+      postgresql: new Array(100).fill({
+        service_id: '/service_id/ab477624-4ee9-49cd-8bd4-9bf3b91628b2',
+        service_name: 'pmm-server-postgresql',
+        node_id: 'pmm-server',
+        address: '127.0.0.1',
+        port: 5432,
+      }),
+    };
+    const { container } = render(
+      <Table
+        data={InventoryDataService.getServiceModel(response as any)}
+        rowKey={(rec) => rec.service_id}
+        columns={SERVICES_COLUMNS}
+        loading={false}
+      />
+    );
+
+    // default page size is 25
+    expect(container.querySelectorAll('tbody tr')).toHaveLength(25);
+  });
+
   it('Nodes table renders correct with right data', () => {
     const response = {
       generic: [
@@ -96,5 +159,22 @@ describe('Inventory tables', () => {
 
     // length is 3 because header is also tr
     expect(container.querySelectorAll('tr')).toHaveLength(3);
+  });
+
+  fit('Nodes table renders first page only', () => {
+    const response = {
+      generic: new Array(100).fill({ node_id: 'pmm-server', node_name: 'pmm-server', address: '127.0.0.1' }),
+    };
+    const { container } = render(
+      <Table
+        data={InventoryDataService.getNodeModel(response as any)}
+        rowKey={(rec) => rec.node_id}
+        columns={NODES_COLUMNS}
+        loading={false}
+      />
+    );
+
+    // default page size is 25
+    expect(container.querySelectorAll('tbody tr')).toHaveLength(25);
   });
 });
