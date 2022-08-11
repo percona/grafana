5ff800b06305e6a3904b33d12a659b05b2ac304e
Dmytro Liakhov <dim@sikk.me>
2022-08-11 12:54:44 +0300

    PMM-10119: Implement displaying datapoint summaries (#472)
    * PMM-10119: Implement displaying datapoint summaries
    
    * Update public/app/percona/settings/Settings.messages.ts
    
    Co-authored-by: Fábio Silva <ffjs1993@gmail.com>
    
    * PMM-10119: Create separate component TelemetryTooltip
    
    * PMM-10119: Refactor LinkTooltip component
    
    * PMM-10119: Update style for list with summaries
    
    * PMM-10119: Rename property
    
    * PMM-10119: Make tooltip with summaries of datapoints scrollable
    
    Co-authored-by: Fábio Silva <ffjs1993@gmail.com>

--- public/app/percona/settings/Settings.service.ts
+++ public/app/percona/settings/Settings.service.ts
@@ -33,6 +33,7 @@ const toModel = (response: SettingsPayload): Settings => ({
   awsPartitions: response.aws_partitions,
   updatesDisabled: response.updates_disabled,
   telemetryEnabled: response.telemetry_enabled,
+  telemetrySummaries: response.telemetry_summaries,
   metricsResolutions: response.metrics_resolutions,
   dataRetention: response.data_retention,
   sshKey: response.ssh_key,
