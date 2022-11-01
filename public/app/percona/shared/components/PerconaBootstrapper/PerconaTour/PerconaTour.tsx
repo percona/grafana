cb1b0a4021dd6ec67640fe6694a74ee45dfc36d1
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2022-11-01 15:05:57 +0100

    PMM-10919 Add alerting path check

--- public/app/percona/shared/components/PerconaBootstrapper/PerconaTour/PerconaTour.tsx
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaTour/PerconaTour.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import usePerconaTour from 'app/percona/shared/core/hooks/tour';
@@ -10,16 +10,23 @@ const PerconaTourBootstrapper: React.FC = () => {
   const { startTour } = usePerconaTour();
   const location = useLocation();
   const user = useSelector(getPerconaUser);
+  const [shouldAlertingTourOpen, setShouldAlertingTourOpen] = useState(false);
 
   useEffect(() => {
     if (!user.productTourCompleted) {
       return;
     }
 
-    if (!user.alertingTourCompleted && location.pathname.startsWith('/alerting')) {
+    if (!shouldAlertingTourOpen && !user.alertingTourCompleted && location.pathname.startsWith('/alerting')) {
       startTour(TourType.Alerting);
+
+      // prevent the alerting tour from opening when navigating through alerting
+      // if the user dismissed it by clicking on the backdrop
+      setShouldAlertingTourOpen(true);
+    } else if (!location.pathname.startsWith('/alerting')) {
+      setShouldAlertingTourOpen(false);
     }
-  }, [startTour, user, location.pathname]);
+  }, [startTour, user, shouldAlertingTourOpen, location.pathname]);
 
   return null;
 };
