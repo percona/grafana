2d4a75e5198b799637f5223cde7b694d56b8fdbf
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2022-11-09 14:38:02 +0400

    PMM-10874-Register-k8s-Cluster-Move-to-the-separate-page: fix tests

--- public/app/percona/dbaas/components/Kubernetes/EditK8sClusterPage/EditK8sClusterPage.test.tsx
+++ public/app/percona/dbaas/components/Kubernetes/EditK8sClusterPage/EditK8sClusterPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
@@ -33,74 +33,45 @@ describe('EditK8sClusterPage::', () => {
     expect(screen.queryByTestId('pmm-server-url-warning')).toBeFalsy();
     expect(screen.queryByTestId('kubernetes-paste-from-clipboard-button')).toBeInTheDocument();
   });
-  // it('shows PMM Server Url Warning', async () => {
-  //   render(
-  //     <Provider
-  //       store={configureStore({
-  //         percona: {
-  //           user: { isAuthorized: true },
-  //           settings: { loading: false, result: { publicAddress: '' } },
-  //         },
-  //       } as StoreState)}
-  //     >
-  //       <Router history={locationService.getHistory()}>
-  //         <EditK8sClusterPage addKubernetes={() => {}} />
-  //       </Router>
-  //     </Provider>
-  //   );
-  //   expect(await screen.findByTestId('pmm-server-url-warning')).toBeInTheDocument();
-  // });
-  // it('calls addKubernetes with correct values on registering new cluster', () => {
-  //   const addKubernetes = jest.fn();
-  //
-  //   render(
-  //     <Provider
-  //       store={configureStore({
-  //         percona: {
-  //           user: { isAuthorized: true },
-  //           settings: { loading: false, result: { publicAddress: 'localhost' } },
-  //         },
-  //       } as StoreState)}
-  //     >
-  //       <EditK8sClusterPage addKubernetes={addKubernetes} />
-  //     </Provider>
-  //   );
-  //
-  //   const name = 'Test name';
-  //   const kubeConfig = 'Test config';
-  //   const nameEvent = { target: { value: name } };
-  //   const configEvent = { target: { value: kubeConfig } };
-  //   const expected = {
-  //     name,
-  //     kubeConfig,
-  //   };
-  //
-  //   fireEvent.change(screen.getByTestId('kubeConfig-textarea-input'), configEvent);
-  //   fireEvent.change(screen.getByTestId('name-text-input'), nameEvent);
-  //   fireEvent.click(screen.getByTestId('kubernetes-add-cluster-button'));
-  //
-  //   expect(addKubernetes).toHaveBeenCalledWith(expected, false);
-  // });
-  // it('clicking isEKS checkbox shows AWS credentials fields', () => {
-  //   render(
-  //     <Provider
-  //       store={configureStore({
-  //         percona: {
-  //           user: { isAuthorized: true },
-  //           settings: { loading: false, result: { publicAddress: 'localhost' } },
-  //         },
-  //       } as StoreState)}
-  //     >
-  //       <EditK8sClusterPage addKubernetes={() => {}} />
-  //     </Provider>
-  //   );
-  //
-  //   expect(screen.queryByTestId('awsAccessKeyID-text-input')).not.toBeInTheDocument();
-  //   expect(screen.queryByTestId('awsSecretAccessKey-password-input')).not.toBeInTheDocument();
-  //
-  //   fireEvent.click(screen.getByTestId('isEKS-checkbox-input'));
-  //
-  //   expect(screen.queryByTestId('awsAccessKeyID-text-input')).toBeInTheDocument();
-  //   expect(screen.queryByTestId('awsSecretAccessKey-password-input')).toBeInTheDocument();
-  // });
+  it('shows PMM Server Url Warning', async () => {
+    render(
+      <Provider
+        store={configureStore({
+          percona: {
+            user: { isAuthorized: true },
+            settings: { loading: false, result: { publicAddress: '' } },
+          },
+        } as StoreState)}
+      >
+        <Router history={locationService.getHistory()}>
+          <EditK8sClusterPage />
+        </Router>
+      </Provider>
+    );
+    expect(await screen.findByTestId('pmm-server-url-warning')).toBeInTheDocument();
+  });
+  it('clicking isEKS checkbox shows AWS credentials fields', () => {
+    render(
+      <Provider
+        store={configureStore({
+          percona: {
+            user: { isAuthorized: true },
+            settings: { loading: false, result: { publicAddress: 'localhost' } },
+          },
+        } as StoreState)}
+      >
+        <Router history={locationService.getHistory()}>
+          <EditK8sClusterPage />
+        </Router>
+      </Provider>
+    );
+
+    expect(screen.queryByTestId('awsAccessKeyID-text-input')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('awsSecretAccessKey-password-input')).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId('isEKS-checkbox-input'));
+
+    expect(screen.queryByTestId('awsAccessKeyID-text-input')).toBeInTheDocument();
+    expect(screen.queryByTestId('awsSecretAccessKey-password-input')).toBeInTheDocument();
+  });
 });
