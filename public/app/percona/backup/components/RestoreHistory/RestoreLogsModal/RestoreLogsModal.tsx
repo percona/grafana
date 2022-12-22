a74a00ca31ee4e2c4a801e1acf80ee29ade79176
Fabio Silva <ffjs1993@gmail.com>
2022-12-22 15:29:11 +0000

    PMM-11174 add RestoreLogsModal

---
+++ public/app/percona/backup/components/RestoreHistory/RestoreLogsModal/RestoreLogsModal.tsx
@@ -0,0 +1,14 @@
+import { Modal } from '@percona/platform-core';
+import React, { FC } from 'react';
+
+import { ChunkedLogsViewer } from '../../ChunkedLogsViewer/ChunkedLogsViewer';
+
+import { RestoreLogsModalProps } from './RestoreLogsModal.types';
+
+export const RestoreLogsModal: FC<RestoreLogsModalProps> = ({ title, isVisible, onClose, getLogChunks }) => {
+  return (
+    <Modal title={title} isVisible={isVisible} onClose={onClose}>
+      <ChunkedLogsViewer getLogChunks={getLogChunks} />
+    </Modal>
+  );
+};
