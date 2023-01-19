6923da40f3c691b2f1d856c9026cd65e26487e50
Fabio Silva <ffjs1993@gmail.com>
2023-01-19 14:26:18 +0000

    PMM-11174 simplify return statement

--- public/app/percona/backup/components/RestoreHistory/RestoreLogsModal/RestoreLogsModal.tsx
+++ public/app/percona/backup/components/RestoreHistory/RestoreLogsModal/RestoreLogsModal.tsx
@@ -5,10 +5,8 @@ import { ChunkedLogsViewer } from '../../ChunkedLogsViewer/ChunkedLogsViewer';
 
 import { RestoreLogsModalProps } from './RestoreLogsModal.types';
 
-export const RestoreLogsModal: FC<RestoreLogsModalProps> = ({ title, isVisible, onClose, getLogChunks }) => {
-  return (
-    <Modal title={title} isVisible={isVisible} onClose={onClose}>
-      <ChunkedLogsViewer getLogChunks={getLogChunks} />
-    </Modal>
-  );
-};
+export const RestoreLogsModal: FC<RestoreLogsModalProps> = ({ title, isVisible, onClose, getLogChunks }) => (
+  <Modal title={title} isVisible={isVisible} onClose={onClose}>
+    <ChunkedLogsViewer getLogChunks={getLogChunks} />
+  </Modal>
+);
