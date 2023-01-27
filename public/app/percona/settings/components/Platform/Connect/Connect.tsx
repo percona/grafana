6df803a1408363075c2f9d0e3d35606db7175217
Fabio Silva <ffjs1993@gmail.com>
2023-01-27 14:52:20 +0000

    PMM-11113: change layout for portal connection

--- public/app/percona/settings/components/Platform/Connect/Connect.tsx
+++ public/app/percona/settings/components/Platform/Connect/Connect.tsx
@@ -1,13 +1,13 @@
 import { LoaderButton, TextInputField } from '@percona/platform-core';
 import React, { FC } from 'react';
 import { Form, FormRenderProps } from 'react-final-form';
-import { useSelector } from 'react-redux';
 
-import { useStyles } from '@grafana/ui';
+import { Button, useStyles2 } from '@grafana/ui';
 import { PMMServerUrlWarning } from 'app/percona/dbaas/components/PMMServerURLWarning/PMMServerUrlWarning';
 import { useShowPMMAddressWarning } from 'app/percona/shared/components/hooks/showPMMAddressWarning';
 import { getPerconaServer } from 'app/percona/shared/core/selectors';
 import validators from 'app/percona/shared/helpers/validators';
+import { useSelector } from 'app/types';
 
 import { Messages } from '../Platform.messages';
 import { ConnectRenderProps } from '../types';
@@ -16,48 +16,73 @@ import { getStyles } from './Connect.styles';
 import { ConnectProps } from './Connect.types';
 
 export const Connect: FC<ConnectProps> = ({ onConnect, connecting, initialValues }) => {
-  const styles = useStyles(getStyles);
+  const styles = useStyles2(getStyles);
   const { saasHost } = useSelector(getPerconaServer);
   const [showPMMAddressWarning] = useShowPMMAddressWarning();
 
   const ConnectForm: FC<FormRenderProps<ConnectRenderProps>> = ({ valid, handleSubmit }) => (
-    <form data-testid="connect-form" className={styles.form} onSubmit={handleSubmit} autoComplete="off">
-      <legend className={styles.legend}>{Messages.title}</legend>
-      {showPMMAddressWarning && <PMMServerUrlWarning />}
-      <TextInputField name="pmmServerId" disabled label={Messages.pmmServerId} />
-      <TextInputField
-        name="pmmServerName"
-        label={Messages.pmmServerName}
-        validators={[validators.required]}
-        showErrorOnBlur
-        required
-        disabled={connecting}
-      />
-      <div className={styles.accessTokenRow}>
-        <TextInputField
-          name="accessToken"
-          label={Messages.accessToken}
-          validators={[validators.required]}
-          showErrorOnBlur
-          required
-          disabled={connecting}
-        />
-        <a href={`${saasHost}/profile`} rel="noreferrer noopener" target="_blank">
-          Get token
-        </a>
-      </div>
-      <LoaderButton
-        data-testid="connect-button"
-        type="submit"
-        size="md"
-        variant="primary"
-        disabled={!valid || connecting}
-        loading={connecting}
-        className={styles.submitButton}
-      >
-        {Messages.connect}
-      </LoaderButton>
-    </form>
+    <>
+      <h2>{Messages.perconaPlatform}</h2>
+      <p>
+        <h4>{Messages.whatIsPerconaPlatform}</h4>
+        {Messages.perconaPlatformExplanation}
+      </p>
+      <p>
+        <h4>{Messages.whyToConnect}</h4>
+        {Messages.connectionReason}
+      </p>
+      <p>
+        <h4>{Messages.noPerconaAccount}</h4>
+        {Messages.createAnAccount}
+      </p>
+      <a href={`${saasHost}/login`} rel="noreferrer noopener" target="_blank">
+        <Button variant="secondary" icon="external-link-alt">
+          {Messages.createPerconaAccountAnchor}
+        </Button>
+      </a>
+      <h2 className={styles.connectionTitle}>{Messages.connectTitle}</h2>
+      <form data-testid="connect-form" className={styles.form} onSubmit={handleSubmit} autoComplete="off">
+        <div className={styles.serverDetails}>
+          <TextInputField name="pmmServerId" disabled label={Messages.pmmServerId} fieldClassName="my-class" />
+          <TextInputField
+            name="pmmServerName"
+            label={Messages.pmmServerName}
+            validators={[validators.required]}
+            showErrorOnBlur
+            required
+            disabled={connecting}
+          />
+        </div>
+        <div className={styles.accessTokenRow}>
+          <TextInputField
+            name="accessToken"
+            label={Messages.accessToken}
+            validators={[validators.required]}
+            placeholder="Token here..."
+            showErrorOnBlur
+            required
+            disabled={connecting}
+          />
+          <a href={`${saasHost}/profile`} rel="noreferrer noopener" target="_blank" className={styles.getTokenAnchor}>
+            <Button variant="secondary" fill="text" icon="external-link-alt">
+              Get token
+            </Button>
+          </a>
+        </div>
+        <LoaderButton
+          data-testid="connect-button"
+          type="submit"
+          size="md"
+          variant="primary"
+          disabled={!valid || connecting}
+          loading={connecting}
+          className={styles.submitButton}
+        >
+          {Messages.connect}
+        </LoaderButton>
+        {showPMMAddressWarning && <PMMServerUrlWarning />}
+      </form>
+    </>
   );
 
   return (
