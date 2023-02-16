72f76fcb9602190abbb59cc159c0e1d8793a9774
Fabio Silva <ffjs1993@gmail.com>
2023-02-16 12:49:29 +0000

    PMM-11199 change comment type

--- public/app/plugins/panel/pmm-update/hooks/useClickOutside.ts
+++ public/app/plugins/panel/pmm-update/hooks/useClickOutside.ts
@@ -3,9 +3,7 @@ import { useEffect, RefObject } from 'react';
 export const useClickOutside = (ref: RefObject<HTMLElement>, handler: (e: Event) => void) => {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
-      {
-        /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions */
-      }
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
       if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
         handler(e);
       }
