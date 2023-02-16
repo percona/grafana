ff3c7b51c74fa772365c1f3e79f18b9332462418
Fabio Silva <ffjs1993@gmail.com>
2023-02-16 13:00:10 +0000

    PMM-11199 add messages to LastCheck

--- public/app/plugins/panel/pmm-update/components/LastCheck/LastCheck.tsx
+++ public/app/plugins/panel/pmm-update/components/LastCheck/LastCheck.tsx
@@ -4,12 +4,14 @@ import { Button, IconName } from '@grafana/ui';
 
 import { LastCheckProps } from '../../types';
 
+import { Messages } from './LastCheck.messages';
 import * as styles from './LastCheck.styles';
 
 export const LastCheck: FC<LastCheckProps> = ({ lastCheckDate, onCheckForUpdates, disabled = false }) => (
   <div className={styles.lastCheck}>
     <p>
-      Last check: <span data-testid="update-last-check">{lastCheckDate}</span>
+      {Messages.lastCheck}
+      <span data-testid="update-last-check">{lastCheckDate}</span>
     </p>
     <Button
       data-testid="update-last-check-button"
