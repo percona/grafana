f89167e7c576cf49a08f1517a0787993d0ed0e7a
Fabio Silva <ffjs1993@gmail.com>
2023-02-18 15:01:45 +0000

    PMM-11199 add UTC to time string

--- public/app/plugins/panel/pmm-update/UpdatePanel.utils.test.ts
+++ public/app/plugins/panel/pmm-update/UpdatePanel.utils.test.ts
@@ -8,15 +8,15 @@ describe('UpdatePanel utils', () => {
   const timestamp4 = '2021-04-06T00:00:00Z';
 
   it('should format an ISO 8601 timestamp correctly as date without time', () => {
-    expect(formatDateWithYear(timestamp1 as ISOTimestamp)).toBe('June 08, 2020');
-    expect(formatDateWithYear(timestamp2 as ISOTimestamp)).toBe('June 08, 2020');
-    expect(formatDateWithYear(timestamp3 as ISOTimestamp)).toBe('June 07, 2020');
-    expect(formatDateWithYear(timestamp4 as ISOTimestamp)).toBe('April 06, 2021');
+    expect(formatDateWithYear(timestamp1 as ISOTimestamp)).toBe('June 08, 2020 UTC');
+    expect(formatDateWithYear(timestamp2 as ISOTimestamp)).toBe('June 08, 2020 UTC');
+    expect(formatDateWithYear(timestamp3 as ISOTimestamp)).toBe('June 07, 2020 UTC');
+    expect(formatDateWithYear(timestamp4 as ISOTimestamp)).toBe('April 06, 2021 UTC');
   });
   it('should format an ISO 8601 timestamp correctly as date with time', () => {
-    expect(formatDateWithTime(timestamp1 as ISOTimestamp)).toBe('June 08, 19:16');
-    expect(formatDateWithTime(timestamp2 as ISOTimestamp)).toBe('June 08, 0:06');
-    expect(formatDateWithTime(timestamp3 as ISOTimestamp)).toBe('June 07, 23:36');
-    expect(formatDateWithTime(timestamp4 as ISOTimestamp)).toBe('April 06, 0:00');
+    expect(formatDateWithTime(timestamp1 as ISOTimestamp)).toBe('June 08, 19:16 UTC');
+    expect(formatDateWithTime(timestamp2 as ISOTimestamp)).toBe('June 08, 0:06 UTC');
+    expect(formatDateWithTime(timestamp3 as ISOTimestamp)).toBe('June 07, 23:36 UTC');
+    expect(formatDateWithTime(timestamp4 as ISOTimestamp)).toBe('April 06, 0:00 UTC');
   });
 });
