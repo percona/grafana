265b64f133520b9cfb1d23f308c68045bd4bc366
Alexey Mukas <ritbl@protonmail.com>
2023-02-20 09:36:31 +0100

    add empty pmm-ui app

---
+++ apps/pmm-ui/config/craco.config.dev.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    webpack: {
+        configure: (config) => {
+            config.output.publicPath = "auto";
+
+            if (!config.plugins) {
+                config.plugins = [];
+            }
+
+            config.plugins.unshift();
+
+            return config;
+        },
+    },
+};
