9d1b233575448b0fe2fbf0f3c1658e8773f7be5b
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-02-28 16:08:54 +0100

    PMM-11512 show correct error message for proxy error

--- public/app/percona/settings/components/Platform/Platform.tsx
+++ public/app/percona/settings/components/Platform/Platform.tsx
@@ -15,10 +15,15 @@ import { getPerconaServer, getPerconaSettings } from 'app/percona/shared/core/se
 
 import { Connect } from './Connect/Connect';
 import { Connected } from './Connected/Connected';
-import { API_INVALID_TOKEN_ERROR_CODE, CONNECT_AFTER_SETTINGS_DELAY, CONNECT_DELAY } from './Platform.constants';
+import {
+  API_INVALID_TOKEN_ERROR_CODE,
+  CONNECT_AFTER_SETTINGS_DELAY,
+  CONNECT_DELAY,
+  PMM_NOT_SUPPORT_PROXY_ERROR_CODE,
+} from './Platform.constants';
 import { Messages } from './Platform.messages';
 import { PlatformService } from './Platform.service';
-import { ConnectRenderProps, ConnectErrorBody } from './types';
+import { ConnectErrorBody, ConnectRenderProps } from './types';
 
 export const Platform: FC = () => {
   const navModel = usePerconaNavModel('settings-percona-platform');
@@ -60,6 +65,8 @@ export const Platform: FC = () => {
 
       if (error.response?.data?.code === API_INVALID_TOKEN_ERROR_CODE) {
         appEvents.emit(AppEvents.alertError, [Messages.invalidToken]);
+      } else if (error.response?.data?.code === PMM_NOT_SUPPORT_PROXY_ERROR_CODE) {
+        appEvents.emit(AppEvents.alertError, [error.response.data.message]);
       } else {
         appEvents.emit(AppEvents.alertError, [error.message]);
       }
