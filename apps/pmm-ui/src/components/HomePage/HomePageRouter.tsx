962decc530f2f05057b90508f99a4cf743b57170
Oleksii Kysil <oleksii.kysil@gmail.com>
2023-03-21 15:14:11 +0200

    PMM-11508 Fix styling loading block
    Fix styling when showing the loading placeholder.

--- apps/pmm-ui/src/components/HomePage/HomePageRouter.tsx
+++ apps/pmm-ui/src/components/HomePage/HomePageRouter.tsx
@@ -2,7 +2,8 @@ import React, { FC, ReactNode, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { StoreState } from '../../reducers/store';
 import { fetchSystemTipsAction } from '../../reducers/tips/tips';
-import { LoadingPlaceholder } from '@grafana/ui';
+import { LoadingPlaceholder, useStyles2 } from '@grafana/ui';
+import { css } from '@emotion/css';
 
 export interface HomePageRouterProps {
   defaultHomePage: ReactNode;
@@ -10,6 +11,7 @@ export interface HomePageRouterProps {
   userId: number;
 }
 export const HomePageRouter: FC<HomePageRouterProps> = (props) => {
+  const styles = useStyles2(getStyles);
   const { loading, tips } = useSelector((state: StoreState) => state.tips.systemTips);
   const allTipsCovered = loading === false && tips.every((t) => t.completed);
   const dispatch = useDispatch();
@@ -18,10 +20,22 @@ export const HomePageRouter: FC<HomePageRouterProps> = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return loading ? (
-    <LoadingPlaceholder text={'Loading...'} />
+    <div className={styles.loadingPage}>
+      <LoadingPlaceholder text={'Loading...'} />
+    </div>
   ) : allTipsCovered ? (
     <>{props.defaultHomePage}</>
   ) : (
     <>{props.overrideHomePage}</>
   );
 };
+
+const getStyles = () => ({
+  loadingPage: css`
+    height: 100%;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+});
