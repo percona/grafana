abef42a50d8429a8be32bf3722159cef8328957b
Przemyslaw Kadej <przemyslaw.kadej@implex.dev>
2023-04-03 20:28:45 +0200

    PMM-11504 Modal content updates

--- apps/pmm-ui/src/components/HelpCenter/components/TipsContainer/TipNotConnected.messages.ts
+++ apps/pmm-ui/src/components/HelpCenter/components/TipsContainer/TipNotConnected.messages.ts
@@ -1,22 +1,12 @@
 export const Messages = {
   title: 'Get more out of PMM',
   connectingInfo: 'Simply connect PMM to Percona Platform to get more Advisors and have insights on:',
-  getMore: 'Get more ',
-  advisors: 'Advisors',
-  advisorsDescription: ' to automatically check your system status.',
-  alertsTemplate: 'Alerts Templates',
-  alertsTemplateDescription: ' to notify you when something happens.',
-  visitOur: 'Visit our ',
-  premiumPlans: 'Premium Plans',
-  premiumPlansDescription: ' to discover all benefits and growth potential for your projects.',
+  li1: 'How to configure your databases in the best way',
+  li2: 'What performance enhancements are available for your unique setup',
+  li3: 'How to tighten your database security',
+  li4: 'How to tune your database performancez',
 
   button: {
     connectToPlatform: 'Connect to Percona Platform',
-  },
-  link: {
-    alertsTemplate:
-      'https://docs.percona.com/percona-monitoring-and-management/get-started/alerting.html#create-a-percona-templated-alert',
-    advisors: 'https://docs.percona.com/percona-monitoring-and-management/how-to/configure.html#advisors',
-    premiumPlans: 'https://www.percona.com/software/percona-platform/subscription',
   },
 };
--- apps/pmm-ui/src/components/HelpCenter/components/TipsContainer/TipNotConnected.tsx
+++ apps/pmm-ui/src/components/HelpCenter/components/TipsContainer/TipNotConnected.tsx
@@ -6,43 +6,26 @@ import { Messages } from './TipNotConnected.messages';
 import tipIcon from '../../assets/pmm-percona-icon-purple.svg';
 
 export const TipNotConnected: FC = () => {
-  const styles = useStyles2 (getStyles);
+  const styles = useStyles2(getStyles);
 
   return (
     <div className={styles.tipContainer}>
-      <div className={styles.headerContainer}>
-        <h3 className={styles.tipsLabel}>{Messages.title}</h3>
-      </div>
       <div className={styles.resourceContainer}>
         <div className={styles.body}>
+          <div className={styles.headerContainer}>
+            <h3 className={styles.tipsLabel}>{Messages.title}</h3>
+          </div>
           {Messages.connectingInfo}
           <ul className={styles.unorderedList}>
-            <li>
-              {Messages.getMore}
-              <a className={styles.link} href={Messages.link.advisors} target="_blank" rel="noreferrer">
-                {Messages.advisors}
-              </a>
-              {Messages.advisorsDescription}
-            </li>
-            <li>
-              {Messages.getMore}
-              <a className={styles.link} href={Messages.link.alertsTemplate} target="_blank" rel="noreferrer">
-                {Messages.alertsTemplate}
-              </a>
-              {Messages.alertsTemplateDescription}
-            </li>
+            <li>{Messages.li1}</li>
+            <li>{Messages.li2}</li>
+            <li>{Messages.li3}</li>
+            <li>{Messages.li4}</li>
           </ul>
-          <p>
-            {Messages.visitOur}
-            <a className={styles.link} href={Messages.link.premiumPlans} target="_blank" rel="noreferrer">
-              {Messages.premiumPlans}
-            </a>
-            {Messages.premiumPlansDescription}
-          </p>
 
           <div>
             <Button className={styles.tipsButton} fullWidth variant="secondary">
-              <img className={styles.buttonImage} alt="pmm-logo" src={tipIcon}/>
+              <img className={styles.buttonImage} alt="pmm-logo" src={tipIcon} />
               <div className={styles.perconaButtonLabel}>{Messages.button.connectToPlatform}</div>
             </Button>
           </div>
@@ -54,17 +37,13 @@ export const TipNotConnected: FC = () => {
 
 const getStyles = (theme: GrafanaTheme2) => ({
   tipsLabel: css`
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 22px;
+    font-size: ${theme.typography.h4.fontSize};
     color: ${theme.colors.text.primary};
   `,
   headerContainer: css`
     display: flex;
     justify-content: flex-start;
-  `,
-  link: css`
-    color: ${theme.colors.text.link};
+    padding-bottom: ${theme.spacing(1)};
   `,
   tipsButton: css`
     width: 230px;
@@ -85,30 +64,16 @@ const getStyles = (theme: GrafanaTheme2) => ({
   `,
   resourceContainer: css`
     flex-direction: column;
-
-    margin-bottom: ${theme.spacing (2)};
-
-    padding-bottom: ${theme.spacing (2)};
-
+    padding-bottom: ${theme.spacing(3)};
     background: ${theme.colors.background.secondary};
     border-radius: 8px;
   `,
-  header: css`
-    padding: ${theme.spacing (2, 2)};
-  `,
-  title: css`
-    font-weight: 400;
-    font-size: ${theme.typography.h4.fontSize};
-    line-height: 22px;
-    color: ${theme.colors.text.primary};
-  `,
   body: css`
-    padding: 6px 16px 0;
+    padding: 16px 16px 0;
     font-size: ${theme.typography.body.fontSize};
   `,
-
   unorderedList: css`
-    padding: ${theme.spacing(2, 3)};
+    padding: ${theme.spacing(2, 3, 2)};
   `,
   tipContainer: css`
     margin-bottom: ${theme.spacing(4)};
