fb1ca7434e3ce5557dc8e1b0191594b97b172a02
Przemyslaw Kadej <przemyslaw.kadej@implex.dev>
2023-04-17 19:59:32 +0200

    Changes in Onboarding Feedback API

--- apps/pmm-ui/src/api/portal.ts
+++ apps/pmm-ui/src/api/portal.ts
@@ -1,15 +1,13 @@
 import { ApiResource } from './api';
-import moment from 'moment';
-import {Config} from "../config";
+import { Config } from '../config';
 
 const apiTelemetryOnboarding = new ApiResource({ baseURL: Config.portal.baseUrl });
 
 export const PortalAPI = {
   createFeedback: async (rate: string, description: string, pmmServerId: string) => {
     return await apiTelemetryOnboarding.post<any, any>('/v1/telemetry/onboarding/feedback', {
-      feedback_rate: rate,
-      feedback_description: description,
-      feedback_date: moment(new Date()).format('YYYY-MM-DD'),
+      rate,
+      description,
       pmm_server_id: pmmServerId,
     });
   },
