1e0f138892bfe422b4b4d49bfc6d8ebf6a7cee9c
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-05-09 13:16:15 +0200

    PMM-11976 remove duplicate

--- public/app/percona/check/components/FailedChecksTab/FailedChecksTab.utils.ts
+++ public/app/percona/check/components/FailedChecksTab/FailedChecksTab.utils.ts
@@ -9,16 +9,4 @@ export const stripServiceId = (serviceId: string) => {
   return '';
 };
 
-export const stripNodeId = (nodeId: string) => {
-  const regex = /\/node_id\/(.*)/gm;
-  const match = regex.exec(nodeId);
-
-  if (match && match.length > 0) {
-    return match[1] || '';
-  }
-
-  return '';
-};
-
 export const formatServiceId = (serviceId: string) => `/service_id/${serviceId}`;
-export const formatNodeId = (nodeId: string) => `/node_id/${nodeId}`;
