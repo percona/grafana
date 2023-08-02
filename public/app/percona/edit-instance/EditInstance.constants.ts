ba3674a4e7e623f35a9435ec7ca28c3c2ebde13c
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2023-08-02 15:25:43 +0200

    PMM-11148 Use shortened docs link

--- public/app/percona/edit-instance/EditInstance.constants.ts
+++ public/app/percona/edit-instance/EditInstance.constants.ts
@@ -1,3 +1,2 @@
 export const FETCH_SERVICE_CANCEL_TOKEN = 'fetchService';
-export const EDIT_INSTANCE_DOCS_LINK =
-  'https://docs.percona.com/percona-monitoring-and-management/get-started/roles-and-permissions/lbac.html#editing-labels-in-pmm';
+export const EDIT_INSTANCE_DOCS_LINK = 'https://per.co.na/editing_labels';
