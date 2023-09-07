92557e5ab6bc6f335dd5652bf1a9826f9ef982bd
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2023-09-07 11:03:54 +0200

    PMM-12378 Revert typings change

--- public/app/percona/inventory/Tabs/Services/Clusters.utils.ts
+++ public/app/percona/inventory/Tabs/Services/Clusters.utils.ts
@@ -6,7 +6,7 @@ import { FlattenService } from '../../Inventory.types';
 import { ServicesCluster } from './Clusters.type';
 
 export const getClustersFromServices = (services: FlattenService[]): ServicesCluster[] => {
-  const clusterNames = [...new Set(services.map((s) => s.cluster))];
+  const clusterNames = [...new Set(services.map((s) => s.cluster || s.serviceName))];
   return clusterNames.map<ServicesCluster>((clusterName) => {
     const clusterServices = services.filter((s) => s.cluster === clusterName);
 
