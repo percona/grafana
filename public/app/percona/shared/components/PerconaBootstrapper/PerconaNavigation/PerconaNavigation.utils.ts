5627057adcadf4961e29bf59eae9f56f4abc909e
Yash Sartanpara <yash.sartanpara@3pillarglobal.com>
2024-02-16 16:52:29 +0530

    PMM-12742 Change menu item nav

--- public/app/percona/shared/components/PerconaBootstrapper/PerconaNavigation/PerconaNavigation.utils.ts
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaNavigation/PerconaNavigation.utils.ts
@@ -100,7 +100,6 @@ export const buildInventoryAndSettings = (mainLinks: NavModelItem[], settings?: 
     }
     mainLinks.push(cfgNode);
   } else {
-    configNode.text = 'Configuration';
     if (!configNode.children) {
       configNode.children = [];
     }
