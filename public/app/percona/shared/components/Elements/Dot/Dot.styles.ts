ee8432d2e9099204ad8e5c91e5ccf6b2dceaf00c
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2024-06-14 13:15:23 +0200

    PMM-12912 Fix dot styling

--- public/app/percona/shared/components/Elements/Dot/Dot.styles.ts
+++ public/app/percona/shared/components/Elements/Dot/Dot.styles.ts
@@ -3,15 +3,15 @@ import { css } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 
 export const getStyles = (theme: GrafanaTheme2, top?: number, bottom?: number, right?: number, left?: number) => ({
-  dot: css`
-    position: absolute;
-    top: ${top ?? 'auto'};
-    bottom: ${bottom ?? 'auto'};
-    right: ${right ?? 'auto'};
-    left: ${left ?? 'auto'};
-    width: 6;
-    height: 6;
-    border-radius: ${theme.shape.radius.circle};
-    background-color: ${theme.colors.error.main};
-  `,
+  dot: css({
+    position: 'absolute',
+    width: 6,
+    height: 6,
+    top,
+    bottom,
+    right,
+    left,
+    borderRadius: theme.shape.radius.circle,
+    backgroundColor: theme.colors.error.main,
+  }),
 });
