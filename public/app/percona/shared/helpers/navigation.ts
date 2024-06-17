1699ce14acc021828613a2a239a9884131f24883
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2024-06-17 13:33:42 +0200

    PMM-13122 Pass variables only to same db type dashboards

--- public/app/percona/shared/helpers/navigation.ts
+++ public/app/percona/shared/helpers/navigation.ts
@@ -5,9 +5,17 @@ export const useLinkWithVariables = (url?: string) => {
     return getLinkSrv().getLinkUrl({
       url: url,
       keepTime: true,
-      includeVars: true,
+      // Check if the DB type matches the current one used
+      includeVars: checkDbType(url),
     });
   } else {
     return url ? url : '#';
   }
+};
+
+const checkDbType = (url: string): boolean => {
+  const currentDB = window.location.pathname.split('/')[3].split('-')[0];
+  const urlDB = url.split('/')[3].split('-')[0];
+
+  return currentDB === urlDB;
 };
