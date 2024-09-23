72994e7a129819bad6cefb9a5ad220c1f423db4e
DoraCretu594118 <103416234+doracretu3pillar@users.noreply.github.com>
2024-09-23 13:30:05 +0300

    Added services for getting the changelogs and snooze updates

--- public/app/percona/shared/services/updates/Updates.service.ts
+++ public/app/percona/shared/services/updates/Updates.service.ts
@@ -1,3 +1,9 @@
+import {
+  CheckUpdatesChangelogsPayload,
+  SnoozePayloadBody,
+  SnoozePayloadResponse,
+} from 'app/percona/shared/core/reducers/updates';
+
 import { api } from '../../helpers/api';
 
 import { CheckUpdatesParams, CheckUpdatesResponse } from './Updates.types';
@@ -5,4 +11,10 @@ import { CheckUpdatesParams, CheckUpdatesResponse } from './Updates.types';
 export const UpdatesService = {
   getCurrentVersion: (params: CheckUpdatesParams = { force: false }) =>
     api.get<CheckUpdatesResponse, CheckUpdatesParams>('/v1/server/updates', true, { params }),
+  getUpdatesChangelogs: () => api.get<CheckUpdatesChangelogsPayload>('/v1/server/updates/changelogs', false),
+
+  snoozeCurrentVersion: (body: SnoozePayloadBody) =>
+    api.put<SnoozePayloadResponse, SnoozePayloadBody>('/v1/users/me', body, true),
+
+  getSnoozeCurrentVersion: () => api.get<SnoozePayloadResponse>('/v1/users/me', false),
 };
