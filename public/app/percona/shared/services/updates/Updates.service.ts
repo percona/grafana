6af32cde2073ef660e066eb4d9a3d1fac7aa9087
DoraCretu594118 <103416234+doracretu3pillar@users.noreply.github.com>
2024-09-24 12:41:07 +0300

    Updated updates service , reducer and dispatching logic

--- public/app/percona/shared/services/updates/Updates.service.ts
+++ public/app/percona/shared/services/updates/Updates.service.ts
@@ -6,15 +6,16 @@ import {
 
 import { api } from '../../helpers/api';
 
-import { CheckUpdatesParams, CheckUpdatesResponse } from './Updates.types';
+import { CheckUpdatesBody, CheckUpdatesResponse } from './Updates.types';
 
 export const UpdatesService = {
-  getCurrentVersion: (params: CheckUpdatesParams = { force: false }) =>
-    api.get<CheckUpdatesResponse, CheckUpdatesParams>('/v1/server/updates', true, { params }),
-  getUpdatesChangelogs: () => api.get<CheckUpdatesChangelogsPayload>('/v1/server/updates/changelogs', false),
+  getCurrentVersion: (body: CheckUpdatesBody = { force: false }) =>
+    api.get<CheckUpdatesResponse, CheckUpdatesBody>('/v1/server/updates', true, { params: body }),
 
-  snoozeCurrentVersion: (body: SnoozePayloadBody) =>
+  getUpdatesChangelogs: () => api.get<CheckUpdatesChangelogsPayload, void>('/v1/server/updates/changelogs', false),
+
+  setSnoozeCurrentVersion: (body: SnoozePayloadBody) =>
     api.put<SnoozePayloadResponse, SnoozePayloadBody>('/v1/users/me', body, true),
 
-  getSnoozeCurrentVersion: () => api.get<SnoozePayloadResponse>('/v1/users/me', false),
+  getSnoozeCurrentVersion: () => api.get<SnoozePayloadResponse, void>('/v1/users/me', false),
 };
