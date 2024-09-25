9d46465bb3c53d6d348d9b0d7f55e9a1d6ff4eb8
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2024-09-25 10:13:06 +0200

    PMM-13386 Link to update page from update panel

--- public/app/percona/shared/services/updates/Updates.service.ts
+++ public/app/percona/shared/services/updates/Updates.service.ts
@@ -3,6 +3,6 @@ import { api } from '../../helpers/api';
 import { CheckUpdatesBody, CheckUpdatesResponse } from './Updates.types';
 
 export const UpdatesService = {
-  getCurrentVersion: (body: CheckUpdatesBody = { force: false }) =>
-    api.post<CheckUpdatesResponse, CheckUpdatesBody>('/v1/Updates/Check', body, true),
+  getCurrentVersion: (params: CheckUpdatesBody = { force: false }) =>
+    api.get<CheckUpdatesResponse, CheckUpdatesBody>('/v1/server/updates', true, { params }),
 };
