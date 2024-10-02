d4f81fbbf7655d70b7ec4e221771c92c4c743626
DoraCretu594118 <103416234+doracretu3pillar@users.noreply.github.com>
2024-10-02 22:24:18 +0300

    Added test that checks the functionality of the snooze button

--- public/app/percona/shared/components/PerconaBootstrapper/PerconaUpdateVersion/PerconaUpdateVersion.test.tsx
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaUpdateVersion/PerconaUpdateVersion.test.tsx
@@ -1,17 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { waitFor } from 'test/test-utils';
 
 import * as GrafanaUpdates from 'app/percona/shared/core/reducers/updates/updates';
+import * as User from 'app/percona/shared/core/reducers/user/user';
 import { UpdatesService } from 'app/percona/shared/services/updates';
 import { configureStore } from 'app/store/configureStore';
 
 import PerconaUpdateVersion from './PerconaUpdateVersion';
 import { StoreState } from 'app/types';
 import { EnhancedStore } from '@reduxjs/toolkit';
+import { Messages } from 'app/percona/shared/components/PerconaBootstrapper/PerconaUpdateVersion/PerconaUpdateVersion.constants';
 
 const checkUpdatesChangeLogsSpy = jest.spyOn(GrafanaUpdates, 'checkUpdatesChangeLogs');
+const setSnoozedVersionSpy = jest.spyOn(User, 'setSnoozedVersion');
 
 describe('PerconaUpdateVersion', () => {
   const setup = (store: EnhancedStore) =>
@@ -111,5 +114,48 @@ describe('PerconaUpdateVersion', () => {
 
     expect(screen.queryByTestId('one-update-modal')).not.toBeInTheDocument();
     expect(screen.queryByTestId('multiple-updates-modal')).toBeInTheDocument();
+  });
+
+  it('should dispatch setSnoozedVersion when pressing button', async () => {
+    const changeLogsAPIResponse = {
+      last_check: '',
+      updates: [
+        {
+          version: 'PMM 3.0.1',
+          tag: 'string',
+          timestamp: '2024-09-23T09:12:31.488Z',
+          release_notes_url: 'http://localhost:3000',
+          release_notes_text: 'text1',
+        },
+      ],
+    };
+    const state = {
+      updates: {
+        isLoading: false,
+        updateAvailable: true,
+        latest: { version: '3.0.1' },
+        lastChecked: '',
+      },
+    };
+    jest.spyOn(UpdatesService, 'getUpdatesChangelogs').mockReturnValue(Promise.resolve({ ...changeLogsAPIResponse }));
+
+    const defaultState = configureStore().getState();
+    const store = configureStore({
+      ...defaultState,
+      percona: {
+        ...defaultState.percona,
+        ...state,
+      },
+    } as StoreState);
+    setup(store);
+    await waitFor(() => {
+      expect(checkUpdatesChangeLogsSpy).toHaveBeenCalled();
+    });
+
+    fireEvent.click(screen.getByRole('button', { name: Messages.snooze }));
+
+    await waitFor(() => {
+      expect(setSnoozedVersionSpy).toHaveBeenCalled();
+    });
   });
 });
