6abaf7e718dfbff95814e9cb5ef51d1892170f4c
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-01-07 13:39:46 +0100

    PMM-13633 Add apikey migration modal

---
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaMigrator/components/MigrationSummary.constants.ts
@@ -0,0 +1,4 @@
+export const SERVICE_ACCOUNTS_DOCS_LINK =
+  'https://docs.percona.com/percona-monitoring-and-management/3/api/authentication.html?h=service+accounts';
+
+export const CONTACT_SUPPORT_LINK = 'https://www.percona.com/about/contact';
