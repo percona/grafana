39c2a734e6b452009dea81949f0a5a32df2fb80f
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-02-07 11:11:52 +0100

    comment out frame

--- public/app/percona/shared/components/PerconaBootstrapper/PerconaFrame/PerconaFrame.tsx
+++ public/app/percona/shared/components/PerconaBootstrapper/PerconaFrame/PerconaFrame.tsx
@@ -1,62 +1,62 @@
-import { FC, useEffect } from 'react';
-import { useLocation } from 'react-router';
+import { FC } from 'react';
+// import { useLocation } from 'react-router';
 
-import { locationService } from '@grafana/runtime';
-import { getLinkWithVariables } from 'app/percona/shared/helpers/navigation';
+// import { locationService } from '@grafana/runtime';
+// import { getLinkWithVariables } from 'app/percona/shared/helpers/navigation';
 
-import messager from './Messager';
-import { LinkVariablesMessage, MessageType, NavigateToMessage } from './Messages.types';
+// import messager from './Messager';
+// import { LinkVariablesMessage, MessageType, NavigateToMessage } from './Messages.types';
 
 export const PerconaFrame: FC = () => {
-  const location = useLocation();
+  // const location = useLocation();
 
-  const onNavigate = (message: NavigateToMessage) => {
-    console.log('onNavigate', message);
-    locationService.push(message.data.to.replace('/graph', ''));
-  };
+  // const onNavigate = (message: NavigateToMessage) => {
+  //   console.log('onNavigate', message);
+  //   locationService.push(message.data.to.replace('/graph', ''));
+  // };
 
-  const onLinkVariables = (message: LinkVariablesMessage) => {
-    console.log('onLinkVariables', message);
+  // const onLinkVariables = (message: LinkVariablesMessage) => {
+  //   console.log('onLinkVariables', message);
 
-    if (message.data.url) {
-      const url = getLinkWithVariables(message.data.url);
+  //   if (message.data.url) {
+  //     const url = getLinkWithVariables(message.data.url);
 
-      messager.sendMessage({
-        type: MessageType.LINK_VARIABLES,
-        data: {
-          id: message.data.id,
-          url,
-        },
-      });
-    }
-  };
+  //     messager.sendMessage({
+  //       type: MessageType.LINK_VARIABLES,
+  //       data: {
+  //         id: message.data.id,
+  //         url,
+  //       },
+  //     });
+  //   }
+  // };
 
-  useEffect(() => {
-    messager.register();
+  // // useEffect(() => {
+  //   // messager.register();
 
-    messager.addListener({
-      type: MessageType.NAVIGATE_TO,
-      onMessage: onNavigate,
-    });
+  //   // messager.addListener({
+  //   //   type: MessageType.NAVIGATE_TO,
+  //   //   onMessage: onNavigate,
+  //   // });
 
-    messager.addListener({
-      type: MessageType.LINK_VARIABLES,
-      onMessage: onLinkVariables,
-    });
+  //   // messager.addListener({
+  //   //   type: MessageType.LINK_VARIABLES,
+  //   //   onMessage: onLinkVariables,
+  //   // });
 
-    return () => {
-      messager.unregister();
-    };
-  }, []);
+  //   // return () => {
+  //   //   messager.unregister();
+  //   // };
+  // // }, []);
 
-  useEffect(() => {
-    messager.sendMessage({
-      type: MessageType.LOCATION_CHANGE,
-      data: {
-        location,
-      },
-    });
-  }, [location]);
+  // useEffect(() => {
+  //   messager.sendMessage({
+  //     type: MessageType.LOCATION_CHANGE,
+  //     data: {
+  //       location,
+  //     },
+  //   });
+  // }, [location]);
 
   return null;
 };
