2fe3ede132c5c61400d0fdcd9a71895b7278cd51
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-02-11 16:16:34 +0100

    PMM-12356 Update unit tests for advisors

--- public/app/percona/check/components/CheckInfoAlert/CheckInfoAlert.test.tsx
+++ public/app/percona/check/components/CheckInfoAlert/CheckInfoAlert.test.tsx
@@ -2,13 +2,19 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import { config } from '@grafana/runtime';
 import { configureStore } from 'app/store/configureStore';
-import { StoreState } from 'app/types';
+import { OrgRole, StoreState } from 'app/types';
 
 import { ChecksInfoAlert } from './CheckInfoAlert';
 import { Messages } from './CheckInfoAlert.messages';
 
 describe('CheckInfoAlert', () => {
+  beforeEach(() => {
+    config.bootData.user.isGrafanaAdmin = true;
+    config.bootData.user.orgRole = OrgRole.Admin;
+  });
+
   it('should only show alert when PMM is not connected to portal', async () => {
     render(
       <Provider
@@ -26,6 +32,38 @@ describe('CheckInfoAlert', () => {
   });
 
   it('should not show alert when PMM is connected to portal', async () => {
+    render(
+      <Provider
+        store={configureStore({
+          percona: {
+            user: { isAuthorized: true },
+            settings: { loading: false, result: { isConnectedToPortal: true } },
+          },
+        } as StoreState)}
+      >
+        <ChecksInfoAlert />
+      </Provider>
+    );
+    expect(screen.queryByText(Messages.title)).not.toBeInTheDocument();
+  });
+
+  it("shouldn't show alert to viewers", () => {
+    render(
+      <Provider
+        store={configureStore({
+          percona: {
+            user: { isAuthorized: true },
+            settings: { loading: false, result: { isConnectedToPortal: true } },
+          },
+        } as StoreState)}
+      >
+        <ChecksInfoAlert />
+      </Provider>
+    );
+    expect(screen.queryByText(Messages.title)).not.toBeInTheDocument();
+  });
+
+  it("shouldn't show alert to editors", () => {
     render(
       <Provider
         store={configureStore({
