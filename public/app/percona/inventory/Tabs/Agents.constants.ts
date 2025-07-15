6cdde209b11e30cd4d98577c8e675815876c45eb
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-07-15 16:45:50 +0200

    PMM-13513 Add extra DSN parameters field & handle nested agent labels

---
+++ public/app/percona/inventory/Tabs/Agents.constants.ts
@@ -0,0 +1,17 @@
+export const AGENTS_MAIN_COLUMNS = [
+  'node_id',
+  'agent_id',
+  'node_name',
+  'address',
+  'custom_labels',
+  'type',
+  'status',
+  'is_connected',
+];
+
+export const AGENT_LABELS_SKIP_KEYS: string[] = [
+  'azure_options',
+  'mongo_db_options',
+  'mysql_options',
+  'postgresql_options',
+];
