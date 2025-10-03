e757bd0a4fa510e3298d6c8a7edde08e58e5f420
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-10-03 15:43:07 +0200

    PMM-13840 Refactor tlsFlag for valkey

--- public/app/percona/add-instance/components/AddRemoteInstance/FormParts/AdditionalOptions/ValkeyTLSCertificate.tsx
+++ public/app/percona/add-instance/components/AddRemoteInstance/FormParts/AdditionalOptions/ValkeyTLSCertificate.tsx
@@ -9,30 +9,30 @@ export const ValkeyTLSCertificate: FC<FormPartProps> = ({ form }) => {
   const values = form.getState().values;
   const tlsFlag = values && values['tls'];
 
+  if (!tlsFlag) {
+    return null;
+  }
+
   return (
     <>
-      {tlsFlag ? (
-        <>
-          <TextareaInputField
-            name="tls_ca"
-            label={Messages.form.labels.additionalOptions.tlsCA}
-            tooltipIcon="info-circle"
-            tooltipText={Messages.form.labels.tooltips.tlsCA}
-          />
-          <TextareaInputField
-            name="tls_key"
-            label={Messages.form.labels.additionalOptions.tlsCertificateKey}
-            tooltipIcon="info-circle"
-            tooltipText={Messages.form.labels.tooltips.tlsCertificateKey}
-          />
-          <TextareaInputField
-            name="tls_cert"
-            label={Messages.form.labels.additionalOptions.tlsCertificate}
-            tooltipIcon="info-circle"
-            tooltipText={Messages.form.labels.tooltips.tlsCertificate}
-          />
-        </>
-      ) : null}
+      <TextareaInputField
+        name="tls_ca"
+        label={Messages.form.labels.additionalOptions.tlsCA}
+        tooltipIcon="info-circle"
+        tooltipText={Messages.form.labels.tooltips.tlsCA}
+      />
+      <TextareaInputField
+        name="tls_key"
+        label={Messages.form.labels.additionalOptions.tlsCertificateKey}
+        tooltipIcon="info-circle"
+        tooltipText={Messages.form.labels.tooltips.tlsCertificateKey}
+      />
+      <TextareaInputField
+        name="tls_cert"
+        label={Messages.form.labels.additionalOptions.tlsCertificate}
+        tooltipIcon="info-circle"
+        tooltipText={Messages.form.labels.tooltips.tlsCertificate}
+      />
     </>
   );
 };
