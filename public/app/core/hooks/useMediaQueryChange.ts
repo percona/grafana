b6255786f9644628395744bc2f2549924d9dd636
Fabio Silva <ffjs1993@gmail.com>
2026-02-10 01:16:46 +0000

    chore: comment

--- public/app/core/hooks/useMediaQueryChange.ts
+++ public/app/core/hooks/useMediaQueryChange.ts
@@ -12,7 +12,7 @@ export function useMediaQueryChange({
     const onMediaQueryChange = (e: MediaQueryListEvent) => onChange(e);
     // @PERCONA
     // Sync current state when effect runs so state is correct even if initial render was wrong
-    // (e.g. theme/breakpoint not ready, or SSR). Without this, we only update on resize.
+    // (e.g. theme/breakpoint not ready). Without this, we only update on resize.
     // This was deeply changed on later Grafana versions
     onChange({ matches: mediaQuery.matches, media: mediaQuery.media } as MediaQueryListEvent);
     mediaQuery.addEventListener('change', onMediaQueryChange);
