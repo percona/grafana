10d39b1422ef6262b5ffef8f19b5119efe887e50
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2026-07-10 09:19:41 +0200

    PMM-14911 Show queries and expressions on alert templates form

--- public/app/percona/integrated-alerting/components/TemplateForm/AdvancedRuleSection/AdvancedRuleSection.types.ts
+++ public/app/percona/integrated-alerting/components/TemplateForm/AdvancedRuleSection/AdvancedRuleSection.types.ts
@@ -1,4 +1,9 @@
+import { TemplateExpression, TemplateQuery } from '../../AlertRuleTemplate/AlertRuleTemplate.types';
+
 export interface AdvancedRuleSectionProps {
   expression: string;
   summary?: string;
+  queries: TemplateQuery[];
+  expressions: TemplateExpression[];
+  condition: string;
 }
